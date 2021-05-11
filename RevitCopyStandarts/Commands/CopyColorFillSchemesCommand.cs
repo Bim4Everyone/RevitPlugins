7ddd29d7bcfc2b6c49867840e58b7272e3e9007e
dosymep <Biseuv_o@a101.ru>
2021-05-11 09:52:54 +0300

    Добавил документацию к классу

--- RevitCopyStandarts/Commands/CopyColorFillSchemesCommand.cs
+++ RevitCopyStandarts/Commands/CopyColorFillSchemesCommand.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Autodesk.Revit.DB;
 
 namespace RevitCopyStandarts.Commands {
+    /// <summary>
+    /// Данный класс копирует цветовые схемы Architecture -> Room & Area -> Color Schemes
+    /// </summary>
     internal class CopyColorFillSchemesCommand : ICopyStandartsCommand {
         private readonly Document _source;
         private readonly Document _target;
@@ -28,6 +31,8 @@ namespace RevitCopyStandarts.Commands {
             using(var transaction = new Transaction(_target)) {
                 transaction.Start($"Копирование \"Схема цветов\"");
 
+                // если не удалять цветовые схемы, то они копируются неверно
+                // появляется дубликат без настроек
                 _target.Delete(targetElements.Intersect(sourceElements, new FillSchemaEqualityComparer()).Select(item => item.Id).ToArray());
                 ElementTransformUtils.CopyElements(_source, sourceElements.Select(item => item.Id).ToArray(), _target, Transform.Identity, new CopyPasteOptions());
 
