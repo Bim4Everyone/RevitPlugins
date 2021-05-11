10f0b422d600a9b5606dd13bdb81d96fceb20f31
dosymep <Biseuv_o@a101.ru>
2021-05-11 11:31:29 +0300

    Добавил документацию

--- RevitCopyStandarts/Commands/CopyMaterialsCommand.cs
+++ RevitCopyStandarts/Commands/CopyMaterialsCommand.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Autodesk.Revit.DB;
 
 namespace RevitCopyStandarts.Commands {
+    /// <summary>
+    /// Копирует материал Manage -> Materials
+    /// </summary>
     internal class CopyMaterialsCommand : CopyStandartsCommand {
         public CopyMaterialsCommand(Document source, Document destination)
             : base(source, destination) {
