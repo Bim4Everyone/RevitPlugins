6e8dea48a869aa967aeae9822bb93886563049e4
dosymep <dosymep@gmail.com>
2021-06-11 11:53:18 +0300

    Добавил вызов команды через pyRevit

--- RevitBatchPrint/Command.cs
+++ RevitBatchPrint/Command.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows.Interop;
 
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
@@ -19,30 +20,54 @@ namespace RevitBatchPrint {
           ExternalCommandData commandData,
           ref string message,
           ElementSet elements) {
-            UIApplication uiapp = commandData.Application;
+
+            new PyRevitCommand().Execute(commandData.Application);
+            return Result.Succeeded;
+        }
+    }
+
+    public class PyRevitCommand {
+        public void Execute(UIApplication uiapp) {
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            var revitPrint = new RevitPrint(doc);
-            var viewSheets = revitPrint.GetViewSheets();
+            try {
+                var revitPrint = new RevitPrint(doc);
+                var viewSheets = revitPrint.GetViewSheets();
 
-            var values = viewSheets
-                .SelectMany(item => revitPrint.FilterParameterNames.Select(paramName => item.LookupParameter(paramName)?.AsString()))
-                .Where(item => !string.IsNullOrEmpty(item))
-                .Distinct()
-                .OrderBy(item => item)
-                .ToList();
+                var viewSheetNames = viewSheets
+                    .SelectMany(item => revitPrint.FilterParameterNames.Select(paramName => item.LookupParameter(paramName)?.AsString()))
+                    .Where(item => !string.IsNullOrEmpty(item))
+                    .Distinct()
+                    .OrderBy(item => item)
+                    .ToList();
 
-            revitPrint.FilterParameterValue = values.FirstOrDefault();
-            revitPrint.Execute();
+                var window = new PrintViewSheetNamesWindow() {
+                    DataContext = new ViewSheetNamesViewModel() {
+                        Names = viewSheetNames,
+                        SelectedName = viewSheetNames.FirstOrDefault()
+                    }
+                };
 
-            if(revitPrint.Errors.Count > 0) {
-                message = string.Join(Environment.NewLine + "- ", revitPrint.Errors);
-                return Result.Failed;
+                new WindowInteropHelper(window) { Owner = uiapp.MainWindowHandle };
+                if(window.ShowDialog() == true) {
+                    revitPrint.FilterParameterValue = viewSheetNames.FirstOrDefault();
+                    revitPrint.Execute();
+
+                    if(revitPrint.Errors.Count > 0) {
+                        TaskDialog.Show("Пакетная печать.", string.Join(Environment.NewLine + "- ", revitPrint.Errors));
+                    } else {
+                        TaskDialog.Show("Пакетная печать.", "Готово!");
+                    }
+                }
+            } catch(Exception ex) {
+#if DEBUG
+                TaskDialog.Show("Пакетная печать.", ex.ToString());
+#else
+                TaskDialog.Show("Пакетная печать.", ex.Message);
+#endif
             }
-
-            return Result.Succeeded;
         }
     }
 }
