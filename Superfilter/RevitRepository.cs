a36ffa8f5ecc7743d2c2133c5cd00ec700fea604
dosymep <dosymep@gmail.com>
2021-07-26 16:38:46 +0300

    Добавил методы получения категорий

--- Superfilter/RevitRepository.cs
+++ Superfilter/RevitRepository.cs
@@ -10,11 +10,11 @@ using Autodesk.Revit.UI;
 
 namespace Superfilter {
     internal class RevitRepository {
-        private readonly Document _document;
-        private readonly UIDocument _uiDocument;
-
         private readonly Application _application;
         private readonly UIApplication _uiApplication;
+
+        private readonly Document _document;
+        private readonly UIDocument _uiDocument;
 
         public RevitRepository(Application application, Document document) {
             _application = application;
@@ -40,6 +40,17 @@ namespace Superfilter {
             return _uiDocument.Selection.GetElementIds()
                 .Select(item => _document.GetElement(item))
                 .ToList();
+        }
+
+        public IList<Category> GetCategories() {
+            return Enum.GetValues(typeof(BuiltInCategory))
+                .OfType<BuiltInCategory>()
+                .Select(item => Category.GetCategory(_document, item))
+                .ToList();
+        }
+
+        public IList<Category> GetCategories(IEnumerable<Element> elements) {
+            return elements.Select(item => item.Category).Distinct().ToList();
         }
     }
 }
