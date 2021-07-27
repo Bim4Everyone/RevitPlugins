ec9fd7eaf75f424411387c6ff960bd4d24e418c4
dosymep <dosymep@gmail.com>
2021-07-27 10:31:26 +0300

    Добавил фильтр категорий

--- Superfilter/RevitViewModel.cs
+++ Superfilter/RevitViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 
 using Autodesk.Revit.ApplicationServices;
@@ -16,14 +18,22 @@ namespace Superfilter {
         private readonly RevitRepository _revitRepository;
         private readonly Func<RevitRepository, IList<Element>> _getElements;
         private string _name;
+        private string _filter;
+        private bool _currentSelection;
+        private string _buttonFilterName;
 
         public RevitViewModel(Application application, Document document, Func<RevitRepository, IList<Element>> getElements) {
             _getElements = getElements;
             _revitRepository = new RevitRepository(application, document);
 
             SelectElements = new RelayCommand(SetSelectedElement, CanSetSelectedElement);
+            SelectCategoriesCommand = new RelayCommand(SelectCategories, CanSelectCategories);
 
             CategoryViewModels = new ObservableCollection<CategoryViewModel>(GetCategoryViewModels());
+            CategoryViewModelsView = CollectionViewSource.GetDefaultView(CategoryViewModels);
+            CategoryViewModelsView.Filter = item => FilterCategory(item as CategoryViewModel);
+
+            ChangeCurrentSelection();
         }
 
         public string Name {
@@ -31,7 +41,27 @@ namespace Superfilter {
             set => _name = value;
         }
 
+        public string Filter {
+            get => _filter;
+            set {
+                _filter = value;
+                OnPropertyChanged(nameof(Filter));
+                CategoryViewModelsView.Refresh();
+            }
+        }
+
+        public string ButtonFilterName {
+            get => _buttonFilterName;
+            set {
+                _buttonFilterName = value;
+                OnPropertyChanged(nameof(ButtonFilterName));
+            }
+        }
+
         public ICommand SelectElements { get; }
+        public ICommand SelectCategoriesCommand { get; }
+
+        public ICollectionView CategoryViewModelsView { get; }
         public ObservableCollection<CategoryViewModel> CategoryViewModels { get; }
 
         private IEnumerable<CategoryViewModel> GetCategoryViewModels() {
@@ -39,6 +69,32 @@ namespace Superfilter {
                 .GroupBy(item => item.Category, new CategoryComparer())
                 .Select(item => new CategoryViewModel(item.Key, item))
                 .OrderBy(item => item.Name);
+        }
+
+        private bool FilterCategory(CategoryViewModel category) {
+            if(string.IsNullOrEmpty(Filter)) {
+                return true;
+            }
+
+            return category.Name.IndexOf(Filter, StringComparison.CurrentCultureIgnoreCase) >= 0;
+        }
+
+        private void SelectCategories(object p) {
+            ChangeCurrentSelection();
+
+            IEnumerable<CategoryViewModel> categories = CategoryViewModelsView.OfType<CategoryViewModel>();
+            foreach(CategoryViewModel category in categories) {
+                category.Selected = _currentSelection;
+            }
+        }
+
+        private void ChangeCurrentSelection() {
+            _currentSelection = !_currentSelection;
+            ButtonFilterName = _currentSelection ? "Выделить всё" : "Убрать выделение";
+        }
+
+        private bool CanSelectCategories(object p) {
+            return CategoryViewModelsView.CanFilter;
         }
 
         private void SetSelectedElement(object p) {
