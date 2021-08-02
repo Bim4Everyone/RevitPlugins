7b068bdff942e6b9ab952b25971cee534aeaa609
dosymep <dosymep@gmail.com>
2021-08-02 10:40:17 +0300

    Добавил свойства

--- RevitCopyViews/CopyViewViewModel.cs
+++ RevitCopyViews/CopyViewViewModel.cs
@@ -12,19 +12,57 @@ namespace RevitCopyViews {
         public List<View> _selectedViews;
         private Delimiter _delimeter;
         private ObservableCollection<string> _prefixes;
+        private string _prefix;
+        private string _suffix;
+        private string _groupView;
+        private ObservableCollection<string> _groupViews;
 
         public CopyViewViewModel(List<View> selectedViews) {
             _selectedViews = selectedViews;
 
+
             Prefixes = new ObservableCollection<string>();
             RevitViewViewModels = new ObservableCollection<RevitViewViewModel>(_selectedViews.Select(item => new RevitViewViewModel(item)));
+
+            Delimeters = new ObservableCollection<Delimiter>() {
+                    new Delimiter() { DisplayValue = "_", Value = "_" },
+                    new Delimiter() { DisplayValue = "Пробел", Value = " " },
+            };
+
+            Delimeter = Delimeters.FirstOrDefault();
         }
 
-        public ObservableCollection<Delimiter> Delimeters { get; }
-            = new ObservableCollection<Delimiter>() {
-            new Delimiter() { DisplayValue = "Пробел", Value = " " },
-            new Delimiter() { DisplayValue = "_", Value = "_" },
-        };
+        public string Prefix {
+            get => _prefix;
+            set {
+                _prefix = value;
+                OnPropertyChanged(nameof(Prefix));
+            }
+        }
+
+        public string Suffix {
+            get => _suffix;
+            set {
+                _suffix = value;
+                OnPropertyChanged(nameof(Suffix));
+            }
+        }
+
+        public string GroupView {
+            get => _groupView;
+            set {
+                _groupView = value;
+                OnPropertyChanged(nameof(GroupView));
+            }
+        }
+
+        public ObservableCollection<string> GroupViews {
+            get => _groupViews;
+            set {
+                _groupViews = value;
+                OnPropertyChanged(nameof(GroupViews));
+            }
+        }
 
         public Delimiter Delimeter {
             get => _delimeter;
@@ -36,9 +74,12 @@ namespace RevitCopyViews {
                     revitView.Delimeter = Delimeter;
                 }
 
-                Prefixes = new ObservableCollection<string>(RevitViewViewModels.Select(item => item.Prefix).OrderBy(item => item));
+                Prefixes = new ObservableCollection<string>(RevitViewViewModels.Select(item => item.Prefix).Distinct().OrderBy(item => item));
+                Prefix = Prefixes.FirstOrDefault();
             }
         }
+
+        public ObservableCollection<Delimiter> Delimeters { get; }
 
         public ObservableCollection<string> Prefixes {
             get => _prefixes;
@@ -54,5 +95,9 @@ namespace RevitCopyViews {
     internal class Delimiter {
         public string Value { get; set; }
         public string DisplayValue { get; set; }
+
+        public override string ToString() {
+            return DisplayValue;
+        }
     }
 }
