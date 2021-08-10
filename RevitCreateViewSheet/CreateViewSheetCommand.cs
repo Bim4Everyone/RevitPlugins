820445ddd8a626a9223c345a36dbcfaa365e867a
dosymep <dosymep@gmail.com>
2021-08-10 11:18:17 +0300

    Add project files.

---
+++ RevitCreateViewSheet/CreateViewSheetCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using dosymep;
+
+namespace RevitCreateViewSheet {
+    [Transaction(TransactionMode.Manual)]
+    public class CreateViewSheetCommand : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            AppDomain.CurrentDomain.AssemblyResolve += AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            try {
+                Excecute(commandData);
+            } catch(Exception ex) {
+#if DEBUG
+                System.Windows.MessageBox.Show(ex.ToString(), "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#else
+                System.Windows.MessageBox.Show(ex.Message, "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#endif
+            } finally {
+                AppDomain.CurrentDomain.AssemblyResolve -= AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            }
+
+            return Result.Succeeded;
+        }
+
+        private void Excecute(ExternalCommandData commandData) {
+            var uiApplication = commandData.Application;
+            var application = uiApplication.Application;
+
+            var uiDocument = uiApplication.ActiveUIDocument;
+            var document = uiDocument.Document;
+
+
+        }
+    }
+}
