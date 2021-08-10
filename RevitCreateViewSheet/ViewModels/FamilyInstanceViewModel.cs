b14276c670bb38f302b10075c29ca389be02a375
dosymep <dosymep@gmail.com>
2021-08-10 11:54:50 +0300

    Добавил ViewModel для основных надписей

---
+++ RevitCreateViewSheet/ViewModels/FamilyInstanceViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+using Autodesk.Revit.DB;
+
+using dosymep.WPF.ViewModels;
+
+namespace RevitCreateViewSheet.ViewModels {
+    internal class FamilyInstanceViewModel : BaseViewModel {
+        private readonly FamilyInstance _familyInstance;
+
+        public FamilyInstanceViewModel(FamilyInstance familyInstance) {
+            _familyInstance = familyInstance;
+        }
+
+        public string Name {
+            get => _familyInstance.Name;
+        }
+    }
+}
