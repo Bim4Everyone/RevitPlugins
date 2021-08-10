a0aa24e6b4cceb24480e4c3c3161c14cd0d179ae
dosymep <dosymep@gmail.com>
2021-08-10 11:55:08 +0300

    Добавил ViewModel для листов

---
+++ RevitCreateViewSheet/ViewModels/ViewSheetViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.WPF.ViewModels;
+
+namespace RevitCreateViewSheet.ViewModels {
+    internal class ViewSheetViewModel : BaseViewModel {
+        private string _name;
+        private FamilyInstanceViewModel _familyInstance;
+
+        public ViewSheetViewModel() {
+            Name = "Без имени";
+        }
+
+        public string Name {
+            get => _name;
+            set => RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        public FamilyInstanceViewModel FamilyInstance {
+            get => _familyInstance;
+            set => RaiseAndSetIfChanged(ref _familyInstance, value);
+        }
+    }
+}
