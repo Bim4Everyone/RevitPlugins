9069a71145b6402fa1d5d6e3b8200cd6b2841cdb
dosymep <dosymep@gmail.com>
2021-08-12 16:55:03 +0300

    Добавил команду переименования видов

---
+++ RevitCopyViews/RenameViewCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using dosymep;
+
+using RevitCopyViews.ViewModels;
+using RevitCopyViews.Views;
+
+namespace RevitCopyViews {
+    [Transaction(TransactionMode.Manual)]
+    public class RenameViewCommand : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            AppDomain.CurrentDomain.AssemblyResolve += AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            try {
+                Excecute(commandData);
+            } catch(Exception ex) {
+#if DEBUG
+                System.Windows.MessageBox.Show(ex.ToString(), "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#else
+                System.Windows.MessageBox.Show(ex.Message, "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#endif
+            } finally {
+                AppDomain.CurrentDomain.AssemblyResolve -= AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            }
+
+            return Result.Succeeded;
+        }
+
+        private void Excecute(ExternalCommandData commandData) {
+            var uiApplication = commandData.Application;
+            var application = uiApplication.Application;
+
+            var uiDocument = uiApplication.ActiveUIDocument;
+            var document = uiDocument.Document;
+
+            var restrictedViewNames = new FilteredElementCollector(document)
+                .OfClass(typeof(View))
+                .Select(item => item.Name)
+                .OrderBy(item => item)
+                .Distinct()
+                .ToArray();
+
+            var window = new RenameViewWindow() {
+                DataContext = new RenameViewViewModel()
+            };
+
+            new WindowInteropHelper(window) { Owner = uiApplication.MainWindowHandle };
+
+            window.ShowDialog();
+        }
+    }
+}
---
+++ RevitCopyViews/ViewModels/RenameViewViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using dosymep.WPF.Commands;
+using dosymep.WPF.ViewModels;
+
+namespace RevitCopyViews.ViewModels {
+    internal class RenameViewViewModel : BaseViewModel {
+        private string _prefix;
+        private string _suffix;
+        private string _replaceOldText;
+        private string _replaceNewText;
+
+        private string _errorText;
+
+        private bool _isAllowReplaceSuffix;
+        private bool _isAllowReplacePrefix;
+
+        public RenameViewViewModel() {
+            RenameViewCommand = new RelayCommand(RenameView, CanRenameView);
+        }
+
+        public string Prefix {
+            get => _prefix;
+            set => this.RaiseAndSetIfChanged(ref _prefix, value);
+        }
+
+        public string Suffix {
+            get => _suffix;
+            set => this.RaiseAndSetIfChanged(ref _suffix, value);
+        }
+
+        public bool IsAllowReplacePrefix {
+            get => _isAllowReplacePrefix;
+            set => this.RaiseAndSetIfChanged(ref _isAllowReplacePrefix, value);
+        }
+        public bool IsAllowReplaceSuffix {
+            get => _isAllowReplaceSuffix;
+            set => this.RaiseAndSetIfChanged(ref _isAllowReplaceSuffix, value);
+        }
+
+        public string ReplaceOldText {
+            get => _replaceOldText;
+            set => this.RaiseAndSetIfChanged(ref _replaceOldText, value);
+        }
+
+        public string ReplaceNewText {
+            get => _replaceNewText;
+            set => this.RaiseAndSetIfChanged(ref _replaceNewText, value);
+        }
+
+        public string ErrorText {
+            get => _errorText;
+            set => this.RaiseAndSetIfChanged(ref _errorText, value);
+        }
+
+        public ICommand RenameViewCommand { get; }
+
+        private void RenameView(object p) {
+
+        }
+
+        private bool CanRenameView(object p) {
+            ErrorText = null;
+            return true;
+        }
+    }
+}
