f234dda7c0db97ab30be1fbe788387a7315476dc
dosymep <dosymep@gmail.com>
2021-08-30 11:46:33 +0300

    Добавил классы управления принтерами

---
+++ RevitBatchPrint/Printing/PrintManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitBatchPrint.Printing {
+    internal class PrintManager {
+        public string[] GetPrinters() {
+            throw new NotImplementedException();
+        }
+
+        public bool HasPrinter(string printerName) {
+            throw new NotImplementedException();
+        }
+
+        public PrinterSettings GetPrinterSettings(string printerName) {
+            return new PrinterSettings(printerName);
+        }
+    }
+}
---
+++ RevitBatchPrint/Printing/PrinterSettings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace RevitBatchPrint.Printing {
+    internal class PrinterSettings : IDisposable {
+        private readonly string _printerName;
+
+        public PrinterSettings(string printerName) {
+            _printerName = printerName;
+        }
+
+        public void AddFormat(string formatName, Size formatSize) {
+            AddFormat(formatName, formatSize, new Rectangle(Point.Empty, formatSize));
+        }
+
+        public void AddFormat(string formatName, Size formatSize, Rectangle visibleArea) {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveForm(string formatName) {
+            throw new NotImplementedException();
+        }
+
+        private void ClosePrinter() {
+            throw new NotImplementedException();
+        }
+
+        #region IDisposable
+
+        private bool _isDisposed;
+
+        ~PrinterSettings() {
+            Dispose(false);
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing) {
+            if(_isDisposed) {
+                ClosePrinter();
+                if(disposing) {
+
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        #endregion
+
+        private void ThrowWin32Exception(bool result) {
+            if(!result) {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+    }
+}
