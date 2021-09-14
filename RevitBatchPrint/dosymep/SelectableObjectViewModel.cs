1415396b74baa9a0b40d3a2c00d06561d6ab7854
dosymep <dosymep@gmail.com>
2021-09-14 15:07:23 +0300

    Добавил класс выбора

---
+++ RevitBatchPrint/dosymep/SelectableObjectViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dosymep.WPF.ViewModels {
+    internal class SelectableObjectViewModel<T> : BaseViewModel {
+        private bool _isSelected;
+
+        public SelectableObjectViewModel(T objectData) {
+            ObjectData = objectData;
+            DisplayData = ObjectData.ToString();
+        }
+
+        public SelectableObjectViewModel(T objectData, bool isSelected) {
+            ObjectData = objectData;
+            IsSelected = isSelected;
+
+            DisplayData = ObjectData.ToString();
+        }
+
+        public T ObjectData { get; }
+        public virtual string DisplayData { get; }
+
+        public bool IsSelected {
+            get => _isSelected;
+            set => this.RaiseAndSetIfChanged(ref _isSelected, value);
+        }
+    }
+}
