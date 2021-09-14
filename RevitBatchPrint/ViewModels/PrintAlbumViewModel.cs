99a958c0f4db5bd38c1bfbfc17c2445f670ff51e
dosymep <dosymep@gmail.com>
2021-09-14 15:07:47 +0300

    Добавил реализацию класса выбора в альбом

--- RevitBatchPrint/ViewModels/PrintAlbumViewModel.cs
+++ RevitBatchPrint/ViewModels/PrintAlbumViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace RevitBatchPrint.ViewModels {
-    public class PrintAlbumViewModel {
+﻿using dosymep.WPF.ViewModels;
+
+namespace RevitBatchPrint.ViewModels {
+    internal class PrintAlbumViewModel : SelectableObjectViewModel<string> {
+        public PrintAlbumViewModel(string objectData) 
+            : base(objectData) {
+            Name = objectData;
+        }
+
+        public PrintAlbumViewModel(string objectData, bool isSelected) 
+            : base(objectData, isSelected) {
+            Name = objectData;
+        }
+
         public int Count { get; set; }
         public string Name { get; set; }
+
+        public override string DisplayData => this.ToString();
 
         public override string ToString() {
             return $"{Name} [{Count}]";
