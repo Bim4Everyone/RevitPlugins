b8429c389fac6fa9efa69fc4e48ffbb7f9b10d75
dosymep <dosymep@gmail.com>
2021-09-17 09:43:09 +0300

    Убрал комментарии

--- Superfilter/SuperfilterCommand.cs
+++ Superfilter/SuperfilterCommand.cs
@@ -23,15 +23,13 @@ namespace Superfilter {
                 var window = new MainWindow() { DataContext = viewModel };
                 new WindowInteropHelper(window) { Owner = commandData.Application.MainWindowHandle };
 
-                if(window.ShowDialog() == true) {
-
-                }
-//            } catch(Exception ex) {
-//#if DEBUG
-//                System.Windows.MessageBox.Show(ex.ToString(), "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
-//#else
-//                System.Windows.MessageBox.Show(ex.Message, "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
-//#endif
+                window.ShowDialog();
+            } catch(Exception ex) {
+#if DEBUG
+                System.Windows.MessageBox.Show(ex.ToString(), "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#else
+                System.Windows.MessageBox.Show(ex.Message, "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#endif
             } finally {
                 AppDomain.CurrentDomain.AssemblyResolve -= AppDomainExtensions.CurrentDomain_AssemblyResolve;
             }
