773d7e5f104ba538757fe49d56363def28d60630
dosymep <dosymep@gmail.com>
2021-09-27 16:26:36 +0300

    Добавил отображение окна

---
+++ RevitGenLookupTables/RevitGetLookupTablesCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using dosymep;
+
+using RevitGenLookupTables.Views;
+
+namespace RevitGenLookupTables {
+    [Transaction(TransactionMode.Manual)]
+    public class SuperfilterCommand : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            AppDomain.CurrentDomain.AssemblyResolve += AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            try {
+                var window = new LookupTablesWindow();
+                new WindowInteropHelper(window) { Owner = commandData.Application.MainWindowHandle };
+                
+                window.ShowDialog();
+            } catch(Exception ex) {
+#if DEBUG
+                System.Windows.MessageBox.Show(ex.ToString(), "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#else
+                System.Windows.MessageBox.Show(ex.Message, "Ошибка", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+#endif
+            } finally {
+                AppDomain.CurrentDomain.AssemblyResolve -= AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
