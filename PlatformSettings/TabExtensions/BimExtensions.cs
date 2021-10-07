a1578125a722d449e3107e2f3c9cbcdb6b0de365
dosymep <dosymep@gmail.com>
2021-10-07 15:07:43 +0300

    Обновил путь получения списка расширений

--- PlatformSettings/TabExtensions/BimExtensions.cs
+++ PlatformSettings/TabExtensions/BimExtensions.cs
@@ -17,7 +17,9 @@ namespace PlatformSettings.TabExtensions {
         public override string Path { get; } = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "pyRevit", "Extensions");
 
         /// <inheritdoc/>
-        public override List<string> Url { get; } = new List<string>() { "https://raw.githubusercontent.com/dosymep/BIMExtensions/master/extensions.json" };
+        public override List<string> Url { get; } = new List<string>() {
+            System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"pyRevit\Extensions\01.BIM.extension\extensions.json")
+        };
 
         /// <inheritdoc/>
         protected override PyRevitExtensionViewModel GetPyRevitExtensionViewModel(PyRevitExtensionDefinitionEx extension) {
