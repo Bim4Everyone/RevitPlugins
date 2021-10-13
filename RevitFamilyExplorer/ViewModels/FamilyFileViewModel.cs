f542106a362a9691a38114f5e4b239bcefa19f95
dosymep <dosymep@gmail.com>
2021-10-13 16:13:45 +0300

    Добавил отображение картинки семейства

--- RevitFamilyExplorer/ViewModels/FamilyFileViewModel.cs
+++ RevitFamilyExplorer/ViewModels/FamilyFileViewModel.cs
@@ -22,10 +22,11 @@ namespace RevitFamilyExplorer.ViewModels {
         private bool _isLoaded;
         private string _imageSource;
         private FileInfo _familyFile;
+        private BitmapSource _familyIcon;
 
         public FamilyFileViewModel(RevitRepository revitRepository, FileInfo familyFile) {
             _revitRepository = revitRepository;
-            
+
 
             ExpandCommand = new RelayCommand(Expand, CanExpand);
             FamilyTypes = new ObservableCollection<FamilyTypeViewModel>() { null };
@@ -42,6 +43,11 @@ namespace RevitFamilyExplorer.ViewModels {
             set => this.RaiseAndSetIfChanged(ref _imageSource, value);
         }
 
+        public BitmapSource FamilyIcon {
+            get => _familyIcon;
+            set => this.RaiseAndSetIfChanged(ref _familyIcon, value);
+        }
+
         public ICommand ExpandCommand { get; }
         public ObservableCollection<FamilyTypeViewModel> FamilyTypes { get; }
 
@@ -50,8 +56,8 @@ namespace RevitFamilyExplorer.ViewModels {
             RaisePropertyChanged(nameof(Name));
 
 #if D2020 || R2020
-            ImageSource = _revitRepository.IsInsertedFamilyFile(newFileInfo) 
-                ? @"pack://application:,,,/RevitFamilyExplorer;component/Resources/insert.png" 
+            ImageSource = _revitRepository.IsInsertedFamilyFile(newFileInfo)
+                ? @"pack://application:,,,/RevitFamilyExplorer;component/Resources/insert.png"
                 : @"pack://application:,,,/RevitFamilyExplorer;component/Resources/not-insert.png";
 #elif D2021 || R2021
             ImageSource = _revitRepository.IsInsertedFamilyFile(newFileInfo) 
@@ -62,12 +68,14 @@ namespace RevitFamilyExplorer.ViewModels {
                 ? @"pack://application:,,,/RevitFamilyExplorer_2022;component/Resources/insert.png" 
                 : @"pack://application:,,,/RevitFamilyExplorer_2022;component/Resources/not-insert.png";
 #endif
+
+            FamilyIcon = ShellFile.FromFilePath(_familyFile.FullName).Thumbnail.BitmapSource;
         }
 
         private void Expand(object p) {
-            _isLoaded = true;            
+            _isLoaded = true;
             FamilyTypes.Clear();
-           
+
             foreach(var familyType in GetFamilySymbols()) {
                 FamilyTypes.Add(familyType);
             }
