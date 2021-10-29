e708f71926783b57e1cbfbd040e9683adfbd7d7e
dosymep <dosymep@gmail.com>
2021-10-29 09:05:00 +0300

    Добавил сравнение стадий

--- RevitRooms/ViewModels/PhaseViewModel.cs
+++ RevitRooms/ViewModels/PhaseViewModel.cs
@@ -9,7 +9,7 @@ using Autodesk.Revit.DB;
 using dosymep.WPF.ViewModels;
 
 namespace RevitRooms.ViewModels {
-    internal class PhaseViewModel : BaseViewModel {
+    internal class PhaseViewModel : BaseViewModel, IComparable<PhaseViewModel>, IEquatable<PhaseViewModel> {
         private readonly Phase _phase;
 
         public PhaseViewModel(Phase phase) {
@@ -19,5 +19,25 @@ namespace RevitRooms.ViewModels {
         public string DisplayData {
             get { return _phase.Name; }
         }
+
+        #region SystemOverrides
+
+        public int CompareTo(PhaseViewModel other) {
+            return _phase.Name.CompareTo(other._phase.Name);
+        }
+
+        public override bool Equals(object obj) {
+            return Equals(obj as PhaseViewModel);
+        }
+
+        public bool Equals(PhaseViewModel other) {
+            return other != null && _phase.Id.Equals(other._phase.Id);
+        }
+
+        public override int GetHashCode() {
+            return -2121273300 + _phase.Id.GetHashCode();
+        }
+
+        #endregion
     }
 }
