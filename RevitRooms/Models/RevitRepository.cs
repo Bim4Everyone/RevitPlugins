689f3209485a96843333f2e0b029316f382e814a
dosymep <dosymep@gmail.com>
2021-11-09 10:03:39 +0300

    Убрал учет зон

--- RevitRooms/Models/RevitRepository.cs
+++ RevitRooms/Models/RevitRepository.cs
@@ -28,7 +28,7 @@ namespace RevitRooms.Models {
             _document = document;
             _uiDocument = new UIDocument(document);
 
-            _filter = new ElementMulticategoryFilter(new[] { BuiltInCategory.OST_Rooms, BuiltInCategory.OST_Areas });
+            _filter = new ElementMulticategoryFilter(new[] { BuiltInCategory.OST_Rooms });
         }
 
         public IList<SpatialElement> GetSelectedSpatialElements() {
