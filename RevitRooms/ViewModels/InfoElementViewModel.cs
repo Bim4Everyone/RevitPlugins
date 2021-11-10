cda256fbb869e7ce7d14b2cbb73dcacc98080dc2
dosymep <dosymep@gmail.com>
2021-11-10 10:11:55 +0300

    Обновил отображение сообщений

--- RevitRooms/ViewModels/InfoElementViewModel.cs
+++ RevitRooms/ViewModels/InfoElementViewModel.cs
@@ -13,14 +13,10 @@ using RevitRooms.Views;
 
 namespace RevitRooms.ViewModels {
     internal class InfoElementViewModel : BaseViewModel {
-        public IElementViewModel<Element> Element { get; set; }
-        public ObservableCollection<InfoMessage> Messages { get; set; }
-
-        public string ImageSource { get; } = "pack://application:,,,/Resources/icons8-show-property-96.png";
-    }
-
-    internal class InfoMessage {
         public string Message { get; set; }
         public TypeInfo TypeInfo { get; set; }
+        public ObservableCollection<IElementViewModel<Element>> Elements { get; set; }
+
+        public string ImageSource { get; } = "pack://application:,,,/Resources/icons8-show-property-96.png";
     }
 }
