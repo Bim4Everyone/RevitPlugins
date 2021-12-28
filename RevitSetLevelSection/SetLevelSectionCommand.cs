7ce2e26c007bb0fe903a47e872970ea1f1243067
dosymep <dosymep@gmail.com>
2021-12-28 16:56:58 +0300

    Добавил основное окно

---
+++ RevitSetLevelSection/SetLevelSectionCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Interop;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using dosymep;
+
+using RevitSetLevelSection.ViewModels;
+using RevitSetLevelSection.Views;
+
+namespace RevitSetLevelSection {
+    [Transaction(TransactionMode.Manual)]
+    public class SetLevelSectionCommand : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            AppDomain.CurrentDomain.AssemblyResolve += AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            try {
+                var viewModel = new MainViewModel();
+
+                var window = new MainWindow() { DataContext = viewModel };
+                new WindowInteropHelper(window) { Owner = commandData.Application.MainWindowHandle };
+
+                window.ShowDialog();
+            } catch(Exception ex) {
+#if D2020 || D2021 || D2022
+                TaskDialog.Show("Назначение уровня/секции.", ex.ToString());
+#else
+                TaskDialog.Show("Назначение уровня/секции.", ex.Message);
+#endif
+            } finally {
+                AppDomain.CurrentDomain.AssemblyResolve -= AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitSetLevelSection/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.WPF.ViewModels;
+
+namespace RevitSetLevelSection.ViewModels {
+    internal class MainViewModel : BaseViewModel {
+    }
+}
