a051ccb9fbd69ced49d5edf586294630924c9ff5
dosymep <dosymep@gmail.com>
2021-12-29 11:40:55 +0300

    Добавил классы по заполнению параметров

---
+++ RevitSetLevelSection/ViewModels/FillLevelParamViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.SharedParams;
+using dosymep.WPF.ViewModels;
+
+using RevitSetLevelSection.Models;
+
+namespace RevitSetLevelSection.ViewModels {
+    internal class FillLevelParamViewModel : BaseViewModel {
+        private readonly RevitRepository _revitRepository;
+
+        public FillLevelParamViewModel(RevitRepository revitRepository) {
+            if(revitRepository is null) {
+                throw new ArgumentNullException(nameof(revitRepository));
+            }
+
+            _revitRepository = revitRepository;
+            RevitParam = SharedParamsConfig.Instance.Level;
+        }
+
+        public RevitParam RevitParam { get; set; }
+        public string Name => $"Заполнить \"{RevitParam.Name}\"";
+    }
+}
---
+++ RevitSetLevelSection/ViewModels/FillParamViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+using dosymep.Bim4Everyone;
+using dosymep.WPF.ViewModels;
+
+using RevitSetLevelSection.Models;
+
+namespace RevitSetLevelSection.ViewModels {
+    internal class FillParamViewModel : BaseViewModel {
+        private DesingOptionsViewModel _desingOptions;
+        private readonly RevitRepository _revitRepository;
+
+        public FillParamViewModel(RevitRepository revitRepository) {
+            if(revitRepository is null) {
+                throw new ArgumentNullException(nameof(revitRepository));
+            }
+
+            _revitRepository = revitRepository;
+        }
+
+        public RevitParam RevitParam { get; set; }
+        public string Name => $"Заполнить \"{RevitParam.Name}\"";
+
+        public DesingOptionsViewModel DesingOptions {
+            get => _desingOptions;
+            set => this.RaiseAndSetIfChanged(ref _desingOptions, value);
+        }
+    }
+}
