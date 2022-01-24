02af6b7aa4333b6a16a83a51c45adf21dc4f0b35
budaevaler <valeria.budaeva@yandex.ru>
2022-01-24 18:43:05 +0300

    Добавлены 2 вида условий

---
+++ RevitLintelPlacement/ViewModels/OpeningWidthConditionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.WPF.ViewModels;
+
+using RevitLintelPlacement.ViewModels.Interfaces;
+
+namespace RevitLintelPlacement.ViewModels {
+    internal class OpeningWidthConditionViewModel : BaseViewModel, IConditionViewModel {
+        private double _minWidth;
+        private double _maxWidth;
+
+        public double MinWidth {
+            get => _minWidth;
+            set => this.RaiseAndSetIfChanged(ref _minWidth, value);
+        }
+
+        public double MaxWidth { 
+            get => _maxWidth; 
+            set => this.RaiseAndSetIfChanged(ref _maxWidth, value); 
+        }
+
+    }
+}
---
+++ RevitLintelPlacement/ViewModels/WallTypesConditionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.WPF.ViewModels;
+
+using RevitLintelPlacement.ViewModels.Interfaces;
+
+namespace RevitLintelPlacement.ViewModels {
+    internal class WallTypesConditionViewModel : BaseViewModel, IConditionViewModel {
+        private ObservableCollection<WallTypeConditionViewModel> _wallTypeCollection;
+
+        ObservableCollection<WallTypeConditionViewModel> WallTypeCollection { 
+            get => _wallTypeCollection; 
+            set => this.RaiseAndSetIfChanged(ref _wallTypeCollection, value); 
+        }
+
+    }
+
+    internal class WallTypeConditionViewModel : BaseViewModel {
+        private bool _isChecked;
+        private string _name;
+
+        public bool IsChecked {
+            get => _isChecked;
+            set => this.RaiseAndSetIfChanged(ref _isChecked, value);
+        }
+
+        public string Name { 
+            get => _name; 
+            set => this.RaiseAndSetIfChanged(ref _name, value); 
+        }
+    }
+
+}
