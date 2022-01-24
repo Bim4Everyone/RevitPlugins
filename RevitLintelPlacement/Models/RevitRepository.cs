8d6a43adb861b83645eefa52baba829753291dba
budaevaler <valeria.budaeva@yandex.ru>
2022-01-24 10:35:40 +0300

    Создание проекта RevitLintelPlacement (плагин для размещения и контроля перемычек)

---
+++ RevitLintelPlacement/Models/RevitRepository.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitLintelPlacement.Models {
+    class RevitRepository {
+        private readonly Application _application;
+        private readonly UIApplication _uiApplication;
+
+        private readonly Document _document;
+        private readonly UIDocument _uiDocument;
+
+        public RevitRepository(Application application, Document document) {
+            _application = application;
+            _uiApplication = new UIApplication(application);
+
+            _document = document;
+            _uiDocument = new UIDocument(document);
+        }
+    }
+}
