d0285e6abf2bdad4cda495d2eaa6488f2b3e2eca
budaevaler <valeria.budaeva@yandex.ru>
2022-01-24 16:04:36 +0300

    Добавление интерфейса IConditionViewModel (абстракция условия)

---
+++ RevitLintelPlacement/ViewModels/ConditionCollectionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.WPF.ViewModels;
+
+using RevitLintelPlacement.ViewModels.Interfaces;
+
+namespace RevitLintelPlacement.ViewModels {
+    internal class ConditionCollectionViewModel : BaseViewModel {
+        private ObservableCollection<IConditionViewModel> _conditionCollection;
+
+        public ObservableCollection<IConditionViewModel> ConditionCollection { 
+            get => _conditionCollection; 
+            set => this.RaiseAndSetIfChanged(ref _conditionCollection, value); 
+        }
+    }
+}
--- RevitLintelPlacement/ViewModels/RuleViewModel.cs
+++ RevitLintelPlacement/ViewModels/RuleViewModel.cs
@@ -10,6 +10,7 @@ namespace RevitLintelPlacement.ViewModels {
     internal class RuleViewModel : BaseViewModel {
         private bool _isChecked;
         private string _name;
+        private ConditionCollectionViewModel _conditionCollection;
 
         public bool IsChecked {
             get => _isChecked;
@@ -17,8 +18,13 @@ namespace RevitLintelPlacement.ViewModels {
         }
 
         public string Name {
-            get => _name; 
+            get => _name;
             set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        public ConditionCollectionViewModel ConditionCollection { 
+            get => _conditionCollection; 
+            set => this.RaiseAndSetIfChanged(ref _conditionCollection, value); 
         }
     }
 }
