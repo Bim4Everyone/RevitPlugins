97ffe68c988ae25b90097a8518078450d89ec5e8
budaevaler <valeria.budaeva@yandex.ru>
2022-01-24 14:25:46 +0300

    Добавлен класс RuleViewModel

--- RevitLintelPlacement/ViewModels/RuleCollectionViewModel.cs
+++ RevitLintelPlacement/ViewModels/RuleCollectionViewModel.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dosymep.WPF.ViewModels;
 
 namespace RevitLintelPlacement.ViewModels {
-    internal class RuleCollectionViewModel {
+    internal class RuleCollectionViewModel : BaseViewModel {
+
+        private ObservableCollection<RuleViewModel> _ruleViewModelCollection;
+        private RuleViewModel _selectedRuleViewModel;
+
+        public ObservableCollection<RuleViewModel> RuleViewModelCollection {
+            get => _ruleViewModelCollection;
+            set => this.RaiseAndSetIfChanged(ref _ruleViewModelCollection, value);
+        }
+
+        public RuleViewModel SelectedRuleViewModel { 
+            get => _selectedRuleViewModel; 
+            set => this.RaiseAndSetIfChanged(ref _selectedRuleViewModel, value); 
+        }
     }
 }
