1bd7d60c5b79cbe8c9b2cf66fb8c436b56cf7b92
budaevaler <valeria.budaeva@yandex.ru>
2022-01-25 12:17:46 +0300

    исправлена команда

--- RevitLintelPlacement/PlaceLintelCommand.cs
+++ RevitLintelPlacement/PlaceLintelCommand.cs
@@ -1,6 +1,12 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using System;
+using System.Windows.Interop;
+
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+
+using dosymep;
+
 using RevitLintelPlacement.ViewModels;
 using RevitLintelPlacement.Views;
 
@@ -9,9 +15,23 @@ namespace RevitLintelPlacement {
     [Transaction(TransactionMode.Manual)]
     public class PlaceLintelCommand : IExternalCommand {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
-            var mainViewModel = new MainViewModel();
-            var view = new MainWindow() { DataContext = mainViewModel };
-            view.ShowDialog();
+            AppDomain.CurrentDomain.AssemblyResolve += AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            try {
+                var mainViewModel = new MainViewModel();
+                var window = new MainWindow() { DataContext = mainViewModel };
+                new WindowInteropHelper(window) { Owner = commandData.Application.MainWindowHandle };
+
+                window.ShowDialog();
+            } catch(Exception ex) {
+#if D2020 || D2021 || D2022
+                TaskDialog.Show("Расстановщик перемычек.", ex.ToString());
+#else
+                TaskDialog.Show("Расстановщик перемычек.", ex.Message);
+#endif
+            } finally {
+                AppDomain.CurrentDomain.AssemblyResolve -= AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            }
+
             return Result.Succeeded;
         }
     }
