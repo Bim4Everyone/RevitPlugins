6f608e74ad584c56517688f4dd32b892ed16079d
budaevaler <valeria.budaeva@yandex.ru>
2022-01-31 19:03:21 +0300

    Начало работы над новым окном правил

---
+++ RevitLintelPlacement/ViewModels/GroupedRuleCollectionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.WPF.ViewModels;
+
+namespace RevitLintelPlacement.ViewModels {
+    internal class GroupedRuleCollectionViewModel : BaseViewModel {
+        private ObservableCollection<GroupedRuleViewModel> _groupedRules;
+
+        public ObservableCollection<GroupedRuleViewModel> GroupedRules {
+            get => _groupedRules; 
+            set => this.RaiseAndSetIfChanged(ref _groupedRules, value);
+        }
+    }
+}
---
+++ RevitLintelPlacement/ViewModels/GroupedRuleViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dosymep.WPF.ViewModels;
+
+namespace RevitLintelPlacement.ViewModels {
+    internal class GroupedRuleViewModel : BaseViewModel {
+        private string _name;
+        private ObservableCollection<ConcreteRuleViewModel> _gruopedRules;
+        private WallTypeConditionViewModel _groupingCondition;
+
+        public string Name {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        public WallTypeConditionViewModel GroupingCondition {
+            get => _groupingCondition;
+            set => this.RaiseAndSetIfChanged(ref _groupingCondition, value);
+        }
+
+        public ObservableCollection<ConcreteRuleViewModel> Rules {
+            get => _gruopedRules;
+            set => this.RaiseAndSetIfChanged(ref _gruopedRules, value);
+        }
+
+
+    }
+
+    internal class ConcreteRuleViewModel : BaseViewModel {
+        private string _selectedLintelType;
+        private OpeningWidthConditionViewModel _openingWidthCondition;
+        private NumberLintelParameterViewModel _offsetParameterViewModel;
+        private List<string> _lintelTypes;
+
+        public string SelectedLintelType { 
+            get => _selectedLintelType; 
+            set => this.RaiseAndSetIfChanged(ref _selectedLintelType, value); 
+        }
+
+
+        public OpeningWidthConditionViewModel OpeningWidthCondition {
+            get => _openingWidthCondition;
+            set => this.RaiseAndSetIfChanged(ref _openingWidthCondition, value);
+        }
+        public NumberLintelParameterViewModel OffsetParameterViewModel {
+            get => _offsetParameterViewModel;
+            set => this.RaiseAndSetIfChanged(ref _offsetParameterViewModel, value);
+        }
+
+        public List<string> LintelTypes {
+            get => _lintelTypes;
+            set => this.RaiseAndSetIfChanged(ref _lintelTypes, value);
+        }
+
+    }
+}
