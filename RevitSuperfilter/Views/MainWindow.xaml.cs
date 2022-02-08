b55209bc73f01c9325ecb676471502465542c52f
dosymep <biseuv@yandex.ru>
2022-02-08 12:01:52 +0300

    Добавил сохранение позиции окна

--- RevitSuperfilter/Views/MainWindow.xaml.cs
+++ RevitSuperfilter/Views/MainWindow.xaml.cs
@@ -13,14 +13,19 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using dosymep.WPF.Views;
+
 namespace RevitSuperfilter.Views {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : PlatformWindow {
         public MainWindow() {
             InitializeComponent();
         }
+
+        public override string PluginName => nameof(RevitSuperfilter);
+        public override string ProjectConfigName => nameof(MainWindow);
 
         private void ButtonOK_Click(object sender, RoutedEventArgs e) {
             DialogResult = true;
