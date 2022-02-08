3105dd3c585bdbc448523c9391369d1303ca0562
dosymep <biseuv@yandex.ru>
2022-02-08 09:59:17 +0300

    Добавил класс с восстановлением позиции окна

---
+++ dosymep/PlatformWindow.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.ProjectConfigs;
+using dosymep.Serializers;
+
+using pyRevitLabs.Json;
+
+namespace dosymep.WPF.Views {
+    public class PlatformWindow : Window {
+        /// <summary>
+        /// Наименование плагина.
+        /// </summary>
+        public virtual string PluginName { get; }
+
+        /// <summary>
+        /// Наименование файла конфигурации.
+        /// </summary>
+        public virtual string ProjectConfigName { get; }
+
+        protected override void OnSourceInitialized(EventArgs e) {
+            base.OnSourceInitialized(e);
+
+            PlatformWindowConfig config = GetProjectConfig();
+            if(config.Maximized.HasValue) {
+                Top = config.Top;
+                Left = config.Left;
+                Width = config.Width;
+                Height = config.Height;
+                WindowState = config.Maximized.Value ? WindowState.Maximized : WindowState;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e) {
+            base.OnClosed(e);
+
+            var config = GetProjectConfig();
+            config.Top = Top;
+            config.Left = Left;
+            config.Width = Width;
+            config.Height = Height;
+            config.Maximized = WindowState == WindowState.Maximized;
+            config.SaveProjectConfig();
+        }
+
+        protected virtual PlatformWindowConfig GetProjectConfig() {
+            return new ProjectConfigBuilder()
+                .SetSerializer(new ConfigSerializer())
+                .SetPluginName(PluginName)
+                .SetRevitVersion(ModuleEnvironment.RevitVersion)
+                .SetProjectConfigName(ProjectConfigName + ".json")
+                .Build<PlatformWindowConfig>();
+        }
+    }
+
+    public class PlatformWindowConfig : ProjectConfig {
+        [JsonIgnore] public override string ProjectConfigPath { get; set; }
+        [JsonIgnore] public override IConfigSerializer Serializer { get; set; }
+
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public bool? Maximized { get; set; }
+    }
+}
