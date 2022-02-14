237400bccf49e9e81910295ccefb55292aca3d7d
dosymep <biseuv@yandex.ru>
2022-02-14 09:28:56 +0300

    Добавил конвертеры в проект

---
+++ dosymep/InverseBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace dosymep.WPF.Converters {
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture) {
+            if(targetType != typeof(bool) && targetType != typeof(bool?))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            bool? b = (bool?) value;
+            return b.HasValue && !b.Value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture) {
+            return !(value as bool?);
+        }
+
+        #endregion
+    }
+}
