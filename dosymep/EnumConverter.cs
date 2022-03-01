6c4d9aec3dc492bd633eff29312bcec8af263d6a
budaevaler <valeria.budaeva@yandex.ru>
2022-03-01 18:00:17 +0300

    Добавлены конверторы для WPF в dosymep

---
+++ dosymep/EnumConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+using dosymep.WPF.Extensions;
+
+namespace dosymep.WPF.Converters {
+    internal class EnumConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value == null || parameter == null) {
+                return string.Empty;
+            }
+            foreach(var one in Enum.GetValues(parameter as Type)) {
+                if(value.Equals(one))
+                    return ((IConvertible) one).GetDescription();
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value == null)
+                return null;
+            foreach(var one in Enum.GetValues(parameter as Type)) {
+                if(value.ToString() == ((IConvertible) one).GetDescription())
+                    return one;
+            }
+            return null;
+        }
+    }
+}
---
+++ dosymep/EnumToItemsSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace dosymep.WPF.Extentions {
+    internal class EnumToItemsSource : MarkupExtension {
+        private readonly Type _type;
+
+        public EnumToItemsSource(Type type) {
+            _type = type;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return _type.GetMembers()
+                .SelectMany(member => member
+                    .GetCustomAttributes(typeof(DescriptionAttribute), true)
+                    .Cast<DescriptionAttribute>())
+                .Select(x => x.Description).ToList();
+        }
+    }
+}
