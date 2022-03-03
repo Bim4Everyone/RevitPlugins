4f29c5cdeef734031d900cd6adf625f4b1960e8b
budaevaler <valeria.budaeva@yandex.ru>
2022-03-03 14:06:39 +0300

    Добавление модели для режимов выборки

---
+++ RevitMarkPlacement/Models/AllElementsSelection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+
+namespace RevitMarkPlacement.Models {
+    internal class AllElementsSelection : ISelectionMode {
+        public IEnumerable<SpotDimension> GetSpotDimentions(Document doc) {
+            return new FilteredElementCollector(doc)
+            .OfClass(typeof(SpotDimension))
+            .Cast<SpotDimension>();
+        }
+    }
+}
---
+++ RevitMarkPlacement/Models/ElementsSelection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitMarkPlacement.Models {
+
+    internal class ElementsSelection : ISelectionMode {
+        public IEnumerable<SpotDimension> GetSpotDimentions(Document doc) {
+            return new UIDocument(doc).Selection.GetElementIds().Any() ?
+                new FilteredElementCollector(doc, new UIDocument(doc).Selection.GetElementIds())
+                .OfClass(typeof(SpotDimension))
+                .Cast<SpotDimension>() : new List<SpotDimension>();
+        }
+    }
+}
