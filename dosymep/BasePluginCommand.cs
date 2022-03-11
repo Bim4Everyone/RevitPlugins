b8614e41c92b91c59bc8546c6a247d6525a22bd9
dosymep <dosymep@gmail.com>
2022-03-11 09:36:42 +0300

    Добавил базовую команду Revit

---
+++ dosymep/BasePluginCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using dosymep;
+using dosymep.Bim4Everyone.SimpleServices;
+using dosymep.SimpleServices;
+
+namespace dosymep.Bim4Everyone {
+    public abstract class BasePluginCommand : IExternalCommand {
+        public BasePluginCommand() {
+            PluginName = GetType().Name;
+        }
+        
+        /// <summary>
+        /// Предоставляет доступ к логгеру платформы.
+        /// </summary>
+        protected ILoggerService PluginLoggerService { get; private set; }
+        
+        /// <summary>
+        /// Предоставляет доступ к логгеру платформы.
+        /// </summary>
+        protected ILoggerService LoggerService => GetPlatformService<ILoggerService>();
+
+
+        /// <inheritdoc />
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            PluginLoggerService = LoggerService.ForPluginContext(PluginName);
+            PluginLoggerService.Information("Запуск команды плагина.");
+            
+            AppDomain.CurrentDomain.AssemblyResolve += AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            try {
+                Execute(commandData.Application);
+                return Result.Succeeded;
+            } finally {
+                PluginLoggerService.Information("Выход из команды плагина.");
+                AppDomain.CurrentDomain.AssemblyResolve -= AppDomainExtensions.CurrentDomain_AssemblyResolve;
+            }
+        }
+
+        /// <summary>
+        /// Наименование плагина для логгера
+        /// </summary>
+        protected string PluginName { get; } 
+
+        /// <summary>
+        /// Метод команды Revit
+        /// </summary>
+        /// <param name="uiApplication">Приложение Revit.</param>
+        protected abstract void Execute(UIApplication uiApplication);
+
+        protected T GetPlatformService<T>() {
+            return ServicesProvider.GetPlatformService<T>();
+        }
+    }
+}
