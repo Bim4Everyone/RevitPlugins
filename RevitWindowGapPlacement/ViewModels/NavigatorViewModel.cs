bbd669206adf9f068d0c8ed84acae661f319d690
dosymep <dosymep@gmail.com>
2022-04-12 10:32:44 +0300

    Добавил свойство для вывода ошибок

--- RevitWindowGapPlacement/ViewModels/NavigatorViewModel.cs
+++ RevitWindowGapPlacement/ViewModels/NavigatorViewModel.cs
@@ -4,10 +4,16 @@ using dosymep.WPF.ViewModels;
 
 namespace RevitWindowGapPlacement.ViewModels {
     internal class NavigatorViewModel : BaseViewModel {
+        private string _errorText;
         private string _windowTitle;
-        
+
         public ICommand PerformWindowCommand { get; }
 
+        public string ErrorText {
+            get => _errorText;
+            set => this.RaiseAndSetIfChanged(ref _errorText, value);
+        }
+        
         public string WindowTitle {
             get => _windowTitle;
             set => this.RaiseAndSetIfChanged(ref _windowTitle, value);
--- RevitWindowGapPlacement/ViewModels/SettingsViewModel.cs
+++ RevitWindowGapPlacement/ViewModels/SettingsViewModel.cs
@@ -4,9 +4,15 @@ using dosymep.WPF.ViewModels;
 
 namespace RevitWindowGapPlacement.ViewModels {
     internal class SettingsViewModel : BaseViewModel {
+        private string _errorText;
         private string _windowTitle;
         
         public ICommand PerformWindowCommand { get; }
+        
+        public string ErrorText {
+            get => _errorText;
+            set => this.RaiseAndSetIfChanged(ref _errorText, value);
+        }
 
         public string WindowTitle {
             get => _windowTitle;
