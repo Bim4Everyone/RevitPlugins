6616bc3060d1af4729829bc56c26cecfd54eda4c
dosymep <dosymep@gmail.com>
2022-04-12 10:25:00 +0300

    Добавил отображение для навигатора отображения проемов окон

---
+++ RevitWindowGapPlacement/ViewModels/NavigatorWindowGapsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+using dosymep.WPF.ViewModels;
+
+namespace RevitWindowGapPlacement.ViewModels {
+    internal class NavigatorWindowGapsViewModel : BaseViewModel {
+        private string _windowTitle;
+        
+        public ICommand PerformWindowCommand { get; }
+
+        public string WindowTitle {
+            get => _windowTitle;
+            set => this.RaiseAndSetIfChanged(ref _windowTitle, value);
+        }
+    }
+}
