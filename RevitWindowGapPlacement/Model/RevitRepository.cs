99d1171fdd23374d0172aa6ca39f94f48bf8b552
dosymep <dosymep@gmail.com>
2022-04-12 16:17:53 +0300

    Добавил репозиторий ревита

---
+++ RevitWindowGapPlacement/Model/RevitRepository.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitWindowGapPlacement.Model {
+    internal class RevitRepository {
+        public RevitRepository(UIApplication uiApplication) {
+            UIApplication = uiApplication;
+        }
+
+        public UIApplication UIApplication { get; }
+
+        public Application Application => UIApplication.Application;
+        
+
+        public Document Document => UIDocument.Document;
+        public UIDocument UIDocument => UIApplication.ActiveUIDocument;
+    }
+}
