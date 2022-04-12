5318a8bb56d3e9f940712c78ff415b060937fef4
dosymep <dosymep@gmail.com>
2022-04-12 10:37:13 +0300

    Добавил дополнительные команды навигатора и настроек

---
+++ RevitWindowGapPlacement/NavigateWindowGapsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Interop;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+
+using dosymep.Bim4Everyone;
+
+using RevitWindowGapPlacement.ViewModels;
+using RevitWindowGapPlacement.Views;
+
+namespace RevitWindowGapPlacement {
+    [Transaction(TransactionMode.Manual)]
+    public class NavigateWindowGapsCommand : BasePluginCommand {
+        public NavigateWindowGapsCommand() {
+            PluginName = "Навигатор проемов окон";
+        }
+
+        protected override void Execute(UIApplication uiApplication) {
+            var window = new MainWindow() {
+                DataContext = new MainViewModel() {
+                    CurrentViewModel = new NavigatorViewModel() {WindowTitle = PluginName}
+                }
+            };
+            
+            var helper = new WindowInteropHelper(window) {Owner = uiApplication.MainWindowHandle};
+
+            window.ShowDialog();
+        }
+    }
+}
