cae3451484380e454f48fba4193bc286c2ecd093
dosymep <dosymep@gmail.com>
2022-04-12 15:33:16 +0300

    Добавил создание дополнительных кнопок

--- RevitWindowGapPlacement/ViewModels/NavigatorViewModel.cs
+++ RevitWindowGapPlacement/ViewModels/NavigatorViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Core;
 
 using dosymep.WPF.ViewModels;
 
@@ -7,16 +11,55 @@ namespace RevitWindowGapPlacement.ViewModels {
         private string _errorText;
         private string _windowTitle;
 
+        public NavigatorViewModel() {
+            AdditionCommands
+                = new ObservableCollection<CommandViewModel>() {
+                    new CommandViewModel() {
+                        ToolTip = "Предыдущий элемент",
+                        ImageSource =
+                            "pack://application:,,,/DevExpress.Images.v21.2;component/Office2013/Arrows/Prev_32x32.png"
+                    },
+                    new CommandViewModel() {
+                        ToolTip = "Следующий элемент",
+                        ImageSource =
+                            "pack://application:,,,/DevExpress.Images.v21.2;component/Office2013/Arrows/Next_32x32.png"
+                    },
+                };
+        }
+
         public ICommand PerformWindowCommand { get; }
+        public ObservableCollection<CommandViewModel> AdditionCommands { get; }
 
         public string ErrorText {
             get => _errorText;
             set => this.RaiseAndSetIfChanged(ref _errorText, value);
         }
-        
+
         public string WindowTitle {
             get => _windowTitle;
             set => this.RaiseAndSetIfChanged(ref _windowTitle, value);
+        }
+    }
+
+
+    internal class CommandViewModel : BaseViewModel {
+        private string _toolTip;
+        private ICommand _command;
+        private string _imageSource;
+
+        public string ToolTip {
+            get => _toolTip;
+            set => this.RaiseAndSetIfChanged(ref _toolTip, value);
+        }
+
+        public ICommand Command {
+            get => _command;
+            set => this.RaiseAndSetIfChanged(ref _command, value);
+        }
+
+        public string ImageSource {
+            get => _imageSource;
+            set => this.RaiseAndSetIfChanged(ref _imageSource, value);
         }
     }
 }
