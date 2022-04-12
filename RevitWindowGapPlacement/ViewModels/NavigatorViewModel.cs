6b33bb4e8484e2077cf705cf9cb7e6050952fa8f
dosymep <dosymep@gmail.com>
2022-04-12 15:33:43 +0300

    Вынес класс кнопок

--- RevitWindowGapPlacement/ViewModels/NavigatorViewModel.cs
+++ RevitWindowGapPlacement/ViewModels/NavigatorViewModel.cs
@@ -40,26 +40,4 @@ namespace RevitWindowGapPlacement.ViewModels {
             set => this.RaiseAndSetIfChanged(ref _windowTitle, value);
         }
     }
-
-
-    internal class CommandViewModel : BaseViewModel {
-        private string _toolTip;
-        private ICommand _command;
-        private string _imageSource;
-
-        public string ToolTip {
-            get => _toolTip;
-            set => this.RaiseAndSetIfChanged(ref _toolTip, value);
-        }
-
-        public ICommand Command {
-            get => _command;
-            set => this.RaiseAndSetIfChanged(ref _command, value);
-        }
-
-        public string ImageSource {
-            get => _imageSource;
-            set => this.RaiseAndSetIfChanged(ref _imageSource, value);
-        }
-    }
 }
