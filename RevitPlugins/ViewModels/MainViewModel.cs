1b7d9c17243e0e26dd6dd3c01382618f3df75b85
dosymep <dosymep@gmail.com>
2022-04-15 15:50:15 +0300

    Обновил проект по умолчанию

---
+++ RevitPlugins/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.UI;
+
+using dosymep.WPF.ViewModels;
+
+using RevitPlugins.Models;
+
+namespace RevitPlugins.ViewModels {
+    internal class MainViewModel : BaseViewModel {
+        private readonly RevitRepository _revitRepository;
+        private string _errorText;
+
+        public MainViewModel(UIApplication uiApplication) {
+            _revitRepository = new RevitRepository(uiApplication);
+        }
+
+        public string ErrorText {
+            get => _errorText;
+            set => this.RaiseAndSetIfChanged(ref _errorText, value);
+        }
+    }
+}
