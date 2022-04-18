d159e8dd1a62a042e9773e4dbf76a4746cda0e2d
dosymep <dosymep@gmail.com>
2022-04-18 14:39:46 +0300

    Переименовал вспомогательные классы получения уровня

--- RevitSetLevelSection/Models/LevelDefinitions/BBPositionBottom.cs
+++ RevitSetLevelSection/Models/LevelDefinitions/BBPositionBottom.cs
@@ -1,7 +1,7 @@
 ﻿using Autodesk.Revit.DB;
 
 namespace RevitSetLevelSection.Models.LevelDefinitions {
-    internal class BBBottom : IBBPosition {
+    internal class BBPositionBottom : IBBPosition {
         public double GetPosition(Outline outline) {
             return outline.MinimumPoint.Z;
         }
--- RevitSetLevelSection/Models/LevelDefinitions/BBPositionMiddle.cs
+++ RevitSetLevelSection/Models/LevelDefinitions/BBPositionMiddle.cs
@@ -1,7 +1,7 @@
 ﻿using Autodesk.Revit.DB;
 
 namespace RevitSetLevelSection.Models.LevelDefinitions {
-    internal class BBMiddle : IBBPosition {
+    internal class BBPositionMiddle : IBBPosition {
         public double GetPosition(Outline outline) {
             return (outline.MaximumPoint.Z - outline.MinimumPoint.Z) / 2 + outline.MinimumPoint.Z;
         }
