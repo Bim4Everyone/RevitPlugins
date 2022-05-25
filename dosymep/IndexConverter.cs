2834dd9b298b811765c5e01051a9aab6ac4e103e
budaevaler <valeria.budaeva@yandex.ru>
2022-05-25 16:52:50 +0300

    Добавлен конвертер для увеличения индекса на единицу

---
+++ dosymep/IndexConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace dosymep.WPF.Converters {
+    internal class IndexConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value is int index) {
+                return index + 1;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if(value is int index) {
+                return index - 1;
+            }
+            return value;
+        }
+    }
+}
