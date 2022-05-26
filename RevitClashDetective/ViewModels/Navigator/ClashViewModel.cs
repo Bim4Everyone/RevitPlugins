77d3fb4c2591eab7be0f146260555574f324fc1e
budaevaler <valeria.budaeva@yandex.ru>
2022-05-26 17:47:06 +0300

    Изменения в навигаторе

--- RevitClashDetective/ViewModels/Navigator/ClashViewModel.cs
+++ RevitClashDetective/ViewModels/Navigator/ClashViewModel.cs
@@ -25,13 +25,16 @@ namespace RevitClashDetective.ViewModels.Navigator {
         public ClashViewModel(ClashModel clash) {
             var levelPropertyName = LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_BASE_LEVEL_PARAM);
 
+            FirstCategory = clash.MainElement.Category?.Name;
             FirstName = clash.MainElement.Name;
             FirstDocumentName = clash.MainElement.Document.Title;
-            FirstLevel = clash.MainElement.IsExistsParam(levelPropertyName) ? clash.MainElement.GetParam(levelPropertyName).AsValueString() : null;
+            FirstLevel = GetLevel(clash.MainElement, levelPropertyName);
 
+            SecondCategory = clash.OtherElement.Category?.Name;
             SecondName = clash.OtherElement.Name;
-            SecondLevel = clash.OtherElement.IsExistsParam(levelPropertyName) ? clash.OtherElement.GetParam(levelPropertyName).AsValueString() : null;
+            SecondLevel = GetLevel(clash.OtherElement, levelPropertyName);
             SecondDocumentName = clash.OtherElement.Document.Title;
+
             _clash = clash;
         }
 
@@ -50,6 +53,8 @@ namespace RevitClashDetective.ViewModels.Navigator {
             set => this.RaiseAndSetIfChanged(ref _firstLevel, value);
         }
 
+        public string FirstCategory { get; set; }
+
         public string SecondName {
             get => _secondName;
             set => this.RaiseAndSetIfChanged(ref _secondName, value);
@@ -65,8 +70,19 @@ namespace RevitClashDetective.ViewModels.Navigator {
             set => this.RaiseAndSetIfChanged(ref _secondDocumentName, value);
         }
 
+        public string SecondCategory { get; set; }
+
         public ElementId GetElementId() {
             return _clash.MainElement.Id;
+        }
+
+        private string GetLevel(Element element, string levelPropertyName) {
+            var level = element.IsExistsParam(levelPropertyName) ? element.GetParam(levelPropertyName).AsValueString() : null;
+            if(level == null) {
+                level = element.LevelId == null ? null : element.Document.GetElement(element.LevelId)?.Name;
+            }
+
+            return level;
         }
     }
 }
