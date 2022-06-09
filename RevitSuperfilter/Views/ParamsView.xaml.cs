354d94858e686feddbba9929e4925dc6d8816aab
Валерия Будаева <Budaeva_v@a101.ru>
2022-06-09 15:12:48 +0300

    Изменения в окне для плагина суперфильтр

--- RevitSuperfilter/Views/ParamsView.xaml.cs
+++ RevitSuperfilter/Views/ParamsView.xaml.cs
@@ -13,13 +13,33 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using DevExpress.Xpf.Core;
+
 namespace RevitSuperfilter.Views {
     /// <summary>
     /// Interaction logic for ParamsView.xaml
     /// </summary>
     public partial class ParamsView : UserControl {
+        private bool _isChecked;
+
         public ParamsView() {
             InitializeComponent();
+        }
+
+        private void SimpleButton_Click(object sender, RoutedEventArgs e) {
+            if(!_isChecked) {
+                _treeView.CheckAllNodes();
+            } else {
+                _treeView.UncheckAllNodes();
+            }
+            
+            _isChecked = _treeView.Nodes.Where(item => item.IsVisible).Any(item => item.IsChecked == true);
+            _sb.Content = _isChecked ? "Cнять выделение" : "Выделить всё";
+        }
+
+        private void _treeView_FilterChanged(object sender, RoutedEventArgs e) {
+            _isChecked = _treeView.Nodes.Where(item=>item.IsVisible).Any(item => item.IsChecked == true);
+            _sb.Content = _isChecked ? "Cнять выделение" : "Выделить всё";
         }
     }
 }
