4608688e928c233a204f3c05eb9038e9c10b8353
Валерия Будаева <Budaeva_v@a101.ru>
2022-06-14 11:03:14 +0300

    Добавлены уведомления в RelayCommand

--- dosymep/RelayCommand.cs
+++ dosymep/RelayCommand.cs
@@ -5,14 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
+using Autodesk.Revit.UI;
+
+using dosymep.Bim4Everyone.SimpleServices;
+using dosymep.SimpleServices;
+
 namespace dosymep.WPF.Commands {
     internal class RelayCommand : ICommand {
         private Action<object> execute;
         private Func<object, bool> canExecute;
 
         public event EventHandler CanExecuteChanged {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
 
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null) {
@@ -25,7 +30,31 @@ namespace dosymep.WPF.Commands {
         }
 
         public void Execute(object parameter) {
-            this.execute(parameter);
+            try {
+                this.execute(parameter);
+            } catch(OperationCanceledException) {
+                GetPlatformService<INotificationService>()
+                    .CreateWarningNotification("C#", "Отмена выполнения команды.")
+                    .ShowAsync();
+            } catch(Autodesk.Revit.Exceptions.OperationCanceledException) {
+                GetPlatformService<INotificationService>()
+                    .CreateWarningNotification("C#", "Отмена выполнения команды.")
+                    .ShowAsync();
+            } catch(Exception ex) {
+                GetPlatformService<INotificationService>()
+                    .CreateFatalNotification("C#", "Ошибка выполнения команды.")
+                    .ShowAsync();
+
+#if D2020 || D2021 || D2022
+                TaskDialog.Show("Ошибка!", ex.ToString());
+#else
+                TaskDialog.Show("Ошибка!", ex.ToString());
+#endif
+            }
+        }
+
+        protected T GetPlatformService<T>() {
+            return ServicesProvider.GetPlatformService<T>();
         }
     }
 }
