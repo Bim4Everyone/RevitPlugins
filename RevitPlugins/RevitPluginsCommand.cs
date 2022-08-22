e15b525d7150a446db6ed4122db844b17ba584f9
dosymep <dosymep@gmail.com>
2022-08-22 18:27:14 +0300

    Переделал проект в тестовый

--- RevitPlugins/RevitPluginsCommand.cs
+++ RevitPlugins/RevitPluginsCommand.cs
@@ -5,30 +5,85 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Interop;
 
+using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-using dosymep;
-using dosymep.Bim4Everyone;
-
-using RevitPlugins.ViewModels;
-using RevitPlugins.Views;
-
 namespace RevitPlugins {
     [Transaction(TransactionMode.Manual)]
-    public class RevitPluginsCommand : BasePluginCommand {
-        public RevitPluginsCommand() {
-            PluginName = "RevitPlugins";
+    public class RevitPluginsCommand : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            Console.WriteLine($"RevitVersion_{commandData.Application.Application.VersionNumber}");
+            return Result.Succeeded;
+        }
+    }
+    
+    [Transaction(TransactionMode.Manual)]
+    public class RevitPluginsCommand1 : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            Console.WriteLine($"RevitVersion_{commandData.Application.Application.VersionNumber}");
+            return Result.Succeeded;
+        }
+    }
+    
+    [Transaction(TransactionMode.Manual)]
+    public class RevitPluginsCommand2 : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            Console.WriteLine($"RevitVersion_{commandData.Application.Application.VersionNumber}");
+            return Result.Succeeded;
+        }
+    }
+    
+    [Transaction(TransactionMode.Manual)]
+    public class RevitPluginsApplication : IExternalApplication {
+        public Result OnStartup(UIControlledApplication application) {
+            Console.WriteLine($"OnStartup: RevitVersion_{application.ControlledApplication.VersionNumber}");
+            return Result.Succeeded;
         }
 
-        protected override void Execute(UIApplication uiApplication) {
-            var viewModel = new MainViewModel(uiApplication);
+        public Result OnShutdown(UIControlledApplication application) {
+            Console.WriteLine($"OnShutdown: RevitVersion_{application.ControlledApplication.VersionNumber}");
+            return Result.Succeeded;
+        }
+    }
+    
+    [Transaction(TransactionMode.Manual)]
+    public class RevitPluginsApplication1 : IExternalApplication {
+        public Result OnStartup(UIControlledApplication application) {
+            Console.WriteLine($"OnStartup: RevitVersion_{application.ControlledApplication.VersionNumber}");
+            return Result.Succeeded;
+        }
 
-            var window = new MainWindow() { Title = PluginName, DataContext = viewModel};
-            var helper = new WindowInteropHelper(window) {Owner = uiApplication.MainWindowHandle};
+        public Result OnShutdown(UIControlledApplication application) {
+            Console.WriteLine($"OnShutdown: RevitVersion_{application.ControlledApplication.VersionNumber}");
+            return Result.Succeeded;
+        }
+    }
+    
+    [Transaction(TransactionMode.Manual)]
+    public class RevitPluginsApplicationDB : IExternalDBApplication {
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application) {
+            Console.WriteLine($"OnStartup: RevitVersion_{application.VersionNumber}");
+            return ExternalDBApplicationResult.Succeeded;
+        }
 
-            window.ShowDialog();
+        public ExternalDBApplicationResult OnShutdown(ControlledApplication application) {
+            Console.WriteLine($"OnShutdown: RevitVersion_{application.VersionNumber}");
+            return ExternalDBApplicationResult.Succeeded;
+        }
+    }
+    
+    [Transaction(TransactionMode.Manual)]
+    public class RevitPluginsApplicationDB1 : IExternalDBApplication {
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application) {
+            Console.WriteLine($"OnStartup: RevitVersion_{application.VersionNumber}");
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        public ExternalDBApplicationResult OnShutdown(ControlledApplication application) {
+            Console.WriteLine($"OnShutdown: RevitVersion_{application.VersionNumber}");
+            return ExternalDBApplicationResult.Succeeded;
         }
     }
 }
