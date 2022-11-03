2db458818b1678eb17ca87a6d68be4296700b36d
Валерия Будаева <Budaeva_v@a101.ru>
2022-11-03 15:43:53 +0300

    Добавлен модуль нахождения точки вставки задания на отверстие в перекрыти

---
+++ RevitOpeningPlacement/Models/OpeningPlacement/PointFinders/FloorPointFinder.cs
@@ -0,0 +1,24 @@
+﻿
+using Autodesk.Revit.DB;
+
+using RevitOpeningPlacement.Models.Extensions;
+using RevitOpeningPlacement.Models.Interfaces;
+
+namespace RevitOpeningPlacement.Models.OpeningPlacement.PointFinders {
+    internal class FloorPointFinder : IPointFinder {
+        private readonly MepCurveClash<CeilingAndFloor> _clash;
+
+        public FloorPointFinder(MepCurveClash<CeilingAndFloor> clash) {
+            _clash = clash;
+        }
+
+        public XYZ GetPoint() {
+            var topFace = (PlanarFace) _clash.Element.GetTopFace();
+            var topPlane = Plane.CreateByNormalAndOrigin(topFace.FaceNormal, topFace.Origin);
+            var transformedTopPlane = _clash.ElementTransform.OfPlane(topPlane);
+            var topPoint = _clash.Curve.GetLine().GetIntersectionWithPlane(transformedTopPlane);
+
+            return topPoint;
+        }
+    }
+}
