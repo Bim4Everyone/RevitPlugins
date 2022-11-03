6128ac56498710312513ea24b3607b398546ebe2
Валерия Будаева <Budaeva_v@a101.ru>
2022-11-03 14:56:44 +0300

    Добавлено получение верхней и нижней граней перекрытия

---
+++ RevitOpeningPlacement/Models/Extensions/CeilingAndFloorExtension.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.Revit.DB;
+
+namespace RevitOpeningPlacement.Models.Extensions {
+    internal static class CeilingAndFloorExtension {
+        public static Face GetTopFace(this CeilingAndFloor ceilingAndFloor) {
+            var faceRefs = HostObjectUtils.GetTopFaces(ceilingAndFloor);
+            return (Face) ceilingAndFloor.GetGeometryObjectFromReference(faceRefs[0]);
+        }
+
+        public static Face GetBottomFace(this CeilingAndFloor ceilingAndFloor) {
+            var faceRefs = HostObjectUtils.GetBottomFaces(ceilingAndFloor);
+            return (Face) ceilingAndFloor.GetGeometryObjectFromReference(faceRefs[0]);
+        }
+    }
+}
