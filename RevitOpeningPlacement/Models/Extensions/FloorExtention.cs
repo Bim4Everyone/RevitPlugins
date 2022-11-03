009fca9059b94d66bff7501213ec3965c9d05089
Валерия Будаева <Budaeva_v@a101.ru>
2022-11-03 14:57:05 +0300

    Добавлено получение толщины перекрытия

---
+++ RevitOpeningPlacement/Models/Extensions/FloorExtention.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+
+using dosymep.Revit;
+
+namespace RevitOpeningPlacement.Models.Extensions {
+    internal static class FloorExtention {
+        public static List<BuiltInParameter> _thicknessParameters = new List<BuiltInParameter> {
+            BuiltInParameter.FLOOR_ATTR_THICKNESS_PARAM,
+            BuiltInParameter.FLOOR_ATTR_DEFAULT_THICKNESS_PARAM,
+            BuiltInParameter.CEILING_THICKNESS,
+        };
+
+        public static double GetThickness(this Element element) {
+            foreach(var param in _thicknessParameters) {
+                if(element.IsExistsParam(param)) {
+                    return element.GetParamValueOrDefault<double>(param);
+                }
+            }
+
+            var type = element.GetTypeId().IsNotNull() ? element.Document.GetElement(element.GetTypeId()) : null;
+            if(type != null) {
+                return element.GetThickness();
+            }
+
+            return 0;
+        }
+    }
+}
