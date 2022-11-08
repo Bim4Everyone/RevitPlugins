0649b242cdb2ccb20e9cb7cf1e34732124f471b1
Валерия Будаева <Budaeva_v@a101.ru>
2022-11-08 13:54:43 +0300

    Добавлен класс возвращающий нулевой угол поворота

---
+++ RevitOpeningPlacement/Models/OpeningPlacement/AngleFinders/ZeroAngleFinder.cs
@@ -0,0 +1,9 @@
+﻿using RevitOpeningPlacement.Models.Interfaces;
+
+namespace RevitOpeningPlacement.Models.OpeningPlacement.AngleFinders {
+    internal class ZeroAngleFinder : IAngleFinder {
+        public Rotates GetAngle() {
+            return new Rotates(0, 0, 0);
+        }
+    }
+}
