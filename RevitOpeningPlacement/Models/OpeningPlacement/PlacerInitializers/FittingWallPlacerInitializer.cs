38c0ab8f9670fccd6ec6ff374fa890df44d8e7d5
Валерия Будаева <Budaeva_v@a101.ru>
2022-12-01 16:00:39 +0300

    Изменено получение точки вставки для задания на отверстие соединительной детали

--- RevitOpeningPlacement/Models/OpeningPlacement/PlacerInitializers/FittingWallPlacerInitializer.cs
+++ RevitOpeningPlacement/Models/OpeningPlacement/PlacerInitializers/FittingWallPlacerInitializer.cs
@@ -10,18 +10,22 @@ using RevitOpeningPlacement.Models.OpeningPlacement.LevelFinders;
 using RevitOpeningPlacement.Models.OpeningPlacement.ParameterGetters;
 using RevitOpeningPlacement.Models.OpeningPlacement.PointFinders;
 using RevitOpeningPlacement.Models.OpeningPlacement.SolidProviders;
+using RevitOpeningPlacement.Models.OpeningPlacement.ValueGetters;
 
 namespace RevitOpeningPlacement.Models.OpeningPlacement.PlacerInitializers {
     internal class FittingWallPlacerInitializer : IFittingPlacerInitializer {
         public OpeningPlacer GetPlacer(RevitRepository revitRepository, ClashModel clashModel, params MepCategory[] categoryOptions) {
             var clash = new FittingClash<Wall>(revitRepository, clashModel);
             var angleFinder = new WallAngleFinder(clash.Element2, clash.Element2Transform);
+            var solidProvider = new FittingClashSolidProvider<Wall>(clash, angleFinder);
+            var heightGetter = new WallOpeningSizeInitializer(solidProvider.GetSolid(), categoryOptions).GetHeight();
+
             return new OpeningPlacer(revitRepository) {
                 Type = revitRepository.GetOpeningType(OpeningType.WallRectangle),
                 LevelFinder = new ClashLevelFinder(revitRepository, clashModel),
                 AngleFinder = angleFinder,
-                ParameterGetter = new WallSolidParameterGetter(new FittingClashSolidProvider<Wall>(clash, angleFinder), categoryOptions),
-                PointFinder = new FittingWallPointFinder(clash, angleFinder)
+                ParameterGetter = new WallSolidParameterGetter(solidProvider, categoryOptions),
+                PointFinder = new FittingWallPointFinder(clash, angleFinder, heightGetter)
             };
         }
     }
--- RevitOpeningPlacement/Models/OpeningPlacement/PointFinders/FittingWallPointFinder.cs
+++ RevitOpeningPlacement/Models/OpeningPlacement/PointFinders/FittingWallPointFinder.cs
@@ -4,6 +4,7 @@ using Autodesk.Revit.DB;
 using dosymep.Revit;
 
 using RevitClashDetective.Models.Extensions;
+using RevitClashDetective.Models.Value;
 
 using RevitOpeningPlacement.Models.Extensions;
 using RevitOpeningPlacement.Models.Interfaces;
@@ -13,22 +14,28 @@ namespace RevitOpeningPlacement.Models.OpeningPlacement.PointFinders {
     internal class FittingWallPointFinder : IPointFinder {
         private readonly FittingClash<Wall> _clash;
         private readonly IAngleFinder _angleFinder;
+        private readonly IValueGetter<DoubleParamValue> _sizeGetter;
 
-        public FittingWallPointFinder(FittingClash<Wall> clash, IAngleFinder angleFinder) {
+        public FittingWallPointFinder(FittingClash<Wall> clash, IAngleFinder angleFinder, IValueGetter<DoubleParamValue> sizeGetter = null) {
             _clash = clash;
             _angleFinder = angleFinder;
+            _sizeGetter = sizeGetter;
         }
 
         public XYZ GetPoint() {
-            //получение z-координаты точки вставки
-            var solid = _clash.GetIntersection();
-            var z = solid.GetOutline().MinimumPoint.Z;
-
             //получение центра пересечения по BoundingBox-у
             var transformedSolid = _clash.GetRotatedIntersection(_angleFinder);
             var outline = transformedSolid.GetOutline();
             var transformedCenter = outline.MinimumPoint + (outline.MaximumPoint - outline.MinimumPoint) / 2;
             var center = Transform.Identity.GetRotationMatrixAroundZ(_angleFinder.GetAngle().Z).OfPoint(transformedCenter);
+
+            //получение z-координаты точки вставки
+            var solid = _clash.GetIntersection();
+            var z = solid.GetOutline().MinimumPoint.Z;
+
+            if(_sizeGetter != null) {
+                z = center.Z - _sizeGetter.GetValue().TValue / 2;
+            }
 
             //проекция центра на грань стены
             var sizeInit = new WallOpeningSizeInitializer(transformedSolid);
