36ee02112d52e0f076b9e5cfdd54d947c130ca50
dosymep <dosymep@gmail.com>
2022-12-05 17:33:19 +0300

    Обновил код компиляции

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,87 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
+
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-class Build : NukeBuild
-{
+class Build : NukeBuild {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    [Parameter("Output")] readonly string Output = RootDirectory / "bin";
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Parameter("PluginName")] readonly string PluginName;
+
+    // [GitVersion] readonly GitVersion GitVersion;
+
+    readonly IEnumerable<RevitConfiguration> Configurations = IsLocalBuild
+        ? RevitConfiguration.GetDebugConfiguration()
+        : RevitConfiguration.GetReleaseConfiguration();
+
+    AbsolutePath SourceDirectory => RootDirectory / PluginName;
 
     Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
-
-    Target Restore => _ => _
-        .Executes(() =>
-        {
+        .Requires(() => PluginName)
+        .Executes(() => {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
+        .DependsOn(Clean)
+        .Requires(() => Output)
+        .Requires(() => PluginName)
+        .Executes(() => {
+            DotNetBuild(s => s
+                .SetProjectFile(PluginName)
+                .EnableNoRestore()
+                .SetOutputDirectory(Output)
+                .CombineWith(Configurations, (settings, config) => s
+                    .SetConfiguration(config)));
         });
-
+    
+    // Target Compile => _ => _
+    //     .DependsOn(Restore)
+    //     .Requires(() => PluginName)
+    //     .Executes(() => {
+    //         if(string.IsNullOrEmpty(OutputPath)) {
+    //             throw new ArgumentNullException(nameof(OutputPath));
+    //         }
+    //
+    //         foreach(var configuration in Configurations) {
+    //             var assemblySemVer = UpdateMajorVersion(configuration, GitVersion.AssemblySemVer);
+    //             var assemblySemFileVer = UpdateMajorVersion(configuration, GitVersion.AssemblySemFileVer);
+    //             var informationalVersion = UpdateMajorVersion(configuration, GitVersion.InformationalVersion);
+    //         
+    //             DotNetBuild(s => s
+    //                 .SetProjectFile(PluginName)
+    //                 .SetAssemblyVersion(assemblySemVer)
+    //                 .SetFileVersion(assemblySemFileVer)
+    //                 .SetInformationalVersion(informationalVersion)
+    //                 .SetConfiguration(configuration)
+    //                 .DisableNoRestore()
+    //                 .SetOutputDirectory(OutputPath));
+    //         }
+    //     });
+    //
+    // string UpdateMajorVersion(RevitConfiguration configuration, string versionString) {
+    //     var index = versionString.IndexOf('.');
+    //     return configuration.Version + versionString.Substring(index);
+    // }
 }
---
+++ build/RevitConfiguration.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Nuke.Common.Tooling;
+
+[TypeConverter(typeof(TypeConverter<RevitConfiguration>))]
+class RevitConfiguration : Enumeration {
+    static RevitConfiguration D2020 = new RevitConfiguration {Value = nameof(D2020), Version = 2020};
+    static RevitConfiguration D2021 = new RevitConfiguration {Value = nameof(D2021), Version = 2021};
+
+    static RevitConfiguration D2022 = new RevitConfiguration {Value = nameof(D2022), Version = 2022};
+    // static RevitConfiguration D2023 => new RevitConfiguration {Value = nameof(D2023), Version = 2023};
+
+    static RevitConfiguration R2020 = new RevitConfiguration {Value = nameof(R2020), Version = 2020};
+    static RevitConfiguration R2021 = new RevitConfiguration {Value = nameof(R2021), Version = 2021};
+
+    static RevitConfiguration R2022 = new RevitConfiguration {Value = nameof(R2022), Version = 2022};
+    // static RevitConfiguration R2023 => new RevitConfiguration {Value = nameof(R2023), Version = 2023};
+
+    public int Version { get; protected set; }
+
+    public static implicit operator string(RevitConfiguration configuration) {
+        return configuration.Value;
+    }
+
+    public static IEnumerable<RevitConfiguration> GetDebugConfiguration() {
+        return typeof(RevitConfiguration).GetFields()
+            .Where(item => item.Name.StartsWith("D"))
+            .Select(item => item.GetValue(null))
+            .OfType<RevitConfiguration>();
+    }
+
+    public static IEnumerable<RevitConfiguration> GetReleaseConfiguration() {
+        return typeof(RevitConfiguration).GetFields()
+            .Where(item => item.Name.StartsWith("R"))
+            .Select(item => item.GetValue(null))
+            .OfType<RevitConfiguration>();
+    }
+}
