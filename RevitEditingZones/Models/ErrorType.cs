93151b3bb93d526a94def285f1d40bbc938abf28
dosymep <dosymep@gmail.com>
2023-02-28 17:34:50 +0300

    Вынес конвертер получения дочерних строк у группировки

---
+++ RevitEditingZones/Models/ErrorType.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+
+namespace RevitEditingZones.Models {
+    internal class ErrorType : IEquatable<ErrorType>, IComparable<ErrorType>, IComparable {
+        public ErrorType(int id) {
+            Id = id;
+        }
+
+        public int Id { get; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        #region IEquatable<ErrorType>
+
+        public bool Equals(ErrorType other) {
+            if(ReferenceEquals(null, other)) {
+                return false;
+            }
+
+            if(ReferenceEquals(this, other)) {
+                return true;
+            }
+
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj) {
+            if(ReferenceEquals(null, obj)) {
+                return false;
+            }
+
+            if(ReferenceEquals(this, obj)) {
+                return true;
+            }
+
+            if(obj.GetType() != GetType()) {
+                return false;
+            }
+
+            return Equals((ErrorType) obj);
+        }
+
+        public override int GetHashCode() {
+            return Id;
+        }
+
+        public static bool operator ==(ErrorType left, ErrorType right) {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ErrorType left, ErrorType right) {
+            return !Equals(left, right);
+        }
+
+        #endregion
+
+        #region IComparable<ErrorType>
+
+        public int CompareTo(ErrorType other) {
+            if(ReferenceEquals(this, other)) {
+                return 0;
+            }
+
+            if(ReferenceEquals(null, other)) {
+                return 1;
+            }
+
+            return Id.CompareTo(other.Id);
+        }
+
+        public int CompareTo(object obj) {
+            return CompareTo(obj as ErrorType);
+        }
+
+        #endregion
+
+        public override string ToString() {
+            return Name;
+        }
+    }
+}
---
+++ dosymep/GroupRowChildrenValuesConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+using DevExpress.Xpf.Grid;
+using DevExpress.XtraSpreadsheet.Utils;
+
+namespace dosymep.WPF.Converters {
+    internal class GroupRowChildrenValuesConverter : IValueConverter, IMultiValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            GroupRowData rowData = value as GroupRowData;
+            if(rowData?.View.DataControl is GridControl gridControl) {
+                Array array = Enumerable.Range(0, gridControl.GetChildRowCount(rowData.RowHandle.Value))
+                    .Select(item => gridControl.GetChildRowHandle(rowData.RowHandle.Value, item))
+                    .Select(item => gridControl.GetRow(item))
+                    .ToArray();
+
+                return CreateTypedArray(array, targetType);
+            }
+
+            return CreateTypedArray(Array.Empty<object>(), targetType);
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            var groupRow = values.OfType<GroupRowData>().FirstOrDefault();
+            return Convert(groupRow, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        public object CreateTypedArray(Array array, Type arrayType) {
+            if(arrayType == null
+               || arrayType == typeof(object)) {
+                return array;
+            }
+
+            var newArray = Array.CreateInstance(arrayType, array.Length);
+            array.CopyTo(newArray, default);
+            return newArray;
+        }
+    }
+}
