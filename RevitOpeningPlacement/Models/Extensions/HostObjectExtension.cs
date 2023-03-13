d394387188bb92844525bb2f80775eab734032a5
Vladimir Stroganov <vlastroganov@gmail.com>
2023-03-13 09:34:37 +0300

    Получение солида полого семейства

---
+++ RevitOpeningPlacement/Models/Extensions/HostObjectExtension.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+using DevExpress.CodeParser;
+
+namespace RevitOpeningPlacement.Models.Extensions {
+    /// <summary>
+    /// Методы расширения для классов - наследников <see cref="Autodesk.Revit.DB.HostObject"/>
+    /// </summary>
+    internal static class HostObjectExtension {
+        #region Getting HostObject OriginalSolid
+        /// <summary>
+        /// Возвращает "оригинальный" Solid элемента, являющегося <see cref="Autodesk.Revit.DB.HostObject">основой</see> без вырезаний.
+        /// Реализовано для элементов следующих типов: Wall | Floor | Ceiling | RoofBase
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Исключение, если <see cref="GetHostBoundCurveLoops"/> возвращает список не из 2 петель кривых</exception>
+        public static Solid GetHostElementOriginalSolid(this HostObject hostObject) {
+            IList<CurveLoop> loops = GetHostBoundCurveLoops(hostObject);
+            if(loops.Count != 2) {
+                throw new ArgumentException($"{nameof(loops)} contains {loops.Count} loops, expected 2.");
+            }
+            return GeometryCreationUtilities.CreateBlendGeometry(
+                loops[0],
+                loops[1],
+                null);
+        }
+
+        /// <summary>
+        /// Возвращает список, содержащий контуры линий 2-х граничных поверхностей геометрии <see cref="Autodesk.Revit.DB.HostObject">хоста</see>
+        /// </summary>
+        /// <returns></returns>
+        private static IList<CurveLoop> GetHostBoundCurveLoops(this HostObject hostObject) {
+            return GetHostBoundFaces(hostObject)
+                .Select(face => face
+                    .GetEdgesAsCurveLoops()
+                    .OrderByDescending(loop => loop.GetExactLength())
+                    .FirstOrDefault())
+                .ToList();
+        }
+
+        /// <summary>
+        /// Возвращает список, содержащий нижнюю и верхнюю поверхности <see cref="Autodesk.Revit.DB.HostObject">хоста</see>, 
+        /// если хост - "плитный" элемент, или содержащий переднюю и заднюю поверхности, если хост - стена.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Исключение, если хост элемента не Стена | Потолок | Перекрытие | Крыша</exception>
+        private static IList<Face> GetHostBoundFaces(this HostObject hostObject) {
+            if(hostObject is Wall hostWall) {
+                return hostWall.GetFaces().ToArray();
+            } else if(hostObject is Floor hostFloor) {
+                return new List<Face> {
+                    hostFloor.GetBottomFace(),
+                    hostFloor.GetTopFace()
+                };
+            } else if(hostObject is Ceiling hostCeiling) {
+                return new List<Face> {
+                    hostCeiling.GetBottomFace(),
+                    hostCeiling.GetTopFace()
+                };
+            } else if(hostObject is RoofBase hostRoofBase) {
+                return new List<Face> {
+                    hostRoofBase.GetBottomFace(),
+                    hostRoofBase.GetTopFace()
+                };
+            } else {
+                throw new ArgumentException($"{nameof(hostObject)} is {hostObject.GetType().Name}, expected: Wall | Floor | Ceiling | RoofBase ");
+            }
+        }
+        #endregion
+    }
+}
--- RevitOpeningPlacement/Models/OpeningIntersection/OpeningReal.cs
+++ RevitOpeningPlacement/Models/OpeningIntersection/OpeningReal.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 using Autodesk.Revit.DB;
 
+using dosymep.Revit.Geometry;
+
+using RevitClashDetective.Models.Extensions;
+
+using RevitOpeningPlacement.Models.Extensions;
+
 namespace RevitOpeningPlacement.Models.OpeningIntersection {
     /// <summary>
     /// Класс - обертка "чистовых" экземпляров семейств, которые проектировщик размещает в местах расположений экземпляров семейств заданий на отверстия
@@ -19,7 +25,30 @@ namespace RevitOpeningPlacement.Models.OpeningIntersection {
         /// </summary>
         /// <returns></returns>
         public override Solid GetSolid() {
-            return base.GetSolid();
+            XYZ openingLocation = (_element.Location as LocationPoint).Point;
+            var hostElement = GetHost();
+            Solid hostSolidCut = hostElement.GetSolid();
+            Solid hostSolidOriginal = (hostElement as HostObject).GetHostElementOriginalSolid();
+            var openings = SolidUtils.SplitVolumes(BooleanOperationsUtils.ExecuteBooleanOperation(hostSolidOriginal, hostSolidCut, BooleanOperationsType.Difference));
+            var thisOpeningSolid = openings.OrderBy(solidOpening => (solidOpening.ComputeCentroid() - openingLocation).GetLength()).FirstOrDefault();
+            if(thisOpeningSolid != null) {
+                return thisOpeningSolid;
+            } else {
+                return GetTransformedBBoxXYZ().CreateSolid();
+            }
+        }
+
+        /// <summary>
+        /// Возвращает хост экземпляра семейства <see cref="OpeningBase._element">отверстия</see>
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException"></exception>
+        private Element GetHost() {
+            var host = _element.Host;
+            if(host is null) {
+                throw new NullReferenceException($"Host of element with id: {_element.Id} returned null");
+            }
+            return host;
         }
     }
 }
