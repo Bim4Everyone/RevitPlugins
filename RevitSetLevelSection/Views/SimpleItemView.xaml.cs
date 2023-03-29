979014c5c86849069c9628ff3238e31e34e6fd33
dosymep <dosymep@gmail.com>
2023-03-29 15:58:58 +0300

    Вынес отдельный Layout для элементы окна

---
+++ RevitSetLevelSection/Views/SimpleItemView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RevitSetLevelSection.Views {
+    public partial class SimpleItemView {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            nameof(Title), typeof(object), 
+            typeof(SimpleItemView), new PropertyMetadata(default(object)));
+
+        public static readonly DependencyProperty RightProperty = DependencyProperty.Register(
+            nameof(Right), typeof(object), 
+            typeof(SimpleItemView), new PropertyMetadata(default(object)));
+
+        public SimpleItemView() {
+            InitializeComponent();
+        }
+        
+        public object Title {
+            get => GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+        
+        public object Right {
+            get => GetValue(RightProperty);
+            set => SetValue(RightProperty, value);
+        }
+    }
+}
