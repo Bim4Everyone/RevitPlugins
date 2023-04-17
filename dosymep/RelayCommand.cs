01a6a6b9c8cc2c4dfd1560180b578d7836c28b4a
dosymep <dosymep@gmail.com>
2023-04-17 11:20:09 +0300

    Добавил AsyncRelayCommand и AsyncRelayCommand<T>

--- dosymep/RelayCommand.cs
+++ dosymep/RelayCommand.cs
@@ -10,6 +10,14 @@ namespace dosymep.WPF.Commands {
         public RelayCommand<T> Create<T>(Action<T> execute, Func<T, bool> canExecute = null) {
             return new RelayCommand<T>(execute, canExecute);
         }
+        
+        public AsyncRelayCommand CreateAsync(Func<Task> execute, Func<bool> canExecute = null) {
+            return new AsyncRelayCommand(p => execute(), canExecute == null ? (Func<object, bool>) null : p => canExecute());
+        }
+
+        public AsyncRelayCommand<T> CreateAsync<T>(Func<T, Task> execute, Func<T, bool> canExecute = null) {
+            return new AsyncRelayCommand<T>(execute, canExecute);
+        }
 
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
             : base(execute, canExecute) {
