ff8dd25e573a116d291908af9b699517a8996933
dosymep <dosymep@gmail.com>
2023-04-19 16:59:05 +0300

    Сделал методы статическими

--- dosymep/RelayCommand.cs
+++ dosymep/RelayCommand.cs
@@ -3,19 +3,19 @@ using System.Threading.Tasks;
 
 namespace dosymep.WPF.Commands {
     internal class RelayCommand : RelayCommand<object> {
-        public RelayCommand Create(Action execute, Func<bool> canExecute = null) {
+        public static RelayCommand Create(Action execute, Func<bool> canExecute = null) {
             return new RelayCommand(p => execute(), canExecute == null ? (Func<object, bool>) null : p => canExecute());
         }
 
-        public RelayCommand<T> Create<T>(Action<T> execute, Func<T, bool> canExecute = null) {
+        public static RelayCommand<T> Create<T>(Action<T> execute, Func<T, bool> canExecute = null) {
             return new RelayCommand<T>(execute, canExecute);
         }
         
-        public AsyncRelayCommand CreateAsync(Func<Task> execute, Func<bool> canExecute = null) {
+        public static AsyncRelayCommand CreateAsync(Func<Task> execute, Func<bool> canExecute = null) {
             return new AsyncRelayCommand(p => execute(), canExecute == null ? (Func<object, bool>) null : p => canExecute());
         }
 
-        public AsyncRelayCommand<T> CreateAsync<T>(Func<T, Task> execute, Func<T, bool> canExecute = null) {
+        public static AsyncRelayCommand<T> CreateAsync<T>(Func<T, Task> execute, Func<T, bool> canExecute = null) {
             return new AsyncRelayCommand<T>(execute, canExecute);
         }
 
