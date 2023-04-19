ec8b344032ab7a636fa6e9fa2a130c13645ad1d8
dosymep <dosymep@gmail.com>
2023-04-19 11:38:22 +0300

    Добавил новое окно

---
+++ PlatformSettings/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+
+using dosymep.WPF.ViewModels;
+
+namespace PlatformSettings.ViewModels {
+    internal class MainViewModel : BaseViewModel {
+        private string _errorText;
+        
+        
+        public ICommand LoadViewCommand { get; }
+
+        public string ErrorText {
+            get => _errorText;
+            set => this.RaiseAndSetIfChanged(ref _errorText, value);
+        }
+    }
+}
---
+++ PlatformSettings/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace PlatformSettings.Views {
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        
+        public override string PluginName => nameof(PlatformSettings);
+        public override string ProjectConfigName => nameof(MainWindow);
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
+            DialogResult = false;
+        }
+    }
+}
