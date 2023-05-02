9c417f168dcbff85644ce104445611be9cbad7ae
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-02 16:51:20 +0300

    Добавление метода округления координаты точки

--- RevitOpeningPlacement/Models/Extensions/XYZExtension.cs
+++ RevitOpeningPlacement/Models/Extensions/XYZExtension.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Autodesk.Revit.DB;
 
 namespace RevitOpeningPlacement.Models.Extensions {
     internal static class XYZExtension {
+        /// <summary>
+        /// Значение округления координат в мм
+        /// </summary>
+        private static readonly int _mmRound = 5;
+
+
         internal static bool IsPapallel(this XYZ vector1, XYZ vector2) {
             return Math.Abs(Math.Abs(Math.Cos(vector1.AngleTo(vector2))) - 1) < 0.0001;
         }
@@ -32,6 +34,32 @@ namespace RevitOpeningPlacement.Models.Extensions {
             var plane = wall.GetHorizontalNormalPlane();
             var wallLine = wall.GetLine();
             return plane.ProjectVector(direction).IsPapallel(plane.ProjectVector(wallLine.Direction));
+        }
+
+        /// <summary>
+        /// Возвращает точку с округленными до 5 мм координатами
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static XYZ Round(this XYZ value) {
+            var x = RoundCoordinate(value.X);
+            var y = RoundCoordinate(value.Y);
+            var z = RoundCoordinate(value.Z);
+            return new XYZ(x, y, z);
+        }
+
+        /// <summary>
+        /// Округляет значение координаты точки на прямой до <see cref="_mmRound">заданного количества мм</see>
+        /// </summary>
+        /// <param name="coordinate"></param>
+        /// <returns></returns>
+        private static double RoundCoordinate(double coordinate) {
+#if REVIT_2020_OR_LESS
+            double roundFeet = UnitUtils.ConvertToInternalUnits(_mmRound, DisplayUnitType.DUT_MILLIMETERS);
+#else
+            double roundFeet = UnitUtils.ConvertToInternalUnits(_mmRound, UnitTypeId.Millimeters);
+#endif
+            return Math.Round(coordinate / roundFeet) * roundFeet;
         }
     }
 }
