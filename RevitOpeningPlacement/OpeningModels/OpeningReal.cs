a790eba086cca919505ad484a4e9ef4947984223
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-02 14:26:40 +0300

    Добавить класс для обертки "чистового" отверстия

---
+++ RevitOpeningPlacement/OpeningModels/OpeningReal.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+
+using dosymep.Revit;
+using dosymep.Revit.Geometry;
+
+using RevitClashDetective.Models.Extensions;
+
+using RevitOpeningPlacement.Models.Extensions;
+using RevitOpeningPlacement.Models.Interfaces;
+using RevitOpeningPlacement.OpeningModels.Enums;
+
+namespace RevitOpeningPlacement.OpeningModels {
+    /// <summary>
+    /// Класс, обозначающий чистовое отверстие, идущее на чертежи
+    /// </summary>
+    internal class OpeningReal : ISolidProvider {
+        /// <summary>
+        /// Экземпляр семейства чистового отверстия
+        /// </summary>
+        private readonly FamilyInstance _familyInstance;
+
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="OpeningReal"/>
+        /// </summary>
+        /// <param name="openingReal">Экземпляр семейства чистового отверстия, идущего на чертежи</param>
+        public OpeningReal(FamilyInstance openingReal) {
+            _familyInstance = openingReal;
+            Id = _familyInstance.Id.IntegerValue;
+        }
+
+
+        /// <summary>
+        /// Id экземпляра семейства задания на отверстие
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// Точка расположения экземпляра семейства задания на отверстие
+        /// </summary>
+        public XYZ Location { get; private set; }
+
+        /// <summary>
+        /// Статус текущего отверстия относительно полученных заданий
+        /// </summary>
+        public OpeningRealTaskStatusEnum Status { get; set; } = OpeningRealTaskStatusEnum.NotDefined;
+
+
+        public Solid GetSolid() {
+            XYZ openingLocation = (_familyInstance.Location as LocationPoint).Point;
+            var hostElement = GetHost();
+            Solid hostSolidCut = hostElement.GetSolid();
+            Solid hostSolidOriginal = (hostElement as HostObject).GetHostElementOriginalSolid();
+            var openings = SolidUtils.SplitVolumes(BooleanOperationsUtils.ExecuteBooleanOperation(hostSolidOriginal, hostSolidCut, BooleanOperationsType.Difference));
+            var thisOpeningSolid = openings.OrderBy(solidOpening => (solidOpening.ComputeCentroid() - openingLocation).GetLength()).FirstOrDefault();
+            if(thisOpeningSolid != null) {
+                return thisOpeningSolid;
+            } else {
+                return GetTransformedBBoxXYZ().CreateSolid();
+            }
+        }
+
+        public BoundingBoxXYZ GetTransformedBBoxXYZ() {
+            return _familyInstance.GetBoundingBox().TransformBoundingBox(_familyInstance.GetTotalTransform().Inverse);
+        }
+
+        /// <summary>
+        /// Возвращает хост экземпляра семейства отверстия
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException"></exception>
+        private Element GetHost() {
+            var host = _familyInstance.Host;
+            if(host is null) {
+                throw new NullReferenceException($"Хост элемента с Id: {_familyInstance.Id} - null");
+            }
+            return host;
+        }
+    }
+}
