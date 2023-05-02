abab1d3dbdfe841db752ea083ad89a8b8618391a
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-02 14:18:35 +0300

    Добавить статусы входящих заданий на отверстия

---
+++ RevitOpeningPlacement/OpeningModels/Enums/OpeningTaskIncomingStatusEnum.cs
@@ -0,0 +1,19 @@
+﻿namespace RevitOpeningPlacement.OpeningModels.Enums {
+    /// <summary>
+    /// Статусы отработки входящих заданий на отверстия
+    /// </summary>
+    internal enum OpeningTaskIncomingStatusEnum {
+        /// <summary>
+        /// Новое задание на отверстие
+        /// </summary>
+        NewTask,
+        /// <summary>
+        /// Частично выполненное задание на отверстие: уже размещенное отверстие не полностью соответствует заданию на отверстие
+        /// </summary>
+        NotMatch,
+        /// <summary>
+        /// Задание на отверстие отработано: размещенное отверстие полностью соответствует заданию
+        /// </summary>
+        Completed
+    }
+}
