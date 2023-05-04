9e6023a5d94835231656382860349059568ea52d
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-04 17:48:37 +0300

    Создание исключения SizeTooSmallException

---
+++ RevitOpeningPlacement/Models/Exceptions/SizeTooSmallException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RevitOpeningPlacement.Models.Exceptions {
+    /// <summary>
+    /// Исключение выбрасывается, если какой-то размер геометрического элемента слишком мал
+    /// </summary>
+    internal class SizeTooSmallException : Exception {
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="SizeTooSmallException"/>
+        /// </summary>
+        public SizeTooSmallException() { }
+
+        /// <summary>
+        /// Инициализирует экземпляр <see cref="SizeTooSmallException"/> с заданным сообщением
+        /// </summary>
+        /// <param name="message">Сообщение об ошибке</param>
+        public SizeTooSmallException(string message) : base(message) { }
+    }
+}
