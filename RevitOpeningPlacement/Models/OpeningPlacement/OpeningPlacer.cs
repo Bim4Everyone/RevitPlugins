52e3548e4b5e97a7d0d22d1526a975323adac6f4
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-04 16:10:45 +0300

    Добавление опционального свойства ClashModel

--- RevitOpeningPlacement/Models/OpeningPlacement/OpeningPlacer.cs
+++ RevitOpeningPlacement/Models/OpeningPlacement/OpeningPlacer.cs
@@ -1,5 +1,10 @@
-﻿using Autodesk.Revit.DB;
+﻿using System;
 
+using Autodesk.Revit.DB;
+
+using RevitClashDetective.Models.Clashes;
+
+using RevitOpeningPlacement.Models.Exceptions;
 using RevitOpeningPlacement.Models.Extensions;
 using RevitOpeningPlacement.Models.Interfaces;
 
@@ -7,14 +12,23 @@ namespace RevitOpeningPlacement.Models.OpeningPlacement {
     internal class OpeningPlacer {
         private readonly RevitRepository _revitRepository;
 
-        public OpeningPlacer(RevitRepository revitRepository) {
+        public OpeningPlacer(RevitRepository revitRepository, ClashModel clashModel = null) {
+            if(revitRepository is null) {
+                throw new ArgumentNullException(nameof(revitRepository));
+            }
+
             _revitRepository = revitRepository;
+            ClashModel = clashModel;
         }
         public ILevelFinder LevelFinder { get; set; }
         public IPointFinder PointFinder { get; set; }
         public IAngleFinder AngleFinder { get; set; }
         public IParametersGetter ParameterGetter { get; set; }
         public FamilySymbol Type { get; set; }
+        /// <summary>
+        /// Свойство, предоставляющее информацию об элементах, по пересечению которых должно размещаться текущее отверстие
+        /// </summary>
+        public ClashModel ClashModel { get; }
 
         public FamilyInstance Place() {
             var point = PointFinder.GetPoint().Round();
