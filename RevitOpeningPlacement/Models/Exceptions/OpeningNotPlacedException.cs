97f87fe2e9cc3e0fe7056d894adeab432b910db7
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-04 16:06:04 +0300

    Создание класса OpeningNotPlacedException

---
+++ RevitOpeningPlacement/Models/Exceptions/OpeningNotPlacedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RevitOpeningPlacement.Models.Exceptions {
+    /// <summary>
+    /// Исключение <see cref="OpeningNotPlacedException"/> выбрасывается, когда не удалось создать семейство задания на отверстие
+    /// </summary>
+    internal class OpeningNotPlacedException : Exception {
+        /// <summary>
+        /// Создает экземпляр <see cref="OpeningNotPlacedException"/>
+        /// </summary>
+        public OpeningNotPlacedException() { }
+
+        /// <summary>
+        /// Создает экземпляр <see cref="OpeningNotPlacedException"/> с заданным сообщением
+        /// </summary>
+        /// <param name="message">Сообщение об ошибке</param>
+        public OpeningNotPlacedException(string message) : base(message) { }
+    }
+}
