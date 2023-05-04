20da07d5a85ea5aa22536e89eec3f31fa5e7d01a
divin_n <nikitad93@mail.ru>
2023-05-04 11:02:11 +0300

    Создал проект для плагина "RevitCreatingFiltersByValues". Добавил описание

---
+++ RevitCreatingFiltersByValues/Views/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace RevitCreatingFiltersByValues.Views {
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        public override string PluginName => nameof(RevitCreatingFiltersByValues);
+        public override string ProjectConfigName => nameof(MainWindow);
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e) {
+            DialogResult = true;
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
+            DialogResult = false;
+        }
+    }
+}
