dcce59132c80a2287129485791a0abfc6c572997
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-05 10:26:17 +0300

    Возвращать пустое перечисление при отсутствии главных элементов

--- RevitOpeningPlacement/Models/ClashInitializer.cs
+++ RevitOpeningPlacement/Models/ClashInitializer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Autodesk.Revit.DB;
 
@@ -14,6 +11,10 @@ namespace RevitOpeningPlacement.Models {
     internal class ClashInitializer {
         public static IEnumerable<ClashModel> GetClashes(RevitClashDetective.Models.RevitRepository revitRepository, Filter mepFilter, Filter architectureFilter) {
             var mainProvider = new FilterProvider(revitRepository.Doc, mepFilter, Transform.Identity);
+            var mainCount = mainProvider.GetElements().Count;
+            if(mainCount == 0) {
+                return Enumerable.Empty<ClashModel>();
+            }
             var otherProviders = revitRepository.DocInfos
                                                 .Select(item => new FilterProvider(item.Doc, architectureFilter, item.Transform))
                                                 .ToList();
