236e253c0150ac7e2db3d79d5f6efe278a8f7c1b
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-10 18:08:40 +0300

    Добавление метода проверки на уже размещенные отверстия

--- RevitOpeningPlacement/OpeningModels/OpeningTaskOutcoming.cs
+++ RevitOpeningPlacement/OpeningModels/OpeningTaskOutcoming.cs
@@ -1,21 +1,34 @@
-﻿using Autodesk.Revit.DB;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
 
 using dosymep.Revit;
 
 using RevitClashDetective.Models.Extensions;
 
+using RevitOpeningPlacement.Models.Extensions;
 using RevitOpeningPlacement.Models.Interfaces;
+using RevitOpeningPlacement.OpeningModels.Comparers;
 
 namespace RevitOpeningPlacement.OpeningModels {
     /// <summary>
     /// Класс, обозначающий экземпляры семейств заданий на отверстия, 
     /// размещаемые в файлах-источниках заданий на отверстия для последующей передачи этих заданий получателю
     /// </summary>
-    internal class OpeningTaskOutcoming : ISolidProvider {
+    internal class OpeningTaskOutcoming : ISolidProvider, IEquatable<OpeningTaskOutcoming> {
         /// <summary>
         /// Экземпляр семейства задания на отверстие
         /// </summary>
         private readonly FamilyInstance _familyInstance;
+
+        /// <summary>
+        /// Допустимое расстояние между экземплярами семейств заданий на отверстия, при котором считается, что они размещены в одном и том же месте
+        /// </summary>
+        private static readonly double _distance3dTolerance = Math.Sqrt(XYZExtension.FeetRound * XYZExtension.FeetRound * XYZExtension.FeetRound);
+
+        private static readonly OpeningTaskOutcomingEqualityComparer _equalityComparer = new OpeningTaskOutcomingEqualityComparer();
 
 
         /// <summary>
@@ -42,7 +55,7 @@ namespace RevitOpeningPlacement.OpeningModels {
         /// <summary>
         /// Флаг, обозначающий, удален ли экземпляр семейства задания на отверстие из проекта
         /// </summary>
-        public bool IsRemoved { get; private set; } = false;
+        public bool IsRemoved => (_familyInstance is null) || (!_familyInstance.IsValidObject);
 
         /// <summary>
         /// Флаг, обозначающий, пересекается ли экземпляр семейства задания на отверстие с каким-либо элементом из текущего проекта, 
@@ -54,19 +67,123 @@ namespace RevitOpeningPlacement.OpeningModels {
 
 
         /// <summary>
-        /// Возвращает Solid экземпляра семейства задания на отверстие с трансформированными координатами
+        /// Возвращает Solid экземпляра семейства задания на отверстие с трансформированными координатами, 
+        /// если элемент был удален из документа, будет возвращено значение по умолчанию
         /// </summary>
         /// <returns></returns>
         public Solid GetSolid() {
+            if(IsRemoved) {
+                return default;
+            }
             return _familyInstance.GetSolid();
         }
 
         /// <summary>
-        /// Возвращает BoundingBoxXYZ с учетом расположения <see cref="_familyInstance">элемента</see> в файле Revit
+        /// Возвращает BoundingBoxXYZ с учетом расположения <see cref="_familyInstance">элемента</see> в файле Revit, 
+        /// если элемент был удален из документа, будет возвращено значение по умолчанию
         /// </summary>
         /// <returns></returns>
         public BoundingBoxXYZ GetTransformedBBoxXYZ() {
+            if(IsRemoved) {
+                return default;
+            }
             return _familyInstance.GetBoundingBox();
+        }
+
+
+        /// <summary>
+        /// Проверяет, размещено ли уже такое же задание на отверстие в проекте. Под "таким" же понимается семейство задания на отверстие с координатами
+        /// </summary>
+        /// <param name="placedOpenings">Существующие задания на отверстия в проекте</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public bool IsAlreadyPlaced(ICollection<OpeningTaskOutcoming> placedOpenings) {
+            if(IsRemoved || placedOpenings.Count == 0) {
+                return false;
+            }
+            var closestPlacedOpenings = GetIntersectingTasksRaw(placedOpenings).Where(placedOpening =>
+                placedOpening.Location
+                .DistanceTo(placedOpening.Location) <= _distance3dTolerance);
+
+            foreach(OpeningTaskOutcoming placedOpening in closestPlacedOpenings) {
+                if(placedOpening.EqualsSolid(GetSolid(), XYZExtension.FeetRound)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj) {
+            if(obj is null) {
+                return false;
+            } else if(obj is OpeningTaskOutcoming openingOther) {
+                return (Id == openingOther.Id) && GetDocument().Equals(openingOther.GetDocument());
+            } else {
+                return false;
+            }
+        }
+
+        public override int GetHashCode() {
+            return Id.GetHashCode();
+        }
+
+        public bool Equals(OpeningTaskOutcoming other) {
+            return (Id == other.Id) && GetDocument().Equals(other.GetDocument());
+        }
+
+        /// <summary>
+        /// Первоначальная быстрая проверка поданной коллекции заданий на отверстия из текущего файла на пересечение с текущим заданием на отверстие
+        /// </summary>
+        /// <param name="openingTasks"></param>
+        /// <returns></returns>
+        private ICollection<OpeningTaskOutcoming> GetIntersectingTasksRaw(ICollection<OpeningTaskOutcoming> openingTasks) {
+            using(var filter = GetFilterTasks(openingTasks)) {
+                var intersects = filter
+                    .Excluding(new ElementId[] { new ElementId(Id) })
+                    .WherePasses(GetBoundingBoxFilter())
+                    .Cast<FamilyInstance>()
+                    .Select(f => new OpeningTaskOutcoming(f));
+                return openingTasks.Intersect(intersects, _equalityComparer).ToList();
+            }
+        }
+
+        private Outline GetOutline() {
+            if(IsRemoved) {
+                return default;
+            }
+            var bb = GetTransformedBBoxXYZ();
+            return new Outline(bb.Min, bb.Max);
+        }
+
+        private BoundingBoxIntersectsFilter GetBoundingBoxFilter() {
+            if(IsRemoved) {
+                return default;
+            }
+            return new BoundingBoxIntersectsFilter(GetOutline());
+        }
+
+        /// <summary>
+        /// Возвращает документ, в котором размещено семейство отверстия. 
+        /// Если экземпляр семейства удален, будет выброшено исключение <see cref="Autodesk.Revit.Exceptions.InvalidObjectException"/>
+        /// Проверку на удаление делать через <see cref="IsRemoved"/>
+        /// </summary>
+        /// <returns></returns>
+        private Document GetDocument() {
+            if(IsRemoved) {
+                return default;
+            }
+            return _familyInstance.Document;
+        }
+
+        private FilteredElementCollector GetFilterTasks(ICollection<OpeningTaskOutcoming> openingTasks) {
+            if(IsRemoved) {
+                return default;
+            }
+            return new FilteredElementCollector(GetDocument(), GetTasksIds(openingTasks));
+        }
+
+        private ICollection<ElementId> GetTasksIds(ICollection<OpeningTaskOutcoming> openingTasks) {
+            return openingTasks.Select(t => new ElementId(t.Id)).ToList();
         }
     }
 }
