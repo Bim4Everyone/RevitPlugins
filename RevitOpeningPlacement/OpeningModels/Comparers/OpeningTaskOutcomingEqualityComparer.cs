d37760e69c16783deb1e30c638124be339b8973b
Vladimir Stroganov <vlastroganov@gmail.com>
2023-05-10 18:09:29 +0300

    Добавить OpeningTaskOutcomingEqualityComparer

---
+++ RevitOpeningPlacement/OpeningModels/Comparers/OpeningTaskOutcomingEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RevitOpeningPlacement.OpeningModels.Comparers {
+    internal class OpeningTaskOutcomingEqualityComparer : IEqualityComparer<OpeningTaskOutcoming> {
+        public bool Equals(OpeningTaskOutcoming x, OpeningTaskOutcoming y) {
+            if((x is null) || (y is null)) {
+                return false;
+            } else {
+                return x.Equals(y);
+            }
+        }
+
+        public int GetHashCode(OpeningTaskOutcoming obj) {
+            return obj.GetHashCode();
+        }
+    }
+}
