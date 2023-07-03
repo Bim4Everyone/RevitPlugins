29541c869b1ea3bcd150fc91f4ff3f3ab9fdf50d
Vladimir Stroganov <vlastroganov@gmail.com>
2023-07-03 17:26:57 +0300

    Убрать не используемую проверку

--- RevitOpeningPlacement/Models/OpeningPlacement/ValueGetters/OpeningSizeGetter.cs
+++ RevitOpeningPlacement/Models/OpeningPlacement/ValueGetters/OpeningSizeGetter.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using RevitClashDetective.Models.Value;
 
 using RevitOpeningPlacement.Models.Configs;
-using RevitOpeningPlacement.Models.Exceptions;
-using RevitOpeningPlacement.Models.Extensions;
 using RevitOpeningPlacement.Models.Interfaces;
 
 namespace RevitOpeningPlacement.Models.OpeningPlacement.ValueGetters {
@@ -24,9 +22,6 @@ namespace RevitOpeningPlacement.Models.OpeningPlacement.ValueGetters {
             var size = _max - _min;
             if(_mepCategories != null && _mepCategories.Length > 0) {
                 size += _mepCategories.Max(item => item.GetOffset(_max - _min));
-            }
-            if(size < XYZExtension.FeetRound) {
-                throw new SizeTooSmallException("Заданный габарит отверстия слишком мал");
             }
             return new DoubleParamValue(size);
         }
