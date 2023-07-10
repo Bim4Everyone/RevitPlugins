0a2f3f3d633237256ede9d8ff4e020e98ffbc759
Vladimir Stroganov <vlastroganov@gmail.com>
2023-07-10 18:24:10 +0300

    Создать ValueGetter для ФОП_Дата

---
+++ RevitOpeningPlacement/Models/OpeningPlacement/ValueGetters/DateValueGetter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RevitClashDetective.Models.Value;
+
+using RevitOpeningPlacement.Models.Interfaces;
+
+namespace RevitOpeningPlacement.Models.OpeningPlacement.ValueGetters {
+    internal class DateValueGetter : IValueGetter<StringParamValue> {
+
+        public DateValueGetter() { }
+
+        public StringParamValue GetValue() {
+            return new StringParamValue(DateTime.Now.ToString("yyyy-MM-dd HH:mm"));
+        }
+    }
+}
