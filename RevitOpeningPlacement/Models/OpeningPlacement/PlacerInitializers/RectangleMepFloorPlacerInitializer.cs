0965ea535159c1388e0fcdf4001b8512cbd55f4f
Vladimir Stroganov <vlastroganov@gmail.com>
2023-07-10 18:22:34 +0300

    Передача PointFinder в PerpendicularRectangleCurveFloorParamGetter

--- RevitOpeningPlacement/Models/OpeningPlacement/PlacerInitializers/RectangleMepFloorPlacerInitializer.cs
+++ RevitOpeningPlacement/Models/OpeningPlacement/PlacerInitializers/RectangleMepFloorPlacerInitializer.cs
@@ -15,15 +15,16 @@ namespace RevitOpeningPlacement.Models.OpeningPlacement.PlacerInitializers {
     internal class RectangleMepFloorPlacerInitializer : IMepCurvePlacerInitializer {
         public OpeningPlacer GetPlacer(RevitRepository revitRepository, ClashModel clashModel, MepCategory categoryOption) {
             var clash = new MepCurveClash<CeilingAndFloor>(revitRepository, clashModel);
+            var pointFinder = new FloorPointFinder<MEPCurve>(clash);
             var placer = new OpeningPlacer(revitRepository, clashModel) {
                 LevelFinder = new ClashLevelFinder(revitRepository, clashModel),
-                PointFinder = new FloorPointFinder<MEPCurve>(clash),
+                PointFinder = pointFinder,
                 Type = revitRepository.GetOpeningType(OpeningType.FloorRectangle),
             };
 
             if(clash.Element2.IsHorizontal() && clash.Element1.IsVertical()) {
                 placer.AngleFinder = new FloorAngleFinder(clash.Element1);
-                placer.ParameterGetter = new PerpendicularRectangleCurveFloorParamGetter(clash, categoryOption);
+                placer.ParameterGetter = new PerpendicularRectangleCurveFloorParamGetter(clash, categoryOption, pointFinder);
             } else {
                 placer.AngleFinder = new ZeroAngleFinder();
                 placer.ParameterGetter = new FloorSolidParameterGetter(new MepCurveClashSolidProvider<CeilingAndFloor>(clash), categoryOption);
