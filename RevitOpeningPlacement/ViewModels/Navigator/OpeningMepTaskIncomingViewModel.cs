9113f237d0146715a97531f0d3ed1718c6138342
Vladimir Stroganov <vlastroganov@gmail.com>
2023-07-13 10:55:39 +0300

    Добавить ViewModel для входящего задания от инженера

---
+++ RevitOpeningPlacement/ViewModels/Navigator/OpeningMepTaskIncomingViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Autodesk.Revit.DB;
+
+using dosymep.WPF.ViewModels;
+
+using RevitOpeningPlacement.OpeningModels;
+
+namespace RevitOpeningPlacement.ViewModels.Navigator {
+    internal class OpeningMepTaskIncomingViewModel : BaseViewModel {
+        private readonly OpeningMepTaskIncoming _openingTask;
+
+
+        public OpeningMepTaskIncomingViewModel(OpeningMepTaskIncoming incomingOpeningTask) {
+            if(incomingOpeningTask is null) {
+                throw new ArgumentNullException(nameof(incomingOpeningTask));
+            }
+            _openingTask = incomingOpeningTask;
+
+            Date = _openingTask.Date;
+            MepSystem = _openingTask.MepSystem;
+            Description = _openingTask.Description;
+            CenterOffset = _openingTask.CenterOffset;
+            BottomOffset = _openingTask.BottomOffset;
+        }
+
+
+
+        public string Date { get; } = string.Empty;
+
+        public string MepSystem { get; } = string.Empty;
+
+        public string Description { get; } = string.Empty;
+
+        public string CenterOffset { get; } = string.Empty;
+
+        public string BottomOffset { get; } = string.Empty;
+
+
+        public FamilyInstance GetFamilyInstance() {
+            return _openingTask.GetFamilyInstance();
+        }
+    }
+}
