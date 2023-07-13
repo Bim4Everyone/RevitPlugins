caf50b4feee0c4ac59b647b23505267cf6626201
Vladimir Stroganov <vlastroganov@gmail.com>
2023-07-13 15:29:23 +0300

    Добавить окно навигатора по входящим заданиям на отверстия от инженера

---
+++ RevitOpeningPlacement/Views/NavigatorMepIncoming.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+using DevExpress.Xpf.Grid;
+
+namespace RevitOpeningPlacement.Views {
+    /// <summary>
+    /// Interaction logic for NavigatorMepIncoming.xaml
+    /// </summary>
+    public partial class NavigatorMepIncoming {
+        public NavigatorMepIncoming() {
+            InitializeComponent();
+            Loaded += NavigatorView_Loaded;
+        }
+
+        private void NavigatorView_Loaded(object sender, RoutedEventArgs e) {
+            _dg.GroupBy(_dg.Columns[1]);
+            _dg.GroupBy(_dg.Columns[2]);
+        }
+
+        public override string PluginName => nameof(RevitOpeningPlacement);
+        public override string ProjectConfigName => nameof(NavigatorMepIncoming);
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void view_FocusedRowHandleChanged(object sender, FocusedRowHandleChangedEventArgs e) {
+            var handle = _dg.View.FocusedRowHandle;
+            _dg.UnselectAll();
+            _dg.SelectItem(handle);
+        }
+
+        private void SimpleButton_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
