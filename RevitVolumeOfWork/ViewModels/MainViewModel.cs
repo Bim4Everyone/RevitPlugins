6974bcf4cd8c6236a4927a2cfd2d95d859726578
sssenya <senia7@bk.ru>
2023-07-21 10:35:37 +0300

    Создал проект для плагина RevitVolumeOfWork. Добавил описание

---
+++ RevitVolumeOfWork/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.UI;
+
+using dosymep.WPF.ViewModels;
+
+using RevitVolumeOfWork.Models;
+
+namespace RevitVolumeOfWork.ViewModels {
+    internal class MainViewModel : BaseViewModel {
+        private readonly PluginConfig _pluginConfig;
+        private readonly RevitRepository _revitRepository;
+
+        private string _errorText;
+
+        public MainViewModel(PluginConfig pluginConfig, RevitRepository revitRepository) {
+            _pluginConfig = pluginConfig;
+            _revitRepository = revitRepository;
+        }
+
+        public string ErrorText {
+            get => _errorText;
+            set => this.RaiseAndSetIfChanged(ref _errorText, value);
+        }
+    }
+}
