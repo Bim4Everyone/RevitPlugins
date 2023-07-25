807ed583fec65f3a05ac27de46f1d34c31df0d7b
Vladimir Stroganov <vlastroganov@gmail.com>
2023-07-25 14:31:28 +0300

    Удалить не используемый код

--- RevitOpeningPlacement/Views/CategoryView.xaml.cs
+++ RevitOpeningPlacement/Views/CategoryView.xaml.cs
@@ -1,37 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using RevitOpeningPlacement.ViewModels.OpeningConfig;
+﻿using System.Windows.Controls;
 
 namespace RevitOpeningPlacement.Views {
-    /// <summary>
-    /// Interaction logic for CategoryView.xaml
-    /// </summary>
     public partial class CategoryView : UserControl {
-        public static DependencyProperty SelectedItemProperty = DependencyProperty.Register(nameof(SelectedItem), typeof(MepCategoryViewModel), typeof(CategoryView));
-
-        internal MepCategoryViewModel SelectedItem {
-            get {
-                return (MepCategoryViewModel) GetValue(SelectedItemProperty);
-            }
-            set {
-                SetValue(SelectedItemProperty, value);
-            }
-        }
-
-
         public CategoryView() {
             InitializeComponent();
         }
