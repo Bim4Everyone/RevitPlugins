b23cacd6a76b2116d0cb6afde25e4fd9b8de809c
Vladimir Stroganov <vlastroganov@gmail.com>
2023-07-25 14:46:26 +0300

    Рефакторинг

--- RevitOpeningPlacement/SetOpeningTaskCommand.cs
+++ RevitOpeningPlacement/SetOpeningTaskCommand.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Interop;
+﻿using System.Windows.Interop;
 
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-using dosymep;
 using dosymep.Bim4Everyone;
 
+using RevitOpeningPlacement.Models;
 using RevitOpeningPlacement.Models.Configs;
-using RevitOpeningPlacement.ViewModels;
 using RevitOpeningPlacement.ViewModels.OpeningConfig;
 using RevitOpeningPlacement.Views;
 
@@ -26,7 +20,8 @@ namespace RevitOpeningPlacement {
 
         protected override void Execute(UIApplication uiApplication) {
             var openingConfig = OpeningConfig.GetOpeningConfig();
-            var viewModel = new MainViewModel(uiApplication, openingConfig);
+            RevitRepository revitRepository = new RevitRepository(uiApplication.Application, uiApplication.ActiveUIDocument.Document);
+            var viewModel = new MainViewModel(revitRepository, openingConfig);
 
             var window = new MainWindow() { Title = PluginName, DataContext = viewModel };
             var helper = new WindowInteropHelper(window) { Owner = uiApplication.MainWindowHandle };
