c49a5cc066c627813bba3dff6f4481887d42204c
Vladimir Stroganov <vlastroganov@gmail.com>
2023-08-02 13:41:55 +0300

    Скорректировать логику работы EmenentIdParamValue

--- RevitClashes/Models/Value/ElementIdParamValue.cs
+++ RevitClashes/Models/Value/ElementIdParamValue.cs
@@ -12,6 +12,9 @@ using RevitClashDetective.Models.Interfaces;
 using RevitClashDetective.Models.Visiter;
 
 namespace RevitClashDetective.Models.Value {
+    /// <summary>
+    /// Параметр с типом значений ElementId. Например: "Уровень", "Типоразмер из семейства" и т.п.
+    /// </summary>
     internal class ElementIdParamValue : ParamValue<string> {
 
         public ElementIdParamValue() : base() {
@@ -19,48 +22,58 @@ namespace RevitClashDetective.Models.Value {
         }
 
         [JsonConstructor]
-        public ElementIdParamValue(int[] categories, string value, string stringValue) : base(value, stringValue) {
-            Categories = categories;
-        }
+        public ElementIdParamValue(string value, string stringValue) : base(value, stringValue) { }
 
-        public int[] Categories { get; }
 
+        // Примечание: значение параметра не связано с категорией элемента, у которого есть этот параметр.
+        // Например, значение параметра "Уровень" какого-то экземпляра семейства это Id конкретного уровня из модели,
+        // а значение параметра с типом данных "Типоразмер из семейства" - это Id конкретного типоразмера семейства
+        //
+        // Поэтому для создания фильтра нужно произвести поиск по элементам заданного документа и найти, если получится, тот элемент/типоразмер/семейство : типоразмер,
+        // у которого название совпадает с ParamValue.TValue
         public override FilterRule GetFilterRule(IVisiter visiter, Document doc, RevitParam param) {
-            if(Categories is null) {
-                throw new ArgumentNullException(nameof(Categories));
-            }
             var paramId = GetParamId(doc, param);
-            if(paramId == ElementId.InvalidElementId)
+            if(paramId == ElementId.InvalidElementId) {
+                // в документе не существует данного параметра
                 return null;
+            }
 
             if(visiter is EqualsVisiter || visiter is NotEqualsVisister) {
+                // попытка получения элемента, являющегося значением параметра
                 var value = new FilteredElementCollector(doc)
-                .WherePasses(new ElementMulticategoryFilter(Categories.Select(item => new ElementId(item)).ToArray()))
-                .Select(item => GetValue(item, param))
-                .Where(item => !item.IsNull())
-                .Select(item => doc.GetElement(item))
-                .FirstOrDefault(item => item.Name.Equals(TValue, StringComparison.CurrentCultureIgnoreCase));
-                if(value == null)
+                    .WhereElementIsNotElementType()
+                    .WhereElementIsViewIndependent()
+                    .FirstOrDefault(item => item.Name.Equals(TValue, StringComparison.CurrentCultureIgnoreCase));
+                if(value is null) {
+                    // попытка получения типоразмера, являющегося значением параметра
+                    value = new FilteredElementCollector(doc)
+                        .WhereElementIsElementType()
+                        .WhereElementIsViewIndependent()
+                        .FirstOrDefault(item => item.Name.Equals(TValue, StringComparison.CurrentCultureIgnoreCase));
+                }
+                if(value is null) {
+                    // попытка получения семейства и типоразмера, являющегося значением параметра
+                    // формат написания семейства и типоразмера в ревите:
+                    //     НазваниеСемейства : НазваниеТипа
+                    value = new FilteredElementCollector(doc)
+                        .WhereElementIsElementType()
+                        .WhereElementIsViewIndependent()
+                        .OfClass(typeof(FamilySymbol))
+                        .Cast<FamilySymbol>()
+                        .FirstOrDefault(item => (item.FamilyName + " : " + item.Name).Equals(TValue, StringComparison.CurrentCultureIgnoreCase));
+                }
+
+                if(value == null) {
+                    // в документе нет элемента/типоразмера с заданным названием
                     return null;
-                return visiter.Create(paramId, value?.Id);
+                } else {
+                    return visiter.Create(paramId, value.Id);
+                }
             }
 
             return visiter.Create(paramId, TValue);
         }
 
-        private ElementId GetValue(Element item, RevitParam param) {
-            if(item.IsExistsParam(param)) {
-                return item.GetParamValueOrDefault<ElementId>(param);
-            } else {
-                var typeId = item.GetTypeId();
-                if(typeId != ElementId.InvalidElementId) {
-                    var type = item.Document.GetElement(typeId);
-                    if(type.IsExistsParam(param))
-                        return type.GetParamValueOrDefault<ElementId>(param);
-                }
-            }
-            return null;
-        }
 
         public override void SetParamValue(Element element, string paramName) {
             if(element.IsExistsParam(paramName)) {
