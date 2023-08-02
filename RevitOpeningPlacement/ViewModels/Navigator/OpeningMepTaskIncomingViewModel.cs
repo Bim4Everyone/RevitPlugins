29bab4f03cba2535d1867bed011c297fefe38b75
Vladimir Stroganov <vlastroganov@gmail.com>
2023-08-02 13:51:13 +0300

    Добавить комментарии

--- RevitOpeningPlacement/ViewModels/Navigator/OpeningMepTaskIncomingViewModel.cs
+++ RevitOpeningPlacement/ViewModels/Navigator/OpeningMepTaskIncomingViewModel.cs
@@ -8,7 +8,13 @@ using dosymep.WPF.ViewModels;
 using RevitOpeningPlacement.OpeningModels;
 
 namespace RevitOpeningPlacement.ViewModels.Navigator {
+    /// <summary>
+    /// Модель представления окна для работы с конкретным входящим заданием на отверстие от инженера в файле архитектора или конструктора
+    /// </summary>
     internal class OpeningMepTaskIncomingViewModel : BaseViewModel {
+        /// <summary>
+        /// Экземпляр семейства задания на отверстие
+        /// </summary>
         private readonly OpeningMepTaskIncoming _openingTask;
 
 
@@ -29,26 +35,53 @@ namespace RevitOpeningPlacement.ViewModels.Navigator {
         }
 
 
+        /// <summary>
+        /// Id экземпляра семейства задания на отверстие
+        /// </summary>
         public string OpeningId { get; } = string.Empty;
 
+        /// <summary>
+        /// Название связанного файла-источника задания на отверстие
+        /// </summary>
         public string FileName { get; } = string.Empty;
 
+        /// <summary>
+        /// Дата создания отверстия
+        /// </summary>
         public string Date { get; } = string.Empty;
 
+        /// <summary>
+        /// Название инженерной системы, для элемента которой создано задание на отверстие
+        /// </summary>
         public string MepSystem { get; } = string.Empty;
 
+        /// <summary>
+        /// Описание задания на отверстие
+        /// </summary>
         public string Description { get; } = string.Empty;
 
+        /// <summary>
+        /// Отметка центра задания на отверстие
+        /// </summary>
         public string CenterOffset { get; } = string.Empty;
 
+        /// <summary>
+        /// Отметка низа задания на отверстие
+        /// </summary>
         public string BottomOffset { get; } = string.Empty;
 
         private bool _isAccepted;
+        /// <summary>
+        /// Статус задания: принято/не принято
+        /// </summary>
         public bool IsAccepted {
             get => _isAccepted;
             set => RaiseAndSetIfChanged(ref _isAccepted, value);
         }
 
+        /// <summary>
+        /// Комментарий к заданию на отверстие
+        /// </summary>
         private string _comment;
         public string Comment {
             get => _comment;
@@ -56,6 +89,10 @@ namespace RevitOpeningPlacement.ViewModels.Navigator {
         }
 
 
+        /// <summary>
+        /// Возвращает экземпляр семейства задания на отверстие
+        /// </summary>
+        /// <returns></returns>
         public FamilyInstance GetFamilyInstance() {
             return _openingTask.GetFamilyInstance();
         }
