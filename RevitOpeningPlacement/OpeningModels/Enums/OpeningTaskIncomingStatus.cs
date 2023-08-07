45cf4e834623e9edab423253cb6aba28584938d5
Vladimir Stroganov <vlastroganov@gmail.com>
2023-08-07 18:11:12 +0300

    Скорректировать перечисление статусов входящих заданий

--- RevitOpeningPlacement/OpeningModels/Enums/OpeningTaskIncomingStatus.cs
+++ RevitOpeningPlacement/OpeningModels/Enums/OpeningTaskIncomingStatus.cs
@@ -1,4 +1,6 @@
-﻿namespace RevitOpeningPlacement.OpeningModels.Enums {
+﻿using System.ComponentModel;
+
+namespace RevitOpeningPlacement.OpeningModels.Enums {
     /// <summary>
     /// Статусы отработки входящих заданий на отверстия
     /// </summary>
@@ -6,14 +8,22 @@
         /// <summary>
         /// Новое задание на отверстие
         /// </summary>
-        NewTask,
+        [Description("Новое задание")]
+        New,
         /// <summary>
         /// Частично выполненное задание на отверстие: уже размещенное отверстие не полностью соответствует заданию на отверстие
         /// </summary>
+        [Description("Частично выполненное задание")]
         NotMatch,
         /// <summary>
         /// Задание на отверстие отработано: размещенное отверстие полностью соответствует заданию
         /// </summary>
-        Completed
+        [Description("Выполненное задание")]
+        Completed,
+        /// <summary>
+        /// Задание на отверстие не пересекается ни с одним элементом конструкции и ни с одним проемом текущем файле АР или КР
+        /// </summary>
+        [Description("Нет пересечения")]
+        NoIntersection
     }
 }
