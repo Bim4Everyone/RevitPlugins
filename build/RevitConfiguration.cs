525adf078d1d5a73433b6412c4786e2e36e0e0d6
dosymep <dosymep@gmail.com>
2023-08-07 10:54:04 +0300

    Обновил конфигурацию ревит

--- build/RevitConfiguration.cs
+++ build/RevitConfiguration.cs
@@ -9,17 +9,19 @@ using Nuke.Common.Tooling;
 
 [TypeConverter(typeof(TypeConverter<RevitConfiguration>))]
 class RevitConfiguration : Enumeration {
-    static RevitConfiguration D2020 = new RevitConfiguration {Value = nameof(D2020), Version = 2020};
-    static RevitConfiguration D2021 = new RevitConfiguration {Value = nameof(D2021), Version = 2021};
-    static RevitConfiguration D2022 = new RevitConfiguration {Value = nameof(D2022), Version = 2022};
-    // static RevitConfiguration D2023 = new RevitConfiguration {Value = nameof(D2023), Version = 2023};
-    // static RevitConfiguration D2024 = new RevitConfiguration {Value = nameof(D2024), Version = 2024};
+    public static readonly RevitConfiguration D2020 = new() {Value = nameof(D2020), Version = 2020};
+    public static readonly RevitConfiguration D2021 = new() {Value = nameof(D2021), Version = 2021};
+    public static readonly RevitConfiguration D2022 = new() {Value = nameof(D2022), Version = 2022};
+    
+    // public static readonly RevitConfiguration D2023 = new() {Value = nameof(D2023), Version = 2023};
+    // public static readonly RevitConfiguration D2024 = new() {Value = nameof(D2024), Version = 2024};
 
-    static RevitConfiguration R2020 = new RevitConfiguration {Value = nameof(R2020), Version = 2020};
-    static RevitConfiguration R2021 = new RevitConfiguration {Value = nameof(R2021), Version = 2021};
-    static RevitConfiguration R2022 = new RevitConfiguration {Value = nameof(R2022), Version = 2022};
-    // static RevitConfiguration R2023 = new RevitConfiguration {Value = nameof(R2023), Version = 2023};
-    // static RevitConfiguration R2024 = new RevitConfiguration {Value = nameof(R2024), Version = 2024};
+    public static readonly RevitConfiguration R2020 = new() {Value = nameof(R2020), Version = 2020};
+    public static readonly RevitConfiguration R2021 = new() {Value = nameof(R2021), Version = 2021};
+    public static readonly RevitConfiguration R2022 = new() {Value = nameof(R2022), Version = 2022};
+    
+    // public static readonly RevitConfiguration R2023 = new() {Value = nameof(R2023), Version = 2023};
+    // public static readonly RevitConfiguration R2024 = new() {Value = nameof(R2024), Version = 2024};
 
     public int Version { get; protected set; }
 
@@ -28,14 +30,14 @@ class RevitConfiguration : Enumeration {
     }
 
     public static IEnumerable<RevitConfiguration> GetDebugConfiguration() {
-        return typeof(RevitConfiguration).GetFields(BindingFlags.Static | BindingFlags.NonPublic)
+        return typeof(RevitConfiguration).GetFields(BindingFlags.Static | BindingFlags.Public)
             .Where(item => item.Name.StartsWith("D"))
             .Select(item => item.GetValue(null))
             .OfType<RevitConfiguration>();
     }
 
     public static IEnumerable<RevitConfiguration> GetReleaseConfiguration() {
-        return typeof(RevitConfiguration).GetFields(BindingFlags.Static | BindingFlags.NonPublic)
+        return typeof(RevitConfiguration).GetFields(BindingFlags.Static | BindingFlags.Public)
             .Where(item => item.Name.StartsWith("R"))
             .Select(item => item.GetValue(null))
             .OfType<RevitConfiguration>();
