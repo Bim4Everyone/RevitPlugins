fdbcccb2d836761af0741ac2b2e82543b728b4e9
dosymep <dosymep@gmail.com>
2023-08-09 09:49:26 +0300

    Вынес классы в отдельные файлы

---
+++ build/BundleType.cs
@@ -0,0 +1,8 @@
+﻿using Nuke.Common.Tooling;
+
+class BundleType : Enumeration {
+    public static readonly BundleType PushButton = new() {Value = nameof(PushButton)};
+    public static readonly BundleType InvokeButton = new() {Value = nameof(InvokeButton)};
+
+    public string ExtensionWithDot => "." + Value.ToLower();
+}
--- build/ICreateBundle.cs
+++ build/ICreateBundle.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 using Nuke.Common;
 using Nuke.Common.IO;
-using Nuke.Common.Tooling;
 
 using Serilog;
 
@@ -54,63 +49,5 @@ interface ICreateBundle : IClean, IHazTemplate {
 
     Uri GetImageUri() {
         return new Uri(string.Format(UriIconFormat, IconSize.Size, IconUrl.AbsolutePath.Split('/')[^2]));
-    }
-}
-
-class BundleType : Enumeration {
-    public static readonly BundleType PushButton = new() {Value = nameof(PushButton)};
-    public static readonly BundleType InvokeButton = new() {Value = nameof(InvokeButton)};
-
-    public string ExtensionWithDot => "." + Value.ToLower();
-}
-
-class IconSize : Enumeration {
-    public static readonly IconSize Size32 = new() {Value = nameof(Size32), Size = 32};
-    public static readonly IconSize Size64 = new() {Value = nameof(Size64), Size = 64};
-    public static readonly IconSize Size96 = new() {Value = nameof(Size96), Size = 96};
-
-    public int Size { get; protected set; }
-
-    public static async Task CreateImages(Uri uri, AbsolutePath target) {
-        using(MemoryStream stream = new(await GetUrlContent(uri))) {
-            Bitmap image = (Bitmap) Image.FromStream(stream);
-            image.Save(target);
-
-            CreateBitmap(
-                image,
-                Color.FromArgb(235, 235, 235),
-                target.Parent / target.NameWithoutExtension + ".dark.png");
-
-            CreateBitmap(
-                image,
-                Color.FromArgb(250, 176, 5),
-                target.Parent / target.NameWithoutExtension + ".fail.png");
-
-            CreateBitmap(
-                image,
-                Color.FromArgb(250, 82, 82),
-                target.Parent / target.NameWithoutExtension + ".warning.png");
-        }
-    }
-
-    static void CreateBitmap(Bitmap source, Color color, AbsolutePath target) {
-        Bitmap output = new(source);
-        for(int x = 0; x < source.Width; x++) {
-            for(int y = 0; y < source.Height; y++) {
-                Color pixel = source.GetPixel(x, y);
-                output.SetPixel(x, y,
-                    pixel.A == 0
-                        ? pixel
-                        : Color.FromArgb(pixel.A, color.R, color.G, color.B));
-            }
-        }
-
-        output.Save(target);
-    }
-
-    public static async Task<byte[]> GetUrlContent(Uri uri) {
-        using(var client = new HttpClient())
-        using(var result = await client.GetAsync(uri))
-            return result.IsSuccessStatusCode ? await result.Content.ReadAsByteArrayAsync() : null;
     }
 }
