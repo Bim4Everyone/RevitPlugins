ed887ceee623a07506286262e14164e6575ba59d
dosymep <dosymep@gmail.com>
2023-08-10 13:37:57 +0300

    Удалил филтьтрацию разделителей

--- RevitRooms/ViewModels/RoomSeparatorViewModel.cs
+++ RevitRooms/ViewModels/RoomSeparatorViewModel.cs
@@ -43,7 +43,7 @@ namespace RevitRooms.ViewModels {
                     return true;
                 }
 
-                return GetRooms(_rooms)
+                return _rooms
                     .Select(item => item.RoomSection?.Id)
                     .Distinct()
                     .Count() <= 1; // может не быть квартир и будет значение 0
@@ -57,34 +57,12 @@ namespace RevitRooms.ViewModels {
                     return true;
                 }
 
-                return GetRooms(_rooms)
+                return _rooms
                     .Where(item =>
                         item.RoomGroup?.Name.IndexOf("квартира", StringComparison.CurrentCultureIgnoreCase) >= 0)
                     .Select(item => item.RoomGroup?.Id)
                     .Distinct()
                     .Count() <= 1; // может не быть квартир и будет значение 0
-            }
-        }
-
-        private IEnumerable<SpatialElementViewModel> GetRooms(List<SpatialElementViewModel> rooms) {
-            foreach(var room1 in rooms) {
-                foreach(var room2 in rooms) {
-                    if(room1 == room2) {
-                        continue;
-                    }
-                    
-                    var copy1 = room1.BoundarySegments.ToHashSet();
-                    copy1.Remove(ElementId);
-                    copy1.Remove(ElementId.InvalidElementId);
-
-                    var copy2 = room2.BoundarySegments.ToHashSet();
-                    copy1.Remove(ElementId);
-                    copy1.Remove(ElementId.InvalidElementId);
-
-                    if(copy1.Overlaps(copy2)) {
-                        yield return room1;
-                    }
-                }
             }
         }
     }
