22efa99c73b719027de7e922f39797eefef69912
dosymep <dosymep@gmail.com>
2023-08-17 09:37:48 +0300

    Добавил название плагина

--- build/ICreateBundle.cs
+++ build/ICreateBundle.cs
@@ -7,7 +7,7 @@ using Nuke.Common.IO;
 
 using Serilog;
 
-interface ICreateBundle : IHazOutput, IHazTemplate {
+interface ICreateBundle : IHazPluginName, IHazOutput, IHazTemplate {
     [Parameter] Uri IconUrl => TryGetValue(() => IconUrl);
     [Parameter] string BundleName => TryGetValue(() => BundleName);
     [Parameter] BundleType BundleType => TryGetValue(() => BundleType);
@@ -22,16 +22,18 @@ interface ICreateBundle : IHazOutput, IHazTemplate {
     
     IconSize IconSize => IconSize.Size96;
     string UriIconFormat => "https://img.icons8.com/?size={0}&id={1}&format=png";
-    
+
     Target CreateBundle => _ => _
-        .OnlyWhenStatic(() => 
+        .Requires(() => PluginName)
+        .OnlyWhenStatic(() =>
             !BundleDirectory.DirectoryExists(), "Bundle directory is exists")
-        .OnlyWhenStatic(() => 
+        .OnlyWhenStatic(() =>
             !IconUrl.AbsolutePath.StartsWith(@"https://icons8.com/icon/"), "Bundle icon must be from icons8 site")
         .Executes(async () => {
+            Log.Debug("PluginName: {PluginName}", PluginName);
             Log.Debug("TemplateName: {TemplateName}", TemplateName);
             Log.Debug("TemplateBundleDirectory: {TemplateBundleDirectory}", TemplateBundle);
-
+            
             Log.Debug("IconUrl: {IconUrl}", IconUrl);
             Log.Debug("BundleName: {BundleName}", BundleName);
             Log.Debug("BundleType: {BundleType}", BundleType);
@@ -41,8 +43,8 @@ interface ICreateBundle : IHazOutput, IHazTemplate {
             CopyDirectory(TemplateBundle, BundleDirectory,
                 new Dictionary<string, string>() {
                     {"${{ gen.bundle_name }}", BundleName},
-                    {"${{ gen.plugin_name }}", this.From<IHazPluginName>().PluginName},
-                    {"${{ gen.plugin_command }}", this.From<IHazPluginName>().PluginName + "Command"},
+                    {"${{ gen.plugin_name }}", PluginName},
+                    {"${{ gen.plugin_command }}", PluginName + "Command"},
                 });
 
             await IconSize.CreateImages(GetImageUri(), BundleDirectory / "image.png");
