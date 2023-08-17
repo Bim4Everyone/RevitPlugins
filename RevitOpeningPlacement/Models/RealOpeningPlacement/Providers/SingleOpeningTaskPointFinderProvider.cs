b92ec8d5aa5f386324f2a87c70ff88bbe0e22790
Vladimir Stroganov <vlastroganov@gmail.com>
2023-08-17 15:54:42 +0300

    Добавить провайдеры классов для размещения чистового отверстия

---
+++ RevitOpeningPlacement/Models/RealOpeningPlacement/Providers/SingleOpeningTaskPointFinderProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using RevitOpeningPlacement.Models.Interfaces;
+using RevitOpeningPlacement.Models.RealOpeningPlacement.PointFinders;
+using RevitOpeningPlacement.OpeningModels;
+
+namespace RevitOpeningPlacement.Models.RealOpeningPlacement.Providers {
+    /// <summary>
+    /// Класс, предоставляющий <see cref="IPointFinder"/> для чистового отверстия
+    /// </summary>
+    internal class SingleOpeningTaskPointFinderProvider {
+        private readonly OpeningMepTaskIncoming _openingMepTaskIncoming;
+
+
+        /// <summary>
+        /// Конструктор класса, предоставляющего <see cref="IPointFinder"/> для чистового отверстия
+        /// </summary>
+        /// <param name="incomingTask">Входящее задание на отверстие</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public SingleOpeningTaskPointFinderProvider(OpeningMepTaskIncoming incomingTask) {
+            if(incomingTask is null) { throw new ArgumentNullException(nameof(incomingTask)); }
+
+            _openingMepTaskIncoming = incomingTask;
+        }
+
+
+        public IPointFinder GetPointFinder() {
+            return new SingleOpeningTaskPointFinder(_openingMepTaskIncoming);
+        }
+    }
+}
