c60ba94bf49e0f363b714ce4e42a86e8fb840481
dosymep <dosymep@gmail.com>
2023-08-17 09:44:55 +0300

    Добавил путь компиляции опубликованного проекта

--- build/IHazOutput.cs
+++ build/IHazOutput.cs
@@ -4,6 +4,8 @@ using Nuke.Common.IO;
 interface IHazOutput : INukeBuild {
     [Parameter($"Output folder, default value is \"RootDirectory/bin\"")]
     AbsolutePath Output => TryGetValue(() => Output) ?? DefaultOutput;
+    [Parameter] AbsolutePath PublishDirectory => TryGetValue(() => PublishDirectory) ?? DefaultOutput;
     
     AbsolutePath DefaultOutput => RootDirectory / "bin";
+    
 }
