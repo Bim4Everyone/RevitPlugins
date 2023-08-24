ac6b0a71982ff88e25d62e897e5fc989d99c127a
dosymep <dosymep@gmail.com>
2023-08-24 10:22:56 +0300

    Добавил компиляцию проектов под 2023/2024 версии Revit

--- build/RevitConfiguration.cs
+++ build/RevitConfiguration.cs
@@ -12,16 +12,14 @@ class RevitConfiguration : Enumeration {
     public static readonly RevitConfiguration D2020 = new() {Value = nameof(D2020), Version = 2020};
     public static readonly RevitConfiguration D2021 = new() {Value = nameof(D2021), Version = 2021};
     public static readonly RevitConfiguration D2022 = new() {Value = nameof(D2022), Version = 2022};
-    
-    // public static readonly RevitConfiguration D2023 = new() {Value = nameof(D2023), Version = 2023};
-    // public static readonly RevitConfiguration D2024 = new() {Value = nameof(D2024), Version = 2024};
+    public static readonly RevitConfiguration D2023 = new() {Value = nameof(D2023), Version = 2023};
+    public static readonly RevitConfiguration D2024 = new() {Value = nameof(D2024), Version = 2024};
 
     public static readonly RevitConfiguration R2020 = new() {Value = nameof(R2020), Version = 2020};
     public static readonly RevitConfiguration R2021 = new() {Value = nameof(R2021), Version = 2021};
     public static readonly RevitConfiguration R2022 = new() {Value = nameof(R2022), Version = 2022};
-    
-    // public static readonly RevitConfiguration R2023 = new() {Value = nameof(R2023), Version = 2023};
-    // public static readonly RevitConfiguration R2024 = new() {Value = nameof(R2024), Version = 2024};
+    public static readonly RevitConfiguration R2023 = new() {Value = nameof(R2023), Version = 2023};
+    public static readonly RevitConfiguration R2024 = new() {Value = nameof(R2024), Version = 2024};
 
     public int Version { get; protected set; }
 
