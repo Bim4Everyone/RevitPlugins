cb8c3f5e27372dfc022d9c0bf2aaf8acb5cdf7ce
Vladimir Stroganov <vlastroganov@gmail.com>
2023-08-30 15:44:24 +0300

    Добавить классы для получения типа проема задания на отверстие

---
+++ RevitOpeningPlacement/Models/Interfaces/IOpeningTaskTypeProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace RevitOpeningPlacement.Models.Interfaces {
+    /// <summary>
+    /// Интерфейс, предоставляющий типоразмер семейства задания на отверстие
+    /// </summary>
+    internal interface IOpeningTaskTypeProvider {
+        /// <summary>
+        /// Возвращает тип проема задания на отверстие
+        /// </summary>
+        /// <returns></returns>
+        OpeningType GetOpeningTaskType();
+    }
+}
---
+++ RevitOpeningPlacement/Models/OpeningPlacement/Providers/RoundMepFloorOpeningTaskTypeProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Autodesk.Revit.DB;
+
+using RevitOpeningPlacement.Models.Configs;
+using RevitOpeningPlacement.Models.Extensions;
+using RevitOpeningPlacement.Models.Interfaces;
+
+namespace RevitOpeningPlacement.Models.OpeningPlacement.Providers {
+    /// <summary>
+    /// Класс, предоставляющий тип проема задания на отверстие для круглых элементов инженерных систем, пересекающихся с перекрытием перпендикулярно ему
+    /// </summary>
+    internal class RoundMepFloorOpeningTaskTypeProvider : IOpeningTaskTypeProvider {
+        private readonly MEPCurve _mepCurve;
+        private readonly MepCategory _categoryOption;
+
+        public RoundMepFloorOpeningTaskTypeProvider(MEPCurve mepCurve, MepCategory categoryOption) {
+            _mepCurve = mepCurve ?? throw new ArgumentNullException(nameof(mepCurve));
+            _categoryOption = categoryOption ?? throw new ArgumentNullException(nameof(categoryOption));
+        }
+
+
+        public OpeningType GetOpeningTaskType() {
+            double diameter = _mepCurve.GetDiameter();
+            Offset offset = _categoryOption.GetOffsetTransformedToInternalUnit(diameter);
+
+            var selectedTypeName = offset?.OpeningTypeName ?? string.Empty;
+            if(!string.IsNullOrWhiteSpace(selectedTypeName)
+                && selectedTypeName.Equals(RevitRepository.OpeningTaskTypeName[OpeningType.FloorRound])) {
+
+                return OpeningType.FloorRound;
+
+            } else {
+                return OpeningType.FloorRectangle;
+            }
+        }
+    }
+}
