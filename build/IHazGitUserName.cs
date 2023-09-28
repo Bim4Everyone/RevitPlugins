ab36dad2a6c905a971b0c2564850c1b8bd49bfcc
dosymep <dosymep@gmail.com>
2023-09-28 09:32:58 +0300

    Добавил интерфейс с именем пользователя гит

---
+++ build/IHazGitUserName.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+using Nuke.Common;
+
+using static Nuke.Common.Tools.Git.GitTasks;
+
+interface IHazGitUserName : INukeBuild {
+    string UserName => Git("config --global user.name").First().Text;
+}
