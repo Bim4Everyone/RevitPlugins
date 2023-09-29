63c30e02a9038603e20305badd32f76b0d6efea6
Vladimir Stroganov <vlastroganov@gmail.com>
2023-09-29 18:02:03 +0300

    Добавить провайдеры классов для размещения отверстия КР

---
+++ RevitOpeningPlacement/Models/RealOpeningKrPlacement/Providers/ManyOpeningArTasksParameterGettersProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+
+using RevitOpeningPlacement.Models.Interfaces;
+using RevitOpeningPlacement.Models.RealOpeningKrPlacement.ParameterGetters;
+using RevitOpeningPlacement.OpeningModels;
+
+namespace RevitOpeningPlacement.Models.RealOpeningKrPlacement.Providers {
+    /// <summary>
+    /// Класс, предоставляющий провайдеров для параметров размещаемого отверстия КР
+    /// </summary>
+    internal class ManyOpeningArTasksParameterGettersProvider {
+        private readonly Element _host;
+        private readonly ICollection<OpeningArTaskIncoming> _incomingTasks;
+        private readonly IPointFinder _pointFinder;
+
+
+        /// <summary>
+        /// Конструктор класса, предоставляющего провайдеров для параметров размещаемого отверстия КР
+        /// </summary>
+        /// <param name="host">Основа для отверстия КР</param>
+        /// <param name="incomingTasks">Входящие задания от АР</param>
+        /// <param name="pointFinder">Провайдер точки вставки отверстия КР</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public ManyOpeningArTasksParameterGettersProvider(Element host, ICollection<OpeningArTaskIncoming> incomingTasks, IPointFinder pointFinder) {
+            if(host is null) { throw new ArgumentNullException(nameof(host)); }
+            if(!((host is Wall) || (host is Floor))) { throw new ArgumentException(nameof(host)); }
+            if(incomingTasks is null) { throw new ArgumentNullException(nameof(incomingTasks)); }
+            if(incomingTasks.Count < 1) { throw new ArgumentException(nameof(incomingTasks)); }
+            if(pointFinder is null) { throw new ArgumentNullException(nameof(pointFinder)); }
+
+            _host = host;
+            _incomingTasks = incomingTasks;
+            _pointFinder = pointFinder;
+        }
+
+
+        public IParametersGetter GetParametersGetter() {
+            if(_host is Floor) {
+                return new ManyOpeningArTasksInFloorParameterGetter(_incomingTasks);
+            } else if(_host is Wall) {
+                return new ManyOpeningArTasksInWallParameterGetter(_incomingTasks, _pointFinder);
+            } else {
+                throw new ArgumentException(nameof(_host));
+            }
+        }
+    }
+}
---
+++ RevitOpeningPlacement/Models/RealOpeningKrPlacement/Providers/SingleOpeningArTaskFamilySymbolProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Autodesk.Revit.DB;
+
+using RevitOpeningPlacement.OpeningModels;
+
+namespace RevitOpeningPlacement.Models.RealOpeningKrPlacement.Providers {
+    /// <summary>
+    /// Класс, предоставляющий типоразмер семейства чистового отверстия КР по одному заданию на отверстие от АР
+    /// </summary>
+    internal class SingleOpeningArTaskFamilySymbolProvider {
+        private readonly RevitRepository _revitRepository;
+        private readonly Element _host;
+        private readonly OpeningArTaskIncoming _incomingTask;
+
+
+        /// <summary>
+        /// Конструктор класса, предоставляющего типоразмер семейства чистового отверстия КР по одному заданию на отверстие от АР
+        /// </summary>
+        /// <param name="revitRepository">Репозиторий активного документа КР, в котором будет происходить размещение чистового семейства отверстия</param>
+        /// <param name="host">Хост чистового отверстия - стена или перекрытие</param>
+        /// <param name="incomingTask">Входящее задание на отверстие от АР</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public SingleOpeningArTaskFamilySymbolProvider(RevitRepository revitRepository, Element host, OpeningArTaskIncoming incomingTask) {
+            _revitRepository = revitRepository ?? throw new ArgumentNullException(nameof(revitRepository));
+            if(host is null) {
+                throw new ArgumentNullException(nameof(host));
+            }
+            if(!(host is Wall) || !(host is Floor)) {
+                throw new ArgumentException(nameof(host));
+            }
+            _host = host;
+            _incomingTask = incomingTask ?? throw new ArgumentNullException(nameof(incomingTask));
+        }
+
+
+        /// <exception cref="ArgumentException"></exception>
+        public FamilySymbol GetFamilySymbol() {
+            if(_host is Wall) {
+                switch(_incomingTask.OpeningType) {
+                    case OpeningType.WallRound:
+                    return _revitRepository.GetOpeningRealKrType(OpeningType.WallRound);
+                    case OpeningType.WallRectangle:
+                    default:
+                    return _revitRepository.GetOpeningRealKrType(OpeningType.WallRectangle);
+                }
+            } else if(_host is Floor) {
+                switch(_incomingTask.OpeningType) {
+                    default:
+                    return _revitRepository.GetOpeningRealKrType(OpeningType.FloorRectangle);
+                }
+            } else {
+                throw new ArgumentException(nameof(_host));
+            }
+        }
+    }
+}
