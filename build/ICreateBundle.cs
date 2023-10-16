e3ab41607d25996b5dd21a5ccb9da488060a5b12
dosymep <dosymep@gmail.com>
2023-10-16 15:36:15 +0300

    Исправил создания бандла

--- build/ICreateBundle.cs
+++ build/ICreateBundle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing.Imaging;
+using System.IO;
 
 using Nuke.Common;
 using Nuke.Common.IO;
@@ -11,14 +12,16 @@ interface ICreateBundle : IHazPluginName, IHazOutput, IHazTemplate, IHazGitUserN
     [Parameter] Uri IconUrl => TryGetValue(() => IconUrl);
     [Parameter] string BundleName => TryGetValue(() => BundleName);
     [Parameter] BundleType BundleType => TryGetValue(() => BundleType);
-    [Parameter] AbsolutePath BundleOutput => TryGetValue(() => BundleOutput) ?? DefaultOutput;
+    [Parameter] string BundleOutput => TryGetValue(() => BundleOutput) ?? DefaultOutput;
 
     Uri DefaultIconUrl => new("https://icons8.com/icon/22361/money-bag");
     string DefaultBundleName => "MyAwesomeBundle";
     BundleType DefaultBundleType => BundleType.InvokeButton;
 
     AbsolutePath TemplateBundle => TemplateDirectory + BundleType.ExtensionWithDot;
-    AbsolutePath BundleDirectory => BundleOutput / BundleName + BundleType.ExtensionWithDot;
+
+    AbsolutePath BundleDirectory => NukeBuildExtensions.GetExtensionsPath(
+        Path.Combine(BundleOutput, BundleName + BundleType.ExtensionWithDot));
 
     IconSize IconSize => IconSize.Size96;
     string UriIconFormat => "https://img.icons8.com/?size={0}&id={1}&format=png";
@@ -34,8 +37,6 @@ interface ICreateBundle : IHazPluginName, IHazOutput, IHazTemplate, IHazGitUserN
         .OnlyWhenStatic(() =>
             !IconUrl.AbsolutePath.StartsWith(@"https://icons8.com/icon/"), "Bundle icon must be from icons8 site")
         .Executes(async () => {
-            AbsolutePath bundleDirectory = NukeBuildExtensions.GetExtensionsPath(BundleDirectory);
-            
             Log.Debug("PluginName: {PluginName}", PluginName);
             Log.Debug("TemplateName: {TemplateName}", TemplateName);
             Log.Debug("TemplateBundleDirectory: {TemplateBundleDirectory}", TemplateBundle);
@@ -44,10 +45,9 @@ interface ICreateBundle : IHazPluginName, IHazOutput, IHazTemplate, IHazGitUserN
             Log.Debug("BundleName: {BundleName}", BundleName);
             Log.Debug("BundleType: {BundleType}", BundleType);
             Log.Debug("BundleDirectory: {BundleDirectory}", BundleDirectory);
-            Log.Debug("bundleDirectory: {BundleDirectory}", bundleDirectory);
 
-            bundleDirectory.CreateDirectory();
-            CopyDirectory(TemplateBundle, bundleDirectory,
+            BundleDirectory.CreateDirectory();
+            CopyDirectory(TemplateBundle, BundleDirectory,
                 new Dictionary<string, string>() {
                     {"${{ gen.bundle_name }}", BundleName},
                     {"${{ gen.plugin_name }}", PluginName},
@@ -57,7 +57,7 @@ interface ICreateBundle : IHazPluginName, IHazOutput, IHazTemplate, IHazGitUserN
                     {"${{ gen.max_revit_version }}", MaxReleaseVersion.ToString()}
                 });
 
-            await IconSize.CreateImages(GetImageUri(), bundleDirectory / "icon.png");
+            await IconSize.CreateImages(GetImageUri(), BundleDirectory / "icon.png");
         });
 
     Uri GetImageUri() {
