4eb4125f84f2b0cd695c9e8a4f7766faed1a31ab
divin_n <nikitad93@mail.ru>
2023-10-20 14:58:16 +0300

    В плагине RevitPylonDocumentation подчищены свойства и поля настроек, исправлено использование директив препроцессора

--- RevitPylonDocumentation/Models/UnitUtilsHelper.cs
+++ RevitPylonDocumentation/Models/UnitUtilsHelper.cs
@@ -11,31 +11,24 @@ namespace RevitPylonDocumentation.Models {
         public static double ConvertFromInternalValue(double value) {
             // Перевод во внутренние единицы Revit
 
-#if REVIT_2021_OR_LESS
-            DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
-            double convertedValue = UnitUtils.ConvertFromInternalUnits(value, unitType);
-#else
+#if REVIT_2021_OR_GREATER
             ForgeTypeId unitType = UnitTypeId.Millimeters;
-            double convertedValue = UnitUtils.ConvertFromInternalUnits(value, unitType);
+#else
+            DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
 #endif
-            return convertedValue;
+            return UnitUtils.ConvertFromInternalUnits(value, unitType);
         }
-
 
 
         public static double ConvertToInternalValue(double value) {
             // Перевод во внутренние единицы Revit
 
-#if REVIT_2020
-            DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
-            double convertedValue = UnitUtils.ConvertToInternalUnits(value, unitType);
-#elif REVIT_2021_OR_GREATER
+#if REVIT_2021_OR_GREATER
             ForgeTypeId unitType = UnitTypeId.Millimeters;
-            double convertedValue = UnitUtils.ConvertToInternalUnits(value, unitType);
 #else
-                TaskDialog.Show("Ошибка!", ex.Message);
+            DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
 #endif
-            return convertedValue;
+            return UnitUtils.ConvertToInternalUnits(value, unitType);
         }
     }
 }
