614fb6ed313744c231f48cc14dc1ecfa65437330
Vladimir Stroganov <vlastroganov@gmail.com>
2023-10-20 12:04:00 +0300

    Скорректировать GetHashCode

--- RevitClashDetective/ViewModels/Navigator/ClashViewModel.cs
+++ RevitClashDetective/ViewModels/Navigator/ClashViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 using Autodesk.Revit.DB;
 
-using dosymep.Revit;
 using dosymep.WPF.ViewModels;
 
 using RevitClashDetective.Models;
@@ -35,7 +34,7 @@ namespace RevitClashDetective.ViewModels.Navigator {
 
         public ClashStatus ClashStatus {
             get => _clashStatus;
-            set => this.RaiseAndSetIfChanged(ref _clashStatus, value);
+            set => RaiseAndSetIfChanged(ref _clashStatus, value);
         }
 
         public string FirstName { get; }
@@ -55,16 +54,6 @@ namespace RevitClashDetective.ViewModels.Navigator {
         public string SecondCategory { get; set; }
         public ClashModel Clash { get; }
 
-#if REVIT_2023_OR_LESS
-        public IEnumerable<ElementId> GetElementIds(string docTitle) {
-            if(docTitle.Contains(FirstDocumentName)) {
-                yield return new ElementId(Clash.MainElement.Id);
-            }
-            if(docTitle.Contains(SecondDocumentName)) {
-                yield return new ElementId(Clash.OtherElement.Id);
-            }
-        }
-#else
         public IEnumerable<ElementId> GetElementIds(string docTitle) {
             if(docTitle.Contains(FirstDocumentName)) {
                 yield return Clash.MainElement.Id;
@@ -73,7 +62,6 @@ namespace RevitClashDetective.ViewModels.Navigator {
                 yield return Clash.OtherElement.Id;
             }
         }
-#endif
 
 
         public ClashModel GetClashModel() {
@@ -88,13 +76,8 @@ namespace RevitClashDetective.ViewModels.Navigator {
         public override int GetHashCode() {
             int hashCode = 635569250;
             hashCode = hashCode * -1521134295 + ClashStatus.GetHashCode();
-#if REVIT_2023_OR_LESS
-            hashCode = hashCode * -1521134295 + EqualityComparer<int>.Default.GetHashCode(Clash.MainElement.Id);
-            hashCode = hashCode * -1521134295 + EqualityComparer<int>.Default.GetHashCode(Clash.OtherElement.Id);
-#else
-            hashCode = hashCode * -1521134295 + EqualityComparer<int>.Default.GetHashCode((int) Clash.MainElement.Id.GetIdValue());
-            hashCode = hashCode * -1521134295 + EqualityComparer<int>.Default.GetHashCode((int) Clash.OtherElement.Id.GetIdValue());
-#endif
+            hashCode = hashCode * -1521134295 + EqualityComparer<ElementId>.Default.GetHashCode(Clash.MainElement.Id);
+            hashCode = hashCode * -1521134295 + EqualityComparer<ElementId>.Default.GetHashCode(Clash.OtherElement.Id);
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FirstName);
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FirstDocumentName);
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FirstLevel);
