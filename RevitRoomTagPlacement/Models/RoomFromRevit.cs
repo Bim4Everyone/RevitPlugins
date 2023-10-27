f9db6dfbac100d3e263bf9c5a98d564d1351932e
sssenya <senia7@bk.ru>
2023-10-27 17:31:36 +0300

    Создан класс для помещений из Revit.

---
+++ RevitRoomTagPlacement/Models/RoomFromRevit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+
+namespace RevitRoomTagPlacement.Models {
+    internal class RoomFromRevit {
+        private Room _room;
+        private ElementId _linkId;
+        public RoomFromRevit(Room room, ElementId linkId = null) {
+            _room = room;
+            _linkId = linkId;
+        }
+
+        public bool IsFromLink { get; set; }
+        public Room RoomObject => _room;
+        public ElementId LinkId => _linkId;
+    }
+}
