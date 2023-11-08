8a03eb5711e38b16b2d5c61ceea3ee5b91dae939
divin_n <nikitad93@mail.ru>
2023-11-08 17:39:11 +0300

    Переделан конструктор класса ParameterGroupHelper

--- RevitFamilyParameterAdder/Models/DefaultParam.cs
+++ RevitFamilyParameterAdder/Models/DefaultParam.cs
@@ -9,16 +9,30 @@ using Autodesk.Revit.DB;
 
 namespace RevitFamilyParameterAdder.Models {
     internal class DefaultParam {
-        public DefaultParam(string name, object builtInParameterGroup) {
+
+#if REVIT_2023_OR_LESS
+        public DefaultParam(string name, BuiltInParameterGroup builtInParameterGroup) {
             ParamName = name;
             BINParameterGroup = new ParameterGroupHelper(builtInParameterGroup);
         }
 
-        public DefaultParam(string name, object builtInParameterGroup, string formula) {
+        public DefaultParam(string name, BuiltInParameterGroup builtInParameterGroup, string formula) {
             ParamName = name;
             BINParameterGroup = new ParameterGroupHelper(builtInParameterGroup);
             Formula = formula;
         }
+#else
+        public DefaultParam(string name, ForgeTypeId builtInParameterGroup) {
+            ParamName = name;
+            BINParameterGroup = new ParameterGroupHelper(builtInParameterGroup);
+        }
+
+        public DefaultParam(string name, ForgeTypeId builtInParameterGroup, string formula) {
+            ParamName = name;
+            BINParameterGroup = new ParameterGroupHelper(builtInParameterGroup);
+            Formula = formula;
+        }
+#endif
 
         public string ParamName { get; set; }
         public ParameterGroupHelper BINParameterGroup { get; set; }
