13dfbdc9cb04b9874ccdfa80b2d789056062c322
Vladimir Stroganov <vlastroganov@gmail.com>
2023-11-14 10:17:20 +0300

    Переопределить equals в RevitDocument

--- RevitMepTotals/Models/RevitDocument.cs
+++ RevitMepTotals/Models/RevitDocument.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 using RevitMepTotals.Models.Interfaces;
 
 namespace RevitMepTotals.Models {
-    class RevitDocument : IDocument {
+    internal class RevitDocument : IDocument, IEquatable<RevitDocument> {
         private readonly FileInfo _file;
 
         public RevitDocument(FileInfo file) {
@@ -14,5 +15,25 @@ namespace RevitMepTotals.Models {
         public string Name => _file.Name;
 
         public string Path => _file.FullName;
+
+
+        public override bool Equals(object obj) {
+            return Equals(obj as RevitDocument);
+        }
+
+        public bool Equals(RevitDocument other) {
+            if(ReferenceEquals(null, other)) { return false; }
+            if(ReferenceEquals(this, other)) { return true; }
+
+            return Name == other.Name
+                && Path == other.Path;
+        }
+
+        public override int GetHashCode() {
+            int hashCode = 193482316;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Path);
+            return hashCode;
+        }
     }
 }
