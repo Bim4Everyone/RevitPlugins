d5ddf1cae4d6af3a2315773d3b0a942bb6de5503
sssenya <senia7@bk.ru>
2023-11-14 15:40:15 +0300

    Добавлена возможность установки нескольких CheckBox в списке групп через Shift.

--- RevitRoomTagPlacement/Views/MainWindow.xaml.cs
+++ RevitRoomTagPlacement/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 using System.Windows;
+using System.Windows.Controls;
+
+using RevitRoomTagPlacement.ViewModels;
 
 namespace RevitRoomTagPlacement.Views {
     public partial class MainWindow {
@@ -15,6 +18,22 @@ namespace RevitRoomTagPlacement.Views {
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
             DialogResult = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+            ChangeSelected(true);
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            ChangeSelected(false);
+        }
+
+        private void ChangeSelected(bool state) {
+            var listBox = (ListBox) FindName("RoomGroups");
+            var groups = listBox.SelectedItems;
+            foreach(RoomGroupViewModel group in groups) {
+                group.IsChecked = state;
+            }
         }
     }
 }
