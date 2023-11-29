da21535d80dedf31a3b271baa953d641a50f0e11
dosymep <dosymep@gmail.com>
2023-11-29 16:19:03 +0300

    Создал новую структуру плагина

---
+++ RevitServerFolders/FileServerExportCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+using Autodesk.Revit.Attributes;
+
+using dosymep.Bim4Everyone;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using dosymep.Bim4Everyone.SimpleServices;
+
+using Ninject;
+
+using RevitServerFolders.Models;
+using RevitServerFolders.ViewModels;
+using RevitServerFolders.Views;
+
+namespace RevitServerFolders {
+    [Transaction(TransactionMode.Manual)]
+    internal sealed class FileServerExportCommand : BasePluginCommand {
+        public FileServerExportCommand() {
+            PluginName = "Экспорт rvt файлов из RS";
+        }
+
+        protected override void Execute(UIApplication uiApplication) {
+            using(IKernel kernel = uiApplication.CreatePlatformServices()) {
+                kernel.Bind<RevitRepository>()
+                    .ToSelf()
+                    .InSingletonScope();
+					
+                kernel.Bind<PluginConfig>()
+                    .ToMethod(c => PluginConfig.GetPluginConfig());
+				
+                kernel.Bind<MainViewModel>().ToSelf();
+                kernel.Bind<MainWindow>().ToSelf()
+                    .WithPropertyValue(nameof(Window.Title), PluginName)
+                    .WithPropertyValue(nameof(Window.DataContext),
+                        c => c.Kernel.Get<MainViewModel>());
+				
+                Notification(kernel.Get<MainWindow>());
+            }
+        }
+    }
+}
---
+++ RevitServerFolders/NavisworksExportCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+
+using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.SimpleServices;
+
+using Ninject;
+
+using RevitServerFolders.Models;
+using RevitServerFolders.ViewModels;
+using RevitServerFolders.Views;
+
+namespace RevitServerFolders {
+    [Transaction(TransactionMode.Manual)]
+    internal sealed class NavisworksExportCommand : BasePluginCommand {
+        public NavisworksExportCommand() {
+            PluginName = "Экспорт вида Navisworks";
+        }
+
+        protected override void Execute(UIApplication uiApplication) {
+            using(IKernel kernel = uiApplication.CreatePlatformServices()) {
+                kernel.Bind<RevitRepository>()
+                    .ToSelf()
+                    .InSingletonScope();
+					
+                kernel.Bind<PluginConfig>()
+                    .ToMethod(c => PluginConfig.GetPluginConfig());
+				
+                kernel.Bind<MainViewModel>().ToSelf();
+                kernel.Bind<MainWindow>().ToSelf()
+                    .WithPropertyValue(nameof(Window.Title), PluginName)
+                    .WithPropertyValue(nameof(Window.DataContext),
+                        c => c.Kernel.Get<MainViewModel>());
+				
+                Notification(kernel.Get<MainWindow>());
+            }
+        }
+    }
+}
