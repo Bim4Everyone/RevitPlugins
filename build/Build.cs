06760417d9f7c12241809eef31c14a78c127e998
Vladimir Stroganov <vlastroganov@gmail.com>
2024-02-28 06:09:03 -0800

    Добавить установку pyRevit

--- build/Build.cs
+++ build/Build.cs
@@ -26,7 +26,7 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
     EnableGitHubToken = true,
     OnPushIncludePaths = new[] {"RevitPlugins/**", "**.cs"}
 )]
-class Build : NukeBuild, ICompile, ICreateScript, IPluginCreate, ICreateBundle, ICreateProfile {
+class Build : NukeBuild, ICompile, ICreateScript, IPluginCreate, ICreateBundle, ICreateProfile, IPyRevitInstall {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
---
+++ build/IPyRevitInstall.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+
+using Nuke.Common;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.CloudFoundry;
+
+using Serilog;
+
+using static Nuke.Common.Tools.CloudFoundry.CloudFoundryTasks;
+using static Nuke.Common.Tools.PowerShell.PowerShellTasks;
+
+interface IPyRevitInstall : INukeBuild {
+    [Parameter] Uri PyRevitInstallerUrl => TryGetValue(() => PyRevitInstallerUrl);
+
+    Target InstallPyRevit => _ => _
+        .Requires(() => PyRevitInstallerUrl)
+        .OnlyWhenStatic(() => !IsServerBuild, "TODO after testing make only on ServerBuild")
+        .Executes(() => {
+            Log.Debug($"Download pyRevit installer from: {PyRevitInstallerUrl}");
+
+            string pyRevitInstallerFile = Path.Combine(RootDirectory, "pyRevitInstaller.exe");
+            PowerShell($"curl.exe -L {PyRevitInstallerUrl} -o {pyRevitInstallerFile} -s").EnsureOnlyStd();
+
+            Assert.FileExists(pyRevitInstallerFile, "pyRevitInstaller didn't download");
+
+            Log.Debug($"Run pyRevit installer from: {pyRevitInstallerFile}");
+            PowerShell($"start {pyRevitInstallerFile} /silent -Wait");
+
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string pyRevitLabsJsonDllPath = Path.Combine(appData, @"pyRevit-Master\bin\pyRevitLabs.Json.dll");
+            Assert.FileExists(pyRevitLabsJsonDllPath, "pyRevitLabs.Json.dll didn't install");
+        })
+    ;
+}
