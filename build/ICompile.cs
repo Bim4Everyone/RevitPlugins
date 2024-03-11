db4f4c9eb9296d0fe56901f28b23c12db0543fa1
Vladimir Stroganov <vlastroganov@gmail.com>
2024-03-11 00:46:32 -0700

    Добавлен Workflow для публикации dll плагинов в платформу (#33)

--- build/ICompile.cs
+++ build/ICompile.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+using System;
 
 using Nuke.Common;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
 using Nuke.Components;
 
@@ -13,7 +10,14 @@ using Serilog;
 
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-interface ICompile : IClean, IHazSolution, IHazGitVersion, IHazGitRepository, IHazRevitVersion, IHazConfigurations {
+interface ICompile : IClean,
+    IHazSolution,
+    IHazGitVersion,
+    IHazGitRepository,
+    IHazRevitVersion,
+    IHazConfigurations,
+    ICloneExtensions,
+    IPublishArtifacts {
     Target Compile => _ => _
         .DependsOn(Clean)
         .Requires(() => PluginName)
@@ -39,7 +43,8 @@ interface ICompile : IClean, IHazSolution, IHazGitVersion, IHazGitRepository, IH
         });
 
     Target Publish => _ => _
-        .DependsOn(Clean)
+        .DependsOn(Clean, CloneRepos)
+        .Triggers(PushPluginDll)
         .Requires(() => PluginName)
         .Requires(() => PublishDirectory)
         .Executes(() => {
@@ -60,7 +65,8 @@ interface ICompile : IClean, IHazSolution, IHazGitVersion, IHazGitRepository, IH
                     .SetSimpleVersion(Versioning, revitVersion)
                     .SetProperty("RevitVersion", (int) revitVersion)
                     .SetProperty("AssemblyName", $"{PluginName}_{revitVersion}")));
-        });
+        })
+    ;
 
     sealed Configure<DotNetBuildSettings> CompileSettingsBase => _ => _
         .DisableNoRestore()
