31144b33cfac3191bd2ebd05dc29b36ec0df65c0
dosymep <biseuv@yandex.ru>
2024-03-18 14:40:54 +0300

    Добавил установку пользователя

--- build/Build.PublishArtifacts.cs
+++ build/Build.PublishArtifacts.cs
@@ -15,15 +15,19 @@ partial class Build {
         .OnlyWhenStatic(() => IsServerBuild, "Target should be run only on server")
         .OnlyWhenDynamic(() => Params.ExtensionDirectory.DirectoryExists(), "ExtensionDirectory does not exists")
         .Executes(() => {
+            Log.Debug("Set git user");
+            Git($"-C \"{Params.ExtensionDirectory}\" config --global user.name \"dosymep\"");
+            Git($"-C \"{Params.ExtensionDirectory}\" config --global user.email \"dosymep@gmail.com\"");
+
             Log.Debug("Execute git commands in directory: {@ExtensionDirectory}", Params.ExtensionDirectory.ToString());
             Git($"-C \"{Params.ExtensionDirectory}\" switch -c {Params.NukeBranchName} -q");
             Git($"-C \"{Params.ExtensionDirectory}\" status");
-            
+
             Log.Debug("Commit updated *.dll");
             Git($"-C \"{Params.ExtensionDirectory}\" add *.dll");
             Git($"-C \"{Params.ExtensionDirectory}\" status");
             Git($"-C \"{Params.ExtensionDirectory}\" commit -m \"Обновление библиотек плагина \"\"\"{Params.PluginName}\"\"\"\"");
-           
+
             Log.Debug("Push changes to origin");
             Git($"-C \"{Params.ExtensionDirectory}\" push -q -u origin {Params.NukeBranchName}");
         });
