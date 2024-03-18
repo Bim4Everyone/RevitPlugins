c8bc9041dd4abf0cf632db69f3435e2dd31a3634
dosymep <biseuv@yandex.ru>
2024-03-18 15:24:51 +0300

    Добавил случайные изменения

--- src/RevitPlugins/RevitPluginsCommand.cs
+++ src/RevitPlugins/RevitPluginsCommand.cs
@@ -29,7 +29,7 @@ namespace RevitPlugins {
     [Transaction(TransactionMode.Manual)]
     public class RevitPluginsCommand : BasePluginCommand {
         public RevitPluginsCommand() {
-            PluginName = "Пример команды";
+            PluginName = "Пример команды test";
         }
 
         protected override void Execute(UIApplication uiApplication) {
