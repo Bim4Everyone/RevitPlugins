b5314a3f8bbda7152bc7dbd0e4907db673901911
Vladimir Stroganov <vlastroganov@gmail.com>
2024-05-27 12:45:53 +0300

    Дополнить комментарии

--- src/RevitFinishingWalls/Models/RevitRepository.cs
+++ src/RevitFinishingWalls/Models/RevitRepository.cs
@@ -296,11 +296,12 @@ namespace RevitFinishingWalls.Models {
         }
 
         /// <summary>
-        /// Проверяет, является ли второй отрезок продолжением первого.
+        /// Проверяет, является ли вторая линия продолжением первой.
         /// </summary>
-        /// <param name="first"></param>
-        /// <param name="second"></param>
-        /// <returns></returns>
+        /// <param name="first">Первая линия</param>
+        /// <param name="second">Вторая линия</param>
+        /// <returns>Вторая линия является продолжением первой только если обе линии - отрезки 
+        /// и если начало второго отрезка - это конец первого отрезка или наоборот</returns>
         /// <exception cref="ArgumentNullException"></exception>
         private bool IsContinuation(Curve first, Curve second) {
             if(first is null) { throw new ArgumentNullException(nameof(first)); }
@@ -315,6 +316,13 @@ namespace RevitFinishingWalls.Models {
             }
         }
 
+        /// <summary>
+        /// Строит отрезок с начальной точкой в начале первой линии и конечной точкой в конце второй линии
+        /// </summary>
+        /// <param name="curveFirst">Первая линия</param>
+        /// <param name="curveSecond">Вторая линия</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         private Curve CombineCurves(Curve curveFirst, Curve curveSecond) {
             if(curveFirst is null) { throw new ArgumentNullException(nameof(curveFirst)); }
             if(curveSecond is null) { throw new ArgumentNullException(nameof(curveSecond)); }
