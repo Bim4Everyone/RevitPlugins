9743768062ab8de31576ce89d1631833b9b9f2ef
Vladimir Stroganov <vlastroganov@gmail.com>
2024-05-30 12:22:08 +0300

    Удалить неиспользуемый код

--- src/RevitFinishingWalls/Views/ErrorWindow.xaml.cs
+++ src/RevitFinishingWalls/Views/ErrorWindow.xaml.cs
@@ -2,15 +2,10 @@ namespace RevitFinishingWalls.Views {
     public partial class ErrorWindow {
         public ErrorWindow() {
             InitializeComponent();
-            Loaded += ThemedPlatformWindow_Loaded;
         }
 
 
         public override string PluginName => nameof(RevitFinishingWalls);
         public override string ProjectConfigName => nameof(ErrorWindow);
-
-        private void ThemedPlatformWindow_Loaded(object sender, System.Windows.RoutedEventArgs e) {
-            //_dgErrors.GroupBy(nameof(ErrorViewModel.Message));
-        }
     }
 }
