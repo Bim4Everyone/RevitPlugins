ed87d35ff5609ca776533b855709039e62e18c59
dosymep <dosymep@gmail.com>
2024-07-02 13:10:57 +0300

    Обновил шаблон

--- .github/templates/RevitPluginTemplate/ViewModels/MainViewModel.cs
+++ .github/templates/RevitPluginTemplate/ViewModels/MainViewModel.cs
@@ -9,13 +9,19 @@ namespace RevitPluginTemplate.ViewModels {
     internal class MainViewModel : BaseViewModel {
         private readonly PluginConfig _pluginConfig;
         private readonly RevitRepository _revitRepository;
+        private readonly ILocalizationService _localizationService;
 
         private string _errorText;
         private string _saveProperty;
 
-        public MainViewModel(PluginConfig pluginConfig, RevitRepository revitRepository) {
+        public MainViewModel(
+            PluginConfig pluginConfig, 
+            RevitRepository revitRepository, 
+            ILocalizationService localizationService) {
+            
             _pluginConfig = pluginConfig;
             _revitRepository = revitRepository;
+            _localizationService = localizationService;
 
             LoadViewCommand = RelayCommand.Create(LoadView);
             AcceptViewCommand = RelayCommand.Create(AcceptView, CanAcceptView);
@@ -44,7 +50,7 @@ namespace RevitPluginTemplate.ViewModels {
         
         private bool CanAcceptView() {
             if(string.IsNullOrEmpty(SaveProperty)) {
-                ErrorText = "Введите значение сохраняемого свойства.";
+                ErrorText =  _localizationService.GetLocalizedString("MainWindow.HelloCheck");
                 return false;
             }
 
@@ -55,7 +61,7 @@ namespace RevitPluginTemplate.ViewModels {
         private void LoadConfig() {
             RevitSettings setting = _pluginConfig.GetSettings(_revitRepository.Document);
 
-            SaveProperty = setting?.SaveProperty ??  _localizationService.GetLocalizedString("MainWindow.Hello");
+            SaveProperty = setting?.SaveProperty ?? _localizationService.GetLocalizedString("MainWindow.Hello");
         }
 
         private void SaveConfig() {
