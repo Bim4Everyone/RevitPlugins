f1e7b32ede0eb25ef174c453aafc4b96ab663592
dosymep <dosymep@gmail.com>
2024-07-18 10:09:52 +0300

    Добавил отображение для страницы About

--- src/RevitPlatformSettings/ViewModels/Settings/AboutSettingsViewModel.cs
+++ src/RevitPlatformSettings/ViewModels/Settings/AboutSettingsViewModel.cs
@@ -9,6 +9,24 @@ namespace RevitPlatformSettings.ViewModels.Settings {
             IPlatformSettingsService platformSettingsService)
             : base(id, parentId, settingsName) {
             _platformSettingsService = platformSettingsService;
+
+            ImagePath = _platformSettingsService.CorpSettings?.ImagePath 
+                        ?? "/dosymep.Bim4Everyone;component/assets/Bim4Everyone.png";
+            
+            PlatformName = _platformSettingsService.CorpSettings?.Name;
+            PlatformPageUrl = _platformSettingsService.SocialsSettings?.PlatformPageUrl;
+            
+            NewsChatUrl = _platformSettingsService.SocialsSettings?.NewsChatUrl;
+            MainChatUrl = _platformSettingsService.SocialsSettings?.MainChatUrl;
+            DownloadLinkUrl = _platformSettingsService.SocialsSettings?.DownloadLinkUrl;
         }
+
+        public string ImagePath { get; }
+        public string PlatformName { get; }
+        public string PlatformPageUrl { get; }
+        
+        public string NewsChatUrl { get; }
+        public string MainChatUrl { get; }
+        public string DownloadLinkUrl { get; }
     }
 }
