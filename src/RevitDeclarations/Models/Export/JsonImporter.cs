bd85367a9c84598a4e1aaf13db7d5eb86e3eb008
Tikhomirov_am <senia7@bk.ru>
2024-07-19 10:52:59 +0300

    Исправлен класс импорта json

--- src/RevitDeclarations/Models/Export/JsonImporter.cs
+++ src/RevitDeclarations/Models/Export/JsonImporter.cs
@@ -1,37 +1,20 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 
 using dosymep.Bim4Everyone.SimpleServices;
 using dosymep.SimpleServices;
 
-using pyRevitLabs.Json;
-
 namespace RevitDeclarations.Models {
     internal class JsonImporter<T> {
+        private readonly ISerializationService _serializationService =
+            ServicesProvider.GetPlatformService<ISerializationService>();
         public string ErrorInfo { get; set; }
 
         public List<T> Import(string path) {
-            List<T> elements = new List<T>();
-
             using(StreamReader file = File.OpenText(path)) {
-                try {                  
-                    ISerializationService service = ServicesProvider.GetPlatformService<ISerializationService>();
-                    string allText = file.ReadToEnd();
-                    return service.Deserialize<List<T>>(allText);
-                }
-                catch(JsonSerializationException e) {
-                    ErrorInfo = $"Ошибка сериализации json файла: {e.Message}";
-                }
-                catch(JsonReaderException e) {
-                    ErrorInfo = $"Ошибка в синтаксисе json файла: {e.Message}";
-                }
-                catch(Exception e) {
-                    ErrorInfo = e.Message;
-                }
+                string allText = file.ReadToEnd();
+                return _serializationService.Deserialize<List<T>>(allText);
             }
-
-            return elements;
         }
     }
 }
