cd89c91055cf31f7ec9e9fdaeacb26d369d6cc0a
Tikhomirov_am <senia7@bk.ru>
2024-09-02 18:05:52 +0300

    Добавлен отдельный класс для экспорта в Excel

--- src/RevitDeclarations/Models/Export/DeclarationExporter.cs
+++ src/RevitDeclarations/Models/Export/DeclarationExporter.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 using Microsoft.Office.Interop.Excel;
+
+using RevitDeclarations.Models.Export.Exporters;
 
 namespace RevitDeclarations.Models {
     internal class DeclarationExporter {
@@ -15,43 +18,10 @@ namespace RevitDeclarations.Models {
         }
 
         public void ExportToExcel(string path, ExcelTableData tableData) {
-            /* Releasing all COM objects was made on the basis of the article:
-             * https://www.add-in-express.com/creating-addins-blog/release-excel-com-objects/
-             */
-            Type officeType = Type.GetTypeFromProgID("Excel.Application.16");
-
-            if(officeType == null) {
-                TaskDialog.Show("Ошибка", "Excel 2016 не найден на компьютере");
-                return;
-            }
-
-            Application excelApp = new Application {
-                Visible = false,
-                DisplayAlerts = false
-            };
-            Workbooks workBooks;
-            Workbook workBook;
-            Sheets workSheets;
-            Worksheet workSheet;
-
             try {
-                workBooks = excelApp.Workbooks;
-                workBook = workBooks.Add();
-                workSheets = workBook.Worksheets;
-                workSheet = (Worksheet) workSheets["Лист1"];
-
-                new ExcelTableCreator(tableData, _settings).Create(workSheet);
+                ExcelExporter excel = new ExcelExporter();
+                excel.Export(path, tableData, _settings);
                 TaskDialog.Show("Декларации", "Файл Excel создан");
-
-                workBook.SaveAs(path);
-                workBook.Close(false);
-
-                excelApp.Quit();
-                if(workSheet != null) { Marshal.ReleaseComObject(workSheet); }
-                if(workSheets != null) { Marshal.ReleaseComObject(workSheets); }
-                if(workBook != null) { Marshal.ReleaseComObject(workBook); }
-                if(workBooks != null) { Marshal.ReleaseComObject(workBooks); }
-                if(excelApp != null) { Marshal.ReleaseComObject(excelApp); }
             } catch(Exception e) {
                 var taskDialog = new TaskDialog("Ошибка выгрузки") {
                     CommonButtons = TaskDialogCommonButtons.No | TaskDialogCommonButtons.Yes,
@@ -61,10 +31,10 @@ namespace RevitDeclarations.Models {
 
                 TaskDialogResult dialogResult = taskDialog.Show();
 
-                if(dialogResult == TaskDialogResult.Ok) {
+                if(dialogResult == TaskDialogResult.Yes) {
                     ExportToCSV(path, tableData);
                 }
-            } 
+            }
         }
 
         public void ExportToJson(string path, IEnumerable<Apartment> apartments) {
@@ -74,11 +44,14 @@ namespace RevitDeclarations.Models {
         }
 
         public void ExportToCSV(string path, ExcelTableData tableData) {
+            CsvTableCreator csvTableCreator = new CsvTableCreator(tableData, _settings);
+
             CsvExporter exporter = new CsvExporter();
 
-            ExcelTableData tableData1 = tableData;
+            string declarationData = csvTableCreator.Create();
 
-            exporter.Export(path, "");
+            exporter.Export(path, declarationData);
+            TaskDialog.Show("Декларации", "Файл CSV создан");
         }
     }
 }
---
+++ src/RevitDeclarations/Models/Export/ExcelExporter.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Runtime.InteropServices;
+
+using Autodesk.Revit.UI;
+
+using Microsoft.Office.Interop.Excel;
+
+namespace RevitDeclarations.Models.Export.Exporters {
+    internal class ExcelExporter {
+        public void Export(string path, ExcelTableData tableData, DeclarationSettings settings) {
+            /* Releasing all COM objects was made on the basis of the article:
+             * https://www.add-in-express.com/creating-addins-blog/release-excel-com-objects/
+             */
+            Type officeType = Type.GetTypeFromProgID("Excel.Application.16");
+
+            if(officeType == null) {
+                TaskDialog.Show("Ошибка", "Excel 2016 не найден на компьютере");
+                return;
+            }
+
+            Application excelApp = new Application {
+                Visible = false,
+                DisplayAlerts = false
+            };
+
+            Workbooks workBooks = null;
+            Workbook workBook = null;
+            Sheets workSheets = null;
+            Worksheet workSheet = null;
+
+            try {
+
+                workBooks = excelApp.Workbooks;
+                workBook = workBooks.Add();
+                workSheets = workBook.Worksheets;
+                workSheet = (Worksheet) workSheets["Лист1"];
+
+                new ExcelTableCreator(tableData, settings).Create(workSheet);
+                
+                workBook.SaveAs(path);
+                workBook.Close(false);
+            } finally {
+                excelApp.Quit();
+                if(workSheet != null) { Marshal.ReleaseComObject(workSheet); }
+                if(workSheets != null) { Marshal.ReleaseComObject(workSheets); }
+                if(workBook != null) { Marshal.ReleaseComObject(workBook); }
+                if(workBooks != null) { Marshal.ReleaseComObject(workBooks); }
+                if(excelApp != null) { Marshal.ReleaseComObject(excelApp); }
+            }
+        }
+    }
+}
