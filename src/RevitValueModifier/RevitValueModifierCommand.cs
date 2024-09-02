7ea08bb9d9fac69b90cbfb6d644d886d513f8d58
DivinN <nikitad93@mail.ru>
2024-09-02 15:29:25 +0300

    Создан и настроен проект Копировать значения

--- src/RevitValueModifier/RevitValueModifierCommand.cs
+++ src/RevitValueModifier/RevitValueModifierCommand.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Interop;
 
-using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -29,33 +22,33 @@ namespace RevitValueModifier {
     [Transaction(TransactionMode.Manual)]
     public class RevitValueModifierCommand : BasePluginCommand {
         public RevitValueModifierCommand() {
-            PluginName = "RevitValueModifier";
+            PluginName = "Копировать значения";
         }
 
         protected override void Execute(UIApplication uiApplication) {
-			using(IKernel kernel = uiApplication.CreatePlatformServices()) {
+            using(IKernel kernel = uiApplication.CreatePlatformServices()) {
                 kernel.Bind<RevitRepository>()
                     .ToSelf()
                     .InSingletonScope();
-					
-				kernel.Bind<PluginConfig>()
+
+                kernel.Bind<PluginConfig>()
                     .ToMethod(c => PluginConfig.GetPluginConfig());
-				
-				kernel.Bind<MainViewModel>().ToSelf();
-				kernel.Bind<MainWindow>().ToSelf()
-                    .WithPropertyValue(nameof(Window.DataContext), 
+
+                kernel.Bind<MainViewModel>().ToSelf();
+                kernel.Bind<MainWindow>().ToSelf()
+                    .WithPropertyValue(nameof(Window.DataContext),
                         c => c.Kernel.Get<MainViewModel>())
                     .WithPropertyValue(nameof(PlatformWindow.LocalizationService),
                         c => c.Kernel.Get<ILocalizationService>());
-				
+
                 string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
 
                 kernel.UseXtraLocalization(
                     $"/{assemblyName};component/Localization/Language.xaml",
                     CultureInfo.GetCultureInfo("ru-RU"));
-                
-				Notification(kernel.Get<MainWindow>());
-			}
+
+                Notification(kernel.Get<MainWindow>());
+            }
         }
     }
 }
