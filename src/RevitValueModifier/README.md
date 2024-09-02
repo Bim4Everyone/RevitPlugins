23d6d6bd337ddaeb66f8c9ec2dfe1b03aa382451
DivinN <nikitad93@mail.ru>
2024-09-02 15:14:57 +0300

    Создан новый плагин RevitValueModifier

---
+++ src/RevitValueModifier/README.md
@@ -0,0 +1,12 @@
+# RevitValueModifier (Копировать значения)
+Описание проекта 
+
+# Сборка проекта
+```
+nuke compile --profile RevitValueModifier
+```
+
+# Публикация проекта
+```
+nuke publish --profile RevitValueModifier
+```
---
+++ src/RevitValueModifier/RevitValueModifierCommand.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.SimpleServices;
+using dosymep.SimpleServices;
+using dosymep.WPF.Views;
+using dosymep.Xpf.Core.Ninject;
+
+using Ninject;
+
+using RevitValueModifier.Models;
+using RevitValueModifier.ViewModels;
+using RevitValueModifier.Views;
+
+namespace RevitValueModifier {
+    [Transaction(TransactionMode.Manual)]
+    public class RevitValueModifierCommand : BasePluginCommand {
+        public RevitValueModifierCommand() {
+            PluginName = "RevitValueModifier";
+        }
+
+        protected override void Execute(UIApplication uiApplication) {
+			using(IKernel kernel = uiApplication.CreatePlatformServices()) {
+                kernel.Bind<RevitRepository>()
+                    .ToSelf()
+                    .InSingletonScope();
+					
+				kernel.Bind<PluginConfig>()
+                    .ToMethod(c => PluginConfig.GetPluginConfig());
+				
+				kernel.Bind<MainViewModel>().ToSelf();
+				kernel.Bind<MainWindow>().ToSelf()
+                    .WithPropertyValue(nameof(Window.DataContext), 
+                        c => c.Kernel.Get<MainViewModel>())
+                    .WithPropertyValue(nameof(PlatformWindow.LocalizationService),
+                        c => c.Kernel.Get<ILocalizationService>());
+				
+                string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+                kernel.UseXtraLocalization(
+                    $"/{assemblyName};component/Localization/Language.xaml",
+                    CultureInfo.GetCultureInfo("ru-RU"));
+                
+				Notification(kernel.Get<MainWindow>());
+			}
+        }
+    }
+}
