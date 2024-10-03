cfea2d80405963b61ab1509b36e8819fda76657a
Redomine <52177952+Redomine@users.noreply.github.com>
2024-10-03 12:03:21 +0300

    Было упущено определение отдельного параметра для гибких воздуховодов и для гибких труб - в одном случае это PIPE_DIAMETER, в другом CURVE_DIAMETER

--- src/RevitMechanicalSpecification/Models/Fillers/ElementParamNameFiller.cs
+++ src/RevitMechanicalSpecification/Models/Fillers/ElementParamNameFiller.cs
@@ -66,9 +66,9 @@ namespace RevitMechanicalSpecification.Models.Fillers {
                 case BuiltInCategory.OST_CableTray:
                     return GetCableTrayName(specificationElement);
                 case BuiltInCategory.OST_FlexDuctCurves:
-                    return GetFlexElementtName(specificationElement);
+                    return GetFlexElementName(specificationElement);
                 case BuiltInCategory.OST_FlexPipeCurves:
-                    return GetFlexElementtName(specificationElement);
+                    return GetFlexElementName(specificationElement);
             }
 
             return $"{_name} {_nameAddon}";
@@ -96,10 +96,21 @@ namespace RevitMechanicalSpecification.Models.Fillers {
         /// </summary>
         /// <param name="specificationElement"></param>
         /// <returns></returns>
-        private string GetFlexElementtName(SpecificationElement specificationElement) {
-            
-            double diameter = UnitConverter.DoubleToMilimeters(
-                specificationElement.Element.GetParam(BuiltInParameter.RBS_CURVE_DIAMETER_PARAM).AsDouble());
+        private string GetFlexElementName(SpecificationElement specificationElement) {
+            double diameter = 0;
+            BuiltInParameter diameterParam = BuiltInParameter.INVALID;
+
+            if(specificationElement.Element.Category.IsId(BuiltInCategory.OST_FlexPipeCurves)) {
+                diameterParam = BuiltInParameter.RBS_PIPE_DIAMETER_PARAM;
+            } else if(specificationElement.Element.Category.IsId(BuiltInCategory.OST_FlexDuctCurves)) {
+                diameterParam = BuiltInParameter.RBS_CURVE_DIAMETER_PARAM;
+            }
+
+            if(diameterParam != BuiltInParameter.INVALID) {
+                diameter = UnitConverter.DoubleToMilimeters(
+                    specificationElement.Element.GetParam(diameterParam).AsDouble());
+            }
+
             return $"{_name} ø{diameter}";
         }
 
