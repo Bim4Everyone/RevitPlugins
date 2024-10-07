2c45ce86b1821a38d99e38feb40b0b8e1dbb1a33
Nikita Divin <72355844+DivinN@users.noreply.github.com>
2024-10-07 12:49:24 +0300

    RevitValueModifier: Добавлен плагин "Изменить значение" (#88)

---
+++ src/RevitValueModifier/ViewModels/RevitElementViewModel.cs
@@ -0,0 +1,42 @@
+using Autodesk.Revit.DB;
+using dosymep.WPF.ViewModels;
+using RevitValueModifier.Models;
+
+namespace RevitValueModifier.ViewModels {
+    internal class RevitElementViewModel : BaseViewModel {
+        private string _paramValue = string.Empty;
+        private string _elemName;
+        private ElementId _elemId;
+        private readonly RevitElement _revitElem;
+
+        public RevitElementViewModel(RevitElement revitElement) {
+            _revitElem = revitElement;
+            ElemName = revitElement.ElemName;
+            ElemId = revitElement.ElemId;
+        }
+        
+        public string ElemName {
+            get => _elemName;
+            set => RaiseAndSetIfChanged(ref _elemName, value);
+        }
+
+        public ElementId ElemId {
+            get => _elemId;
+            set => RaiseAndSetIfChanged(ref _elemId, value);
+        }
+
+        public string ParamValue {
+            get => _paramValue;
+            set => RaiseAndSetIfChanged(ref _paramValue, value);
+        }
+
+        public void WriteParamValue(RevitParameter revitParameter) {
+            _revitElem.WriteParamValue(revitParameter);
+        }
+
+        public void SetParamValue(string paramValueMask) {
+            _revitElem.SetParamValue(paramValueMask);
+            ParamValue = _revitElem.ParamValue;
+        }
+    }
+}
