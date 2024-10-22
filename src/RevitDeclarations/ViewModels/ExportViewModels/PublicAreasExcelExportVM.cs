defc91ab3737eec857cfeab9ca3f90714b207fd1
Tikhomirov_am <senia7@bk.ru>
2024-10-22 10:34:55 +0300

    Добалвены классы VM экспорта

---
+++ src/RevitDeclarations/ViewModels/ExportViewModels/PublicAreasExcelExportVM.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RevitDeclarations.Models;
+
+namespace RevitDeclarations.ViewModels
+{
+    internal class PublicAreasExcelExportVM : ExportViewModel {
+        public PublicAreasExcelExportVM(string name, Guid id, DeclarationSettings settings)
+            : base(name, id, settings) {
+        }
+
+        public override void Export(string path, IEnumerable<RoomGroup> publicAreas) {
+            PublicAreasTableInfo tableData = new PublicAreasTableInfo(publicAreas.Cast<PublicArea>().ToList(), _settings);
+            PublicAreasDataTable table = new PublicAreasDataTable(tableData, _settings);
+
+            ExportTable<ExcelExporter>(path, table);
+        }
+    }
+}
