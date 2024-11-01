80daff915874daab3340ac7927dc0a8e034319fe
Tikhomirov_am <senia7@bk.ru>
2024-11-01 13:08:47 +0300

    Добавлены отдельные классы для конфигураций каждого скрипта

---
+++ src/RevitDeclarations/Models/Configs/ApartmentsConfig.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+
+using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.ProjectConfigs;
+using dosymep.Bim4Everyone.SharedParams;
+using dosymep.Serializers;
+
+using pyRevitLabs.Json;
+
+namespace RevitDeclarations.Models.Configs {
+    internal class ApartmentsConfig : ProjectConfig<ApartmnetsConfigSettings> {
+        [JsonIgnore] public override string ProjectConfigPath { get; set; }
+
+        [JsonIgnore] public override IConfigSerializer Serializer { get; set; }
+
+        public static ApartmentsConfig GetPluginConfig() {
+            return new ProjectConfigBuilder()
+                .SetSerializer(new ConfigSerializer())
+                .SetPluginName(nameof(RevitDeclarations))
+                .SetRevitVersion(ModuleEnvironment.RevitVersion)
+                .SetProjectConfigName(nameof(ApartmentsConfig) + ".json")
+                .Build<ApartmentsConfig>();
+        }
+    }
+
+    internal class ApartmnetsConfigSettings : DeclarationConfigSettings {
+        public string ApartmentFullNumberParam { get; set; }
+
+        public string BuildingNumberParam { get; set; }
+        public string ConstrWorksNumberParam { get; set; }
+        public string ApartmentAreaCoefParam { get; set; }
+        public string RoomsAmountParam { get; set; }
+        public string ApartmentAreaLivingParam { get; set; }
+        public string RoomsHeightParam { get; set; }
+        public string ApartmentAreaNonSumParam { get; set; }
+        public string RoomAreaCoefParam { get; set; }
+        public string PrioritiesFilePath { get; set; }
+
+        public ApartmnetsConfigSettings GetCompanyConfig() {
+            return new ApartmnetsConfigSettings() {
+                FilterRoomsParam = LabelUtils.GetLabelFor(BuiltInParameter.ROOM_DEPARTMENT),
+                FilterRoomsValue = "квартира",
+                GroupingBySectionParam = SharedParamsConfig.Instance.RoomSectionShortName.Name,
+                GroupingByGroupParam = SharedParamsConfig.Instance.RoomGroupShortName.Name,
+                MultiStoreyParam = SharedParamsConfig.Instance.RoomMultilevelGroup.Name,
+
+                ApartmentFullNumberParam = SharedParamsConfig.Instance.ApartmentNumber.Name,
+                DepartmentParam = LabelUtils.GetLabelFor(BuiltInParameter.ROOM_DEPARTMENT),
+                LevelParam = SharedParamsConfig.Instance.Level.Name,
+                SectionParam = SharedParamsConfig.Instance.RoomSectionShortName.Name,
+                BuildingParam = SharedParamsConfig.Instance.RoomBuildingShortName.Name,
+                BuildingNumberParam = SharedParamsConfig.Instance.BuildingNumber.Name,
+                ConstrWorksNumberParam = SharedParamsConfig.Instance.ConstructionWorksNumber.Name,
+                ApartmentNumberParam = SharedParamsConfig.Instance.RoomGroupShortName.Name,
+                ApartmentAreaParam = SharedParamsConfig.Instance.ApartmentArea.Name,
+                ApartmentAreaCoefParam = SharedParamsConfig.Instance.ApartmentAreaRatio.Name,
+                ApartmentAreaLivingParam = SharedParamsConfig.Instance.ApartmentLivingArea.Name,
+                RoomsAmountParam = SharedParamsConfig.Instance.RoomsCount.Name,
+                ProjectNameID = "",
+                ApartmentAreaNonSumParam = SharedParamsConfig.Instance.ApartmentAreaNoBalcony.Name,
+                RoomsHeightParam = LabelUtils.GetLabelFor(BuiltInParameter.ROOM_HEIGHT),
+
+                RoomAreaParam = SharedParamsConfig.Instance.RoomArea.Name,
+                RoomAreaCoefParam = SharedParamsConfig.Instance.RoomAreaWithRatio.Name,
+            };
+        }
+    }
+}
---
+++ src/RevitDeclarations/Models/Configs/PublicAreasConfig.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+
+using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.ProjectConfigs;
+using dosymep.Bim4Everyone.SharedParams;
+using dosymep.Serializers;
+
+using pyRevitLabs.Json;
+
+namespace RevitDeclarations.Models {
+    internal class PublicAreasConfig : ProjectConfig<PublicAreasConfigSettings> {
+        [JsonIgnore] public override string ProjectConfigPath { get; set; }
+
+        [JsonIgnore] public override IConfigSerializer Serializer { get; set; }
+
+        public static PublicAreasConfig GetPluginConfig() {
+            return new ProjectConfigBuilder()
+                .SetSerializer(new ConfigSerializer())
+                .SetPluginName(nameof(RevitDeclarations))
+                .SetRevitVersion(ModuleEnvironment.RevitVersion)
+                .SetProjectConfigName(nameof(PublicAreasConfig) + ".json")
+                .Build<PublicAreasConfig>();
+        }
+    }
+
+    internal class PublicAreasConfigSettings : DeclarationConfigSettings {
+        public bool AddPostfixToNumber { get; set; }
+
+        public PublicAreasConfigSettings GetCompanyConfig() {
+            return new PublicAreasConfigSettings() {
+                FilterRoomsParam = LabelUtils.GetLabelFor(BuiltInParameter.ROOM_DEPARTMENT),
+                FilterRoomsValue = "общественное; техническое",
+                GroupingBySectionParam = SharedParamsConfig.Instance.RoomGroupShortName.Name,
+                GroupingByGroupParam = LabelUtils.GetLabelFor(BuiltInParameter.ROOM_NUMBER),
+                MultiStoreyParam = SharedParamsConfig.Instance.RoomMultilevelGroup.Name,
+
+                DepartmentParam = LabelUtils.GetLabelFor(BuiltInParameter.ROOM_DEPARTMENT),
+                LevelParam = SharedParamsConfig.Instance.Level.Name,
+                SectionParam = SharedParamsConfig.Instance.RoomSectionShortName.Name,
+                BuildingParam = SharedParamsConfig.Instance.RoomBuildingShortName.Name,
+                ApartmentAreaParam = SharedParamsConfig.Instance.ApartmentArea.Name,
+                RoomAreaParam = SharedParamsConfig.Instance.RoomArea.Name,
+                AddPostfixToNumber = true,
+                ProjectNameID = ""
+            };
+        }
+    }
+}
