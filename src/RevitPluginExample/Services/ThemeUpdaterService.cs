73d55f7ac9d58995ef9e2949bfe35344efcb6b73
dosymep <dosymep@gmail.com>
2024-12-11 14:41:57 +0300

    исправлен сервис обновления темы

--- src/RevitPluginExample/Services/ThemeUpdaterService.cs
+++ src/RevitPluginExample/Services/ThemeUpdaterService.cs
@@ -9,15 +9,11 @@ using Wpf.Ui.Controls;
 
 namespace RevitPluginExample.Services {
     public class ThemeUpdaterService : IUIThemeUpdaterService {
-
-        private ApplicationTheme _currentTheme = ApplicationThemeManager.GetAppTheme();
-
         public void SetTheme(Window window, UIThemes theme) {
-            var appTheme = ThemeConverter.ConvertToApplicationTheme(theme);
-            var newTheme = appTheme == ApplicationTheme.Dark ? ApplicationTheme.Light : ApplicationTheme.Dark;
-            if(_currentTheme != newTheme) {
-                _currentTheme = newTheme;
-                ApplicationThemeManager.Apply(_currentTheme, WindowBackdropType.Mica, true);
+            if(theme == UIThemes.Dark) {
+                ApplicationThemeManager.Apply(ApplicationTheme.Dark);
+            } else if(theme == UIThemes.Light) {
+                ApplicationThemeManager.Apply(ApplicationTheme.Light);
             }
         }
     }
