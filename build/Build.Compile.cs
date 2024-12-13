bf55802c92c44d82632007f7d835f393d2964f4d
Biseuv Olzhas <dosymep@gmail.com>
2024-12-13 16:06:57 +0600

    Nuke: Обновлены зависимости (#129)

--- build/Build.Compile.cs
+++ build/Build.Compile.cs
@@ -3,6 +3,7 @@ using System;
 using Nuke.Common;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
 using Nuke.Components;
 
@@ -13,25 +14,25 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 partial class Build {
     Target Compile => _ => _
         .DependsOn(Clean)
+        .Requires(() => Output)
         .Requires(() => PluginName)
-        .Requires(() => PublishDirectory)
         .Executes(() => {
-            string publishDirectory = NukeBuildExtensions.GetExtensionsPath(PublishDirectory);
-            Log.Debug("publishDirectory: {PublishDirectory}", publishDirectory);
+            Log.Debug("Params.Output: {Output}", Params.Output);
 
             ReportSummary(_ => _
                 .AddPairWhenValueNotNull(nameof(PluginName), PluginName)
-                .AddPairWhenValueNotNull(nameof(PublishDirectory), PublishDirectory)
+                .AddPairWhenValueNotNull(nameof(Params.Configuration), Params.Configuration)
+                .AddPairWhenValueNotNull(nameof(Params.Output), Params.Output)
             );
 
             DotNetBuild(s => s
                 .Apply(CompileSettingsBase)
                 .SetProjectFile("src/" + PluginName)
-                .SetConfiguration(Configuration.Debug)
-                .SetOutputDirectory(publishDirectory)
+                .SetConfiguration(Params.Configuration)
                 .CombineWith(Params.BuildRevitVersions, (settings, revitVersion) => settings
                     .SetSimpleVersion(Params, revitVersion)
                     .SetProperty("RevitVersion", (int) revitVersion)
+                    .SetProperty("OutputPath", Params.Output)
                     .SetProperty("AssemblyName", $"{PluginName}_{revitVersion}")));
         });
 
@@ -46,26 +47,27 @@ partial class Build {
 
             ReportSummary(_ => _
                 .AddPairWhenValueNotNull(nameof(PluginName), PluginName)
+                .AddPairWhenValueNotNull(nameof(Params.Configuration), Params.Configuration)
                 .AddPairWhenValueNotNull(nameof(PublishDirectory), PublishDirectory)
             );
 
             DotNetBuild(s => s
                 .Apply(CompileSettingsBase)
                 .SetProjectFile("src/" + PluginName)
-                .SetConfiguration(Configuration.Release)
-                .SetOutputDirectory(publishDirectory)
+                .SetConfiguration(Params.Configuration)
                 .CombineWith(Params.BuildRevitVersions, (settings, revitVersion) => settings
                     .SetSimpleVersion(Params, revitVersion)
                     .SetProperty("RevitVersion", (int) revitVersion)
+                    .SetProperty("OutputPath", publishDirectory)
                     .SetProperty("AssemblyName", $"{PluginName}_{revitVersion}")));
         });
 
     Configure<DotNetBuildSettings> CompileSettingsBase => _ => _
         .DisableNoRestore()
-        .SetOutputDirectory(Output)
         .SetCopyright($"Copyright © {DateTime.Now.Year}")
-        .When(IsServerBuild, _ => _
-            .EnableContinuousIntegrationBuild())
+        .When(settings => IsServerBuild,
+            _ => _
+                .EnableContinuousIntegrationBuild())
         .WhenNotNull(this, (_, o) => _
             .SetRepositoryUrl(o.GitRepository.HttpsUrl));
 }
