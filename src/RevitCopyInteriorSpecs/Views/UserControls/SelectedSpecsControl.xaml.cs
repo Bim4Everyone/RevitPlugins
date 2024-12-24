4fe7a1833773ed9831a0013ce0833cd6f23b865d
DivinN <nikitad93@mail.ru>
2024-12-24 23:43:04 +0300

    Основное окно разделено при помощи UserControl`ов

---
+++ src/RevitCopyInteriorSpecs/Views/UserControls/SelectedSpecsControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RevitCopyInteriorSpecs.Views.UserControls {
+    /// <summary>
+    /// Логика взаимодействия для SelectedSpecsControl.xaml
+    /// </summary>
+    public partial class SelectedSpecsControl : UserControl {
+        public SelectedSpecsControl() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/RevitCopyInteriorSpecs/Views/UserControls/TasksControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace RevitCopyInteriorSpecs.Views.UserControls {
+    /// <summary>
+    /// Логика взаимодействия для TasksControl.xaml
+    /// </summary>
+    public partial class TasksControl : UserControl {
+        public TasksControl() {
+            InitializeComponent();
+        }
+    }
+}
