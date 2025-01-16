3ed12719a3f19e9a51fa35a02244750c6c3c8b9d
vlastroG <vlastroganov@gmail.com>
2025-01-16 17:27:38 +0300

    Тестовая разметка окна

--- src/RevitRefreshLinks/ViewModels/LinkToUpdateViewModel.cs
+++ src/RevitRefreshLinks/ViewModels/LinkToUpdateViewModel.cs
@@ -1,16 +1,47 @@
-using dosymep.WPF.ViewModels;
+using Autodesk.Revit.DB;
 
-using RevitRefreshLinks.Models;
+using dosymep.WPF.ViewModels;
 
 namespace RevitRefreshLinks.ViewModels {
     internal class LinkToUpdateViewModel : BaseViewModel {
-        private readonly ILinkPair _linkPair;
+        private readonly RevitLinkType _linkType;
+        private bool _isSelected;
+        private string _sourceStatus;
+        private string _displayWorkset;
+        private string _displayLinkStatus;
 
-        public LinkToUpdateViewModel(ILinkPair linkPair) {
-            _linkPair = linkPair ?? throw new System.ArgumentNullException(nameof(linkPair));
+        public LinkToUpdateViewModel(RevitLinkType linkType) {
+            _linkType = linkType ?? throw new System.ArgumentNullException(nameof(linkType));
+            Name = _linkType.Name;
+            LinkStatus = _linkType.GetLinkedFileStatus();
+            WorksetIsClosed = LinkStatus == LinkedFileStatus.InClosedWorkset;
         }
 
 
+        public string Name { get; }
 
+        public bool WorksetIsClosed { get; }
+
+        public LinkedFileStatus LinkStatus { get; }
+
+        public bool IsSelected {
+            get => _isSelected;
+            set => RaiseAndSetIfChanged(ref _isSelected, value);
+        }
+
+        public string SourceStatus {
+            get => _sourceStatus;
+            set => RaiseAndSetIfChanged(ref _sourceStatus, value);
+        }
+
+        public string DisplayWorkset {
+            get => _displayWorkset;
+            set => RaiseAndSetIfChanged(ref _displayWorkset, value);
+        }
+
+        public string DisplayLinkStatus {
+            get => _displayLinkStatus;
+            set => RaiseAndSetIfChanged(ref _displayLinkStatus, value);
+        }
     }
 }
--- src/RevitRefreshLinks/ViewModels/UpdateLinksViewModel.cs
+++ src/RevitRefreshLinks/ViewModels/UpdateLinksViewModel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 using dosymep.SimpleServices;
@@ -10,9 +13,8 @@ namespace RevitRefreshLinks.ViewModels {
     internal class UpdateLinksViewModel : BaseViewModel {
         private readonly RevitRepository _revitRepository;
         private readonly ILocalizationService _localizationService;
-
         private string _errorText;
-        private string _saveProperty;
+
 
         public UpdateLinksViewModel(
             RevitRepository revitRepository,
@@ -21,37 +23,93 @@ namespace RevitRefreshLinks.ViewModels {
             _revitRepository = revitRepository;
             _localizationService = localizationService;
 
+            LinksToUpdate = new ObservableCollection<LinkToUpdateViewModel>();
+
             LoadViewCommand = RelayCommand.Create(LoadView);
             AcceptViewCommand = RelayCommand.Create(AcceptView, CanAcceptView);
+            SelectLocalSourceCommand = RelayCommand.Create(SelectLocalSource);
+            SelectServerSourceCommand = RelayCommand.Create(SelectServerSource);
+            SelectAllLinksCommand = RelayCommand.Create(SelectAllLinks, CanSelectAllLinks);
+            UnselectAllLinksCommand = RelayCommand.Create(UnselectAllLinks, CanUnselectAllLinks);
+            InvertSelectedLinksCommand = RelayCommand.Create(InvertSelectedLinks, CanInvertSelectedLinks);
         }
 
+
         public ICommand LoadViewCommand { get; }
+
         public ICommand AcceptViewCommand { get; }
+
+        public ICommand SelectLocalSourceCommand { get; }
+
+        public ICommand SelectServerSourceCommand { get; }
+
+        public ICommand SelectAllLinksCommand { get; }
+
+        public ICommand UnselectAllLinksCommand { get; }
+
+        public ICommand InvertSelectedLinksCommand { get; }
+
+        public ObservableCollection<LinkToUpdateViewModel> LinksToUpdate { get; }
 
         public string ErrorText {
             get => _errorText;
             set => this.RaiseAndSetIfChanged(ref _errorText, value);
         }
 
-        public string SaveProperty {
-            get => _saveProperty;
-            set => this.RaiseAndSetIfChanged(ref _saveProperty, value);
-        }
 
         private void LoadView() {
+            var links = _revitRepository
+                .GetExistingLinks()
+                .Select(t => new LinkToUpdateViewModel(t))
+                .OrderBy(t => t.Name);
+            foreach(var link in links) {
+                LinksToUpdate.Add(link);
+            }
         }
 
         private void AcceptView() {
         }
 
         private bool CanAcceptView() {
-            if(string.IsNullOrEmpty(SaveProperty)) {
-                ErrorText = _localizationService.GetLocalizedString("UpdateLinksWindow.HelloCheck");
-                return false;
-            }
+            //if(true) {
+            //    ErrorText = _localizationService.GetLocalizedString("UpdateLinksWindow.HelloCheck");
+            //    return false;
+            //}
 
-            ErrorText = null;
+            //ErrorText = null;
             return true;
+        }
+
+        private void InvertSelectedLinks() {
+            throw new NotImplementedException();
+        }
+
+        private bool CanInvertSelectedLinks() {
+            return true;
+        }
+
+        private void UnselectAllLinks() {
+            throw new NotImplementedException();
+        }
+
+        private bool CanUnselectAllLinks() {
+            return true;
+        }
+
+        private void SelectAllLinks() {
+            throw new NotImplementedException();
+        }
+
+        private bool CanSelectAllLinks() {
+            return true;
+        }
+
+        private void SelectServerSource() {
+            throw new NotImplementedException();
+        }
+
+        private void SelectLocalSource() {
+            throw new NotImplementedException();
         }
     }
 }
