239e267156ed7a31ce552ff13913ab2e16a466af
Nikita Divin <72355844+DivinN@users.noreply.github.com>
2025-01-24 11:30:57 +0300

    RevitCopyInteriorSpecs: Добавлен плагин "Копирование спецификаций АИ" (#142)

---
+++ src/RevitCopyInteriorSpecs/Models/RevitRepository.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitCopyInteriorSpecs.Models {
+    internal class RevitRepository {
+        public RevitRepository(UIApplication uiApplication) {
+            UIApplication = uiApplication;
+        }
+
+        public UIApplication UIApplication { get; }
+        public UIDocument ActiveUIDocument => UIApplication.ActiveUIDocument;
+        public Application Application => UIApplication.Application;
+        public Document Document => ActiveUIDocument.Document;
+
+        internal List<ViewSchedule> GetSelectedSpecs() => ActiveUIDocument.Selection.GetElementIds()
+                .Select(id => Document.GetElement(id))
+                .Where(element => element is ViewSchedule)
+                .Cast<ViewSchedule>()
+                .ToList();
+
+        internal List<T> GetElements<T>() => new FilteredElementCollector(Document)
+            .OfClass(typeof(T))
+            .WhereElementIsNotElementType()
+            .Cast<T>()
+            .ToList();
+
+
+        public ViewSchedule GetSpecByName(string specName) => new FilteredElementCollector(Document)
+                .OfClass(typeof(ViewSchedule))
+                .OfType<ViewSchedule>()
+                .FirstOrDefault(o => o.Name.Equals(specName));
+
+        public Level GetLevelByName(string specName) => new FilteredElementCollector(Document)
+            .OfClass(typeof(Level))
+            .Cast<Level>()
+            .FirstOrDefault(o => o.Name.Equals(specName));
+    }
+}
