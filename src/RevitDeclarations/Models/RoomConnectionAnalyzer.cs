9e783e1d9049c81469722cf0d31ed7bb4d92ace1
sssenya <tikhomirov_am@a101.ru>
2025-01-28 09:28:53 +0300

    Обновлена логика поиска помещений по разделителям

--- src/RevitDeclarations/Models/RoomConnectionAnalyzer.cs
+++ src/RevitDeclarations/Models/RoomConnectionAnalyzer.cs
@@ -14,9 +14,6 @@ namespace RevitDeclarations.Models {
         private readonly ApartmentsSettings _settings;
         private readonly PrioritiesConfig _priorities;
 
-        private readonly BuiltInParameter _bltNameParam = BuiltInParameter.ROOM_NAME;
-
-
         // Мастер-спальни 
         private IEnumerable<ElementId> _masterBedrooms;
         // Санузлы, относящиеся к мастер спальням
@@ -44,23 +41,34 @@ namespace RevitDeclarations.Models {
         }
 
         public void FindConnections() {
-            IReadOnlyCollection<FamilyInstance> doors = _project.GetDoors();
+            IEnumerable<RoomSeparator> doorSeparators = _project
+                .GetDoors()
+                .Select(x => new FamInstanceRoomSeparator(_project, x));
+
+            IEnumerable<RoomSeparator> curveSeparators = _project
+                .GetCurveSeparators()
+                .Select(x => new CurveRoomSeparator(_project, x));
+
+            IEnumerable<RoomSeparator> separators = doorSeparators
+                .Concat(curveSeparators)
+                .Where(x => x.CheckIsValid());
 
             // Словарь с жилыми комнатами, соединенными с СУ и этими СУ
-            var bedroomBathroom = GetRoomsByDoors(doors, _priorities.LivingRoom, _priorities.Bathroom);
+            var bedroomBathroom = GetRoomsBySeparators(separators, _priorities.LivingRoom, _priorities.Bathroom);
             // Жилые комнаты, соединенные с санузлами
             List<ElementId> bedroomsWithBathroom = bedroomBathroom[_priorities.LivingRoom];
 
-            var pantryBedroom = GetRoomsByDoors(doors, _priorities.Pantry, _priorities.LivingRoom);
-            var pantryBathroom = GetRoomsByDoors(doors, _priorities.Pantry, _priorities.Bathroom);
+            var pantryBedroom = GetRoomsBySeparators(separators, _priorities.Pantry, _priorities.LivingRoom);
+            var pantryBathroom = GetRoomsBySeparators(separators, _priorities.Pantry, _priorities.Bathroom);
             // Гардеробные, соединенные с жилыми комнатами (не учитываются для УТП Гардеробная)
             _pantriesWithBedroom = pantryBedroom[_priorities.Pantry];
             // Гардеробные, соединенные с санузлами
             List<ElementId> pantriesWithBathroom = pantryBathroom[_priorities.Pantry];
+
             // Мастер-гардеробные (имеющие дверь в санузел) для определения мастер-спален
             List<ElementId> masterPantries = _pantriesWithBedroom.Intersect(pantriesWithBathroom).ToList();
 
-            var bedroomPantry = GetRoomsByDoors(doors, _priorities.LivingRoom, masterPantries);
+            var bedroomPantry = GetRoomsBySeparators(separators, _priorities.LivingRoom, masterPantries);
             // Жилые комнаты, соединенные с мастер-гардеробными
             var bedroomsWithPantryAndBathroom = bedroomPantry[_priorities.LivingRoom];
 
@@ -70,7 +78,7 @@ namespace RevitDeclarations.Models {
                 .ToList();
 
             List<ElementId> bathroomsWithBedroom = bedroomBathroom[_priorities.Bathroom];
-            var bathroomPantry = GetRoomsByDoors(doors, _priorities.Bathroom, masterPantries);
+            var bathroomPantry = GetRoomsBySeparators(separators, _priorities.Bathroom, masterPantries);
             // Санузлы, соединенные с мастер-гардеробными
             var bathroomsWithMasterPantry = bathroomPantry[_priorities.Bathroom];
 
@@ -80,68 +88,43 @@ namespace RevitDeclarations.Models {
                 .ToList();
         }
 
-        private Dictionary<RoomPriority, List<ElementId>> GetRoomsByDoors(IReadOnlyCollection<FamilyInstance> doors, 
-                                                                          RoomPriority priority1, 
-                                                                          RoomPriority priority2) {
+        private Dictionary<RoomPriority, List<ElementId>> GetRoomsBySeparators(IEnumerable<RoomSeparator> separators, 
+                                                                               RoomPriority priority1, 
+                                                                               RoomPriority priority2) {
             var roomByNames = new Dictionary<RoomPriority, List<ElementId>>() {
                 [priority1] = new List<ElementId>(),
                 [priority2] = new List<ElementId>()
             };
 
-            foreach(var door in doors) {
-                Room room1 = door.get_FromRoom(_project.Phase);
-                Room room2 = door.get_ToRoom(_project.Phase);
-                // У дверей может не быть помещения с одной стороны
+            foreach(var separator in separators) {                
+                var room1 = separator.GetRoom(priority1);
+                var room2 = separator.GetRoom(priority2);
                 if(room1 != null && room2 != null) {
-                    string roomName1 = room1.get_Parameter(_bltNameParam).AsString();
-                    string roomName2 = room2.get_Parameter(_bltNameParam).AsString();
-
-                    if(priority1.CheckName(roomName1) && priority2.CheckName(roomName2)) {
-                        roomByNames[priority1].Add(room1.Id);
-                        roomByNames[priority2].Add(room2.Id);
-                    }
-
-                    if(priority1.CheckName(roomName2) && priority2.CheckName(roomName1)) {
-                        roomByNames[priority1].Add(room2.Id);
-                        roomByNames[priority2].Add(room1.Id);
-                    }
+                    roomByNames[priority1].Add(room1.Id);
+                    roomByNames[priority2].Add(room2.Id);
                 }
             }
 
             return roomByNames;
         }
 
-        private Dictionary<RoomPriority, List<ElementId>> GetRoomsByDoors(IReadOnlyCollection<FamilyInstance> doors, 
-                                                                          RoomPriority priority, 
-                                                                          List<ElementId> rooms) {
+        private Dictionary<RoomPriority, List<ElementId>> GetRoomsBySeparators(IEnumerable<RoomSeparator> separators, 
+                                                                               RoomPriority priority, 
+                                                                               List<ElementId> rooms) {
             var roomByNames = new Dictionary<RoomPriority, List<ElementId>>() {
                 [priority] = new List<ElementId>()
             };
 
-            foreach(var door in doors) {
-                Room room1 = door.get_FromRoom(_project.Phase);
-                Room room2 = door.get_ToRoom(_project.Phase);
-                // У дверей может не быть помещения с одной стороны
-                if(room1 != null && room2 != null) {
-                    string roomName1 = room1.get_Parameter(_bltNameParam).AsString();
-                    string roomName2 = room2.get_Parameter(_bltNameParam).AsString();
-
-                    if(priority.CheckName(roomName1) && rooms.Contains(room2.Id)) {
-                        roomByNames[priority].Add(room1.Id);
-                    }
-
-                    if(priority.CheckName(roomName2) && rooms.Contains(room1.Id)) {
-                        roomByNames[priority].Add(room2.Id);
+            foreach(var separator in separators) {
+                if(separator.Rooms.Where(x => rooms.Contains(x.Id)).Any()) {
+                    var room = separator.GetRoom(priority);
+                    if(room != null) {
+                        roomByNames[priority].Add(room.Id);
                     }
                 }
             }
 
             return roomByNames;
-        }
-
-        private Dictionary<string, List<ElementId>> GetRoomsBySeparators(RoomPriority priority1,
-                                                                         RoomPriority priority2) {
-
         }
     }
 }
