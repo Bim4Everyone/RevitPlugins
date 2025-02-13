68f48c2ea5941ea51c678f310e2435f10c2b6d08
vlastroG <vlastroganov@gmail.com>
2025-02-13 14:45:38 +0300

    Убрать ненужные файлы

--- src/RevitRefreshLinks/Services/ILinkSourceFinder.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-using RevitRefreshLinks.Models;
-
-namespace RevitRefreshLinks.Services {
-    internal interface ILinkSourceFinder {
-        void FindAndSetSources(ICollection<ILinkPair> destinations, ICollection<ILink> sources);
-    }
-}
