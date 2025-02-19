cba398f4f11f2abfb2b90339d0e896094bd357f6
vlastroG <vlastroganov@gmail.com>
2025-02-19 17:23:47 +0300

    Удалить неиспользуемый код

--- src/RevitRefreshLinks/Models/IExplorerSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace RevitRefreshLinks.Models {
-    internal interface IExplorerSettings {
-        string Title { get; }
-        string InitialDirectory { get; }
-        bool MultiSelect { get; }
-    }
-}
