b46440e6b1a61f041f70a4ec02a8e3e068191107
dosymep <dosymep@gmail.com>
2025-03-27 15:35:01 +0300

    Упростил регистрацию в контейнере

---
+++ src/RevitPlatformSettings/Extensions/NinjectExtensions.cs
@@ -0,0 +1,93 @@
+using Ninject;
+
+using RevitPlatformSettings.Factories;
+using RevitPlatformSettings.Model;
+using RevitPlatformSettings.Services;
+using RevitPlatformSettings.ViewModels.Settings;
+using RevitPlatformSettings.Views.Pages;
+
+namespace RevitPlatformSettings.Extensions {
+    internal static class NinjectExtensions {
+        public static IKernel BindPages(this IKernel kernel) {
+            kernel.Bind<SettingsPage>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<GeneralSettingsPage>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<ExtensionsSettingsPage>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<RevitParamsSettingsPage>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<TelemetrySettingsPage>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<AboutSettingsPage>()
+                .ToSelf()
+                .InSingletonScope();
+
+            return kernel;
+        }
+
+        public static IKernel BindViewModels(this IKernel kernel) {
+            kernel.Bind<SettingsViewModel>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<GeneralSettingsViewModel>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<ExtensionsSettingsViewModel>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<RevitParamsSettingsViewModel>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<TelemetrySettingsViewModel>()
+                .ToSelf()
+                .InSingletonScope();
+
+            kernel.Bind<AboutSettingsViewModel>()
+                .ToSelf()
+                .InSingletonScope();
+
+
+            return kernel;
+        }
+
+        public static IKernel BindExtensions(this IKernel kernel) {
+            kernel.Bind<BuiltinExtension>().ToSelf();
+            kernel.Bind<ThirdPartyExtension>().ToSelf();
+
+            kernel.Bind<IExtensionFactory<BuiltinExtension>>()
+                .To<ExtensionFactory<BuiltinExtension>>();
+
+            kernel.Bind<IExtensionFactory<ThirdPartyExtension>>()
+                .To<ExtensionFactory<ThirdPartyExtension>>();
+
+            kernel.Bind<IExtensionsService<BuiltinExtension>>()
+                .To<BuiltinExtensionsService>();
+
+            kernel.Bind<IExtensionsService<ThirdPartyExtension>>()
+                .To<ThirdPartyExtensionsService>();
+
+            kernel.Bind<IPyRevitExtensionsService>()
+                .To<PyRevitExtensionsService>();
+
+            kernel.Bind<IExtensionViewModelFactory>()
+                .To<ExtensionViewModelFactory>();
+
+            return kernel;
+        }
+    }
+}
--- src/RevitPlatformSettings/PlatformSettingsCommand.cs
+++ src/RevitPlatformSettings/PlatformSettingsCommand.cs
@@ -15,6 +15,7 @@ using dosymep.Xpf.Core.Ninject;
 
 using Ninject;
 
+using RevitPlatformSettings.Extensions;
 using RevitPlatformSettings.Factories;
 using RevitPlatformSettings.Model;
 using RevitPlatformSettings.Services;
@@ -40,80 +41,10 @@ namespace RevitPlatformSettings {
 
         public bool? OpenSettingsWindow(UIApplication uiApplication) {
             using(IKernel kernel = uiApplication.CreatePlatformServices()) {
-                kernel.Bind<UIApplication>()
-                    .ToConstant(uiApplication)
-                    .InTransientScope();
-                kernel.Bind<Application>()
-                    .ToConstant(uiApplication.Application)
-                    .InTransientScope();
-
-
-                kernel.Bind<BuiltinExtension>().ToSelf();
-                kernel.Bind<ThirdPartyExtension>().ToSelf();
-
-                kernel.Bind<IExtensionFactory<BuiltinExtension>>()
-                    .To<ExtensionFactory<BuiltinExtension>>();
-
-                kernel.Bind<IExtensionFactory<ThirdPartyExtension>>()
-                    .To<ExtensionFactory<ThirdPartyExtension>>();
-
-                kernel.Bind<IExtensionsService<BuiltinExtension>>()
-                    .To<BuiltinExtensionsService>();
-
-                kernel.Bind<IExtensionsService<ThirdPartyExtension>>()
-                    .To<ThirdPartyExtensionsService>();
-
-                kernel.Bind<IPyRevitExtensionsService>()
-                    .To<PyRevitExtensionsService>();
-
-                kernel.Bind<IExtensionViewModelFactory>()
-                    .To<ExtensionViewModelFactory>();
-
-                kernel.Bind<ISettingsViewModelFactory>()
-                    .To<SettingsViewModelFactory>();
-
-                kernel.Bind<SettingsPage>()
-                    .ToSelf()
-                    .InSingletonScope();
-                kernel.Bind<SettingsViewModel>()
-                    .ToSelf()
-                    .InSingletonScope();
-
-                kernel.Bind<GeneralSettingsPage>()
-                    .ToSelf()
-                    .InSingletonScope();
-                kernel.Bind<GeneralSettingsViewModel>()
-                    .ToSelf()
-                    .InSingletonScope();
-
-                kernel.Bind<ExtensionsSettingsPage>()
-                    .ToSelf()
-                    .InSingletonScope();
-                kernel.Bind<ExtensionsSettingsViewModel>()
-                    .ToSelf()
-                    .InSingletonScope();
-
-                kernel.Bind<RevitParamsSettingsPage>()
-                    .ToSelf()
-                    .InSingletonScope();
-                kernel.Bind<RevitParamsSettingsViewModel>()
-                    .ToSelf()
-                    .InSingletonScope();
+                kernel.BindPages();
+                kernel.BindViewModels();
+                kernel.BindExtensions();
                 
-                kernel.Bind<TelemetrySettingsPage>()
-                    .ToSelf()
-                    .InSingletonScope();
-                kernel.Bind<TelemetrySettingsViewModel>()
-                    .ToSelf()
-                    .InSingletonScope();
-
-                kernel.Bind<AboutSettingsPage>()
-                    .ToSelf()
-                    .InSingletonScope();
-                kernel.Bind<AboutSettingsViewModel>()
-                    .ToSelf()
-                    .InSingletonScope();
-
                 kernel.Bind<INavigationViewPageProvider>()
                     .To<NavigationViewPageProvider>()
                     .InSingletonScope();
