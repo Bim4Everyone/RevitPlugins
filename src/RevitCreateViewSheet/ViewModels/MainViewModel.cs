3545333c32dd501266d0ff6b0b1ae19a9ce35b9b
vlastroG <vlastroganov@gmail.com>
2025-04-04 12:34:39 +0300

    Добавить проверку на редактирование перед вызовом обновления

--- src/RevitCreateViewSheet/ViewModels/MainViewModel.cs
+++ src/RevitCreateViewSheet/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows.Data;
 using System.Windows.Input;
@@ -213,6 +214,10 @@ namespace RevitCreateViewSheet.ViewModels {
                 sheet.SheetModel.MarkAsDeleted();
             } else {
                 AllSheets.Remove(sheet);
+            }
+            var editableView = VisibleSheets.View as IEditableCollectionView;
+            if(editableView?.IsEditingItem ?? false) {
+                editableView.CommitEdit();
             }
             VisibleSheets.View.Refresh();
         }
