8a462955c69f04d905db2c31fa112c377b33308f
Pavel Kozlov <64225538+KozlovPS@users.noreply.github.com>
2025-04-11 09:28:51 +0300

    RevitListOfSchedules: Добавлен плагин "Ведомость спецификаций" (#176)

---
+++ src/RevitListOfSchedules/ViewModels/GroupParameterViewModel.cs
@@ -0,0 +1,38 @@
+using dosymep.Bim4Everyone;
+using dosymep.SimpleServices;
+
+namespace RevitListOfSchedules.ViewModels {
+    public class GroupParameterViewModel {
+        private readonly ILocalizationService _localizationService;
+        private readonly RevitParam _parameter;
+        private readonly string _name;
+        private readonly string _id;
+
+        public GroupParameterViewModel(ILocalizationService localizationService, RevitParam parameter = null) {
+            _localizationService = localizationService;
+            _parameter = parameter;
+            _name = GetParamName();
+            _id = GetId();
+        }
+
+        public RevitParam Parameter => _parameter;
+        public string Name => _name;
+        public string Id => _id;
+
+        private string GetParamName() {
+            if(_parameter != null) {
+                return _parameter.Name;
+            } else {
+                return _localizationService.GetLocalizedString("GroupParameter.NoParameter");
+            }
+        }
+
+        private string GetId() {
+            if(_parameter != null) {
+                return _parameter.Id;
+            }
+            return string.Empty;
+        }
+
+    }
+}
---
+++ src/RevitListOfSchedules/ViewModels/LinkViewModel.cs
@@ -0,0 +1,66 @@
+using System.Windows.Input;
+
+using Autodesk.Revit.DB;
+
+using dosymep.WPF.Commands;
+using dosymep.WPF.ViewModels;
+
+using RevitListOfSchedules.Models;
+
+namespace RevitListOfSchedules.ViewModels {
+    internal class LinkViewModel : BaseViewModel {
+
+        private readonly LinkTypeElement _linkElement;
+        private string _status;
+        private bool _isChecked;
+        private bool _isLoaded;
+
+        public LinkViewModel(LinkTypeElement linkElement) {
+            _linkElement = linkElement;
+            _status = GetStatus();
+
+            ReloadCommand = RelayCommand.Create(ReloadLinkType, CanReloadLinkType);
+        }
+        public ICommand ReloadCommand { get; set; }
+        public ElementId Id => _linkElement.Id;
+        public string Name => _linkElement.Name;
+        public string FullName => _linkElement.FullName;
+
+        public string Status {
+            get => _status;
+            private set => RaiseAndSetIfChanged(ref _status, value);
+        }
+
+        public bool IsLoaded {
+            get => _isLoaded;
+            set => RaiseAndSetIfChanged(ref _isLoaded, value, nameof(IsLoaded));
+        }
+
+        public bool IsChecked {
+            get => _isChecked;
+            set => RaiseAndSetIfChanged(ref _isChecked, value, nameof(IsChecked));
+        }
+
+        public void ReloadLinkType() {
+            _linkElement.Reload();
+            Status = GetStatus();
+        }
+
+        private bool CanReloadLinkType() {
+            if(_isChecked) {
+                return true;
+            }
+            return false;
+        }
+
+        private string GetStatus() {
+            SetStatus();
+            return $"{_linkElement.RevitLink.AttachmentType}_{_linkElement.RevitLink.GetLinkedFileStatus()}";
+
+        }
+
+        private void SetStatus() {
+            IsLoaded = _linkElement.RevitLink.GetLinkedFileStatus() is LinkedFileStatus.Loaded;
+        }
+    }
+}
