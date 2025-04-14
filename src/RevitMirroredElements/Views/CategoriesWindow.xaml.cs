1051f02ada733332dec030c49b20e1d812048bfc
Kirill Gubin <84503067+KGubin@users.noreply.github.com>
2025-04-14 12:31:26 +0300

    RevitMirroredElements: Добавлена поддержка WPF-UI (#173)

--- src/RevitMirroredElements/Views/CategoriesWindow.xaml.cs
+++ src/RevitMirroredElements/Views/CategoriesWindow.xaml.cs
@@ -1,9 +1,19 @@
 using System.Windows;
 
+using dosymep.SimpleServices;
+
 namespace RevitMirroredElements.Views {
 
-    public partial class CategoriesWindow : Window {
-        public CategoriesWindow() {
+    public partial class CategoriesWindow {
+        public CategoriesWindow(
+        ILoggerService loggerService,
+        ISerializationService serializationService,
+        ILanguageService languageService, ILocalizationService localizationService,
+        IUIThemeService uiThemeService, IUIThemeUpdaterService themeUpdaterService)
+        : base(loggerService,
+        serializationService,
+        languageService, localizationService,
+        uiThemeService, themeUpdaterService) {
             InitializeComponent();
         }
 
