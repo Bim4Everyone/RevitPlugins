d3865ebf9c66d3017cd100c50f578802736a5de6
KGubin <kirill2000gubin@gmail.com>
2025-04-14 15:17:53 +0300

    Создан новый плагин RevitRsnManager

---
+++ src/RevitRsnManager/README.md
@@ -0,0 +1,12 @@
+# RevitRsnManager (RSN.ini менеджер)
+Описание проекта 
+
+# Сборка проекта
+```
+nuke compile --profile RevitRsnManager
+```
+
+# Публикация проекта
+```
+nuke publish --profile RevitRsnManager
+```
---
+++ src/RevitRsnManager/Views/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+using System.Windows;
+
+namespace RevitRsnManager.Views;
+
+/// <summary>
+/// Класс главного окна плагина.
+/// </summary>
+public partial class MainWindow {
+    /// <summary>
+    /// Иницализирует главное окно плагина.
+    /// </summary>
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// Наименование плагина.
+    /// </summary>
+    /// <remarks>
+    /// Используется для сохранения положения окна.
+    /// </remarks>
+    public override string PluginName => nameof(RevitRsnManager);
+    
+    /// <summary>
+    /// Наименование файла конфигурации.
+    /// </summary>
+    /// <remarks>
+    /// Используется для сохранения положения окна.
+    /// </remarks>
+    public override string ProjectConfigName => nameof(MainWindow);
+    
+    private void ButtonOk_Click(object sender, RoutedEventArgs e) {
+        DialogResult = true;
+    }
+
+    private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
+        DialogResult = false;
+    }
+}
