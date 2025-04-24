caca4f5691ede9cf83c5bc8fbfa1310e7301b0c3
vlastroG <vlastroganov@gmail.com>
2025-04-24 09:36:20 +0300

    Добавить команду для настроек АР

--- src/RevitOpeningPlacement/Views/OpeningRealsArSettingsView.xaml.cs
+++ src/RevitOpeningPlacement/Views/OpeningRealsArSettingsView.xaml.cs
@@ -1,13 +1,13 @@
 using System.Windows;
 
 namespace RevitOpeningPlacement.Views {
-    public partial class OpeningRealsKrSettingsView {
-        public OpeningRealsKrSettingsView() {
+    public partial class OpeningRealsArSettingsView {
+        public OpeningRealsArSettingsView() {
             InitializeComponent();
         }
 
         public override string PluginName => nameof(RevitOpeningPlacement);
-        public override string ProjectConfigName => nameof(OpeningRealsKrSettingsView);
+        public override string ProjectConfigName => nameof(OpeningRealsArSettingsView);
 
         private void ButtonOk_Click(object sender, RoutedEventArgs e) {
             DialogResult = true;
