c2aa7558a9ee4cab20d2491021e6e406b0c99ff6
vlastroG <vlastroganov@gmail.com>
2025-04-29 15:38:09 +0300

    Скорректировать разметку окна ошибок

--- src/RevitFinishingWalls/Views/ErrorWindow.xaml.cs
+++ src/RevitFinishingWalls/Views/ErrorWindow.xaml.cs
@@ -16,16 +16,10 @@ namespace RevitFinishingWalls.Views {
               uiThemeService,
               themeUpdaterService) {
             InitializeComponent();
-            //Loaded += ThemedPlatformWindow_Loaded;
         }
 
 
         public override string PluginName => nameof(RevitFinishingWalls);
         public override string ProjectConfigName => nameof(ErrorWindow);
-
-        //private void ThemedPlatformWindow_Loaded(object sender, System.Windows.RoutedEventArgs e) {
-        //    _dgRooms.GroupBy(nameof(RoomErrorsViewModel.LevelName));
-        //    _dgErrors.GroupBy(nameof(ErrorViewModel.Title));
-        //}
     }
 }
