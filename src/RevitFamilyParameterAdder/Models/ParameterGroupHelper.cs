5cf48e09ce056b43a6a3cc31f0062a74834b5f34
Nikita Divin <nikitad93@mail.ru>
2025-06-04 15:29:56 +0300

    Обновление кода на C#12

--- src/RevitFamilyParameterAdder/Models/ParameterGroupHelper.cs
+++ src/RevitFamilyParameterAdder/Models/ParameterGroupHelper.cs
@@ -1,31 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Autodesk.Revit.DB;
 
-namespace RevitFamilyParameterAdder.Models
-{
-    internal class ParameterGroupHelper
-    {
+namespace RevitFamilyParameterAdder.Models;
+
+internal class ParameterGroupHelper {
 
 #if REVIT_2023_OR_LESS
-        public ParameterGroupHelper(BuiltInParameterGroup builtInParameterGroup) {
-            BuiltInParamGroup = builtInParameterGroup;
-            GroupName = LabelUtils.GetLabelFor(builtInParameterGroup);
-        }
+    public ParameterGroupHelper(BuiltInParameterGroup builtInParameterGroup) {
+        BuiltInParamGroup = builtInParameterGroup;
+        GroupName = LabelUtils.GetLabelFor(builtInParameterGroup);
+    }
 #else
-        public ParameterGroupHelper(ForgeTypeId builtInParameterGroup) {
-            BuiltInParamGroup = builtInParameterGroup;
-            if(builtInParameterGroup != null) {
-                GroupName = LabelUtils.GetLabelForGroup(builtInParameterGroup);
-            }
+    public ParameterGroupHelper(ForgeTypeId builtInParameterGroup) {
+        BuiltInParamGroup = builtInParameterGroup;
+        if(builtInParameterGroup != null) {
+            GroupName = LabelUtils.GetLabelForGroup(builtInParameterGroup);
         }
+    }
 #endif
 
-        internal object BuiltInParamGroup { get; set; }
-        public string GroupName { get; set; }
-    }
+    internal object BuiltInParamGroup { get; set; }
+    public string GroupName { get; set; }
 }
--- src/RevitFamilyParameterAdder/Views/MainWindow.xaml.cs
+++ src/RevitFamilyParameterAdder/Views/MainWindow.xaml.cs
@@ -1,28 +1,27 @@
-﻿using System.Windows;
+using System.Windows;
 
-namespace RevitFamilyParameterAdder.Views {
-    public partial class MainWindow {
-        public MainWindow() {
-            InitializeComponent();
-        }
+namespace RevitFamilyParameterAdder.Views;
+public partial class MainWindow {
+    public MainWindow() {
+        InitializeComponent();
+    }
 
-        public override string PluginName => nameof(RevitFamilyParameterAdder);
-        public override string ProjectConfigName => nameof(MainWindow);
+    public override string PluginName => nameof(RevitFamilyParameterAdder);
+    public override string ProjectConfigName => nameof(MainWindow);
 
-        private void ButtonOk_Click(object sender, RoutedEventArgs e) {
-            DialogResult = true;
-        }
+    private void ButtonOk_Click(object sender, RoutedEventArgs e) {
+        DialogResult = true;
+    }
 
-        private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
-            DialogResult = false;
-        }
+    private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
+        DialogResult = false;
+    }
 
 
-        private void SelectAllParams(object sender, RoutedEventArgs e) {
-            parameters.SelectAll();
-        }
-        private void UnselectAllParams(object sender, RoutedEventArgs e) {
-            parameters.UnselectAll();
-        }
+    private void SelectAllParams(object sender, RoutedEventArgs e) {
+        parameters.SelectAll();
+    }
+    private void UnselectAllParams(object sender, RoutedEventArgs e) {
+        parameters.UnselectAll();
     }
 }
