27f7fffda751025c5084a1c8b3151a1e14f66928
Nikita Divin <nikitad93@mail.ru>
2025-06-04 15:13:18 +0300

    Обновление кода на C#12

--- src/RevitShakeSpecs/RevitShakeSpecsCommand.cs
+++ src/RevitShakeSpecs/RevitShakeSpecsCommand.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-using dosymep;
 using dosymep.Bim4Everyone;
 using dosymep.SimpleServices;
 
@@ -21,46 +15,44 @@ using RevitShakeSpecs.Views;
 
 using Application = Autodesk.Revit.ApplicationServices.Application;
 
-namespace RevitShakeSpecs {
-    [Transaction(TransactionMode.Manual)]
-    public class RevitShakeSpecsCommand : BasePluginCommand {
-        public RevitShakeSpecsCommand() {
-            PluginName = "Встряхнуть спецификации";
-        }
+namespace RevitShakeSpecs;
+[Transaction(TransactionMode.Manual)]
+public class RevitShakeSpecsCommand : BasePluginCommand {
+    public RevitShakeSpecsCommand() {
+        PluginName = "Встряхнуть спецификации";
+    }
 
-        protected override void Execute(UIApplication uiApplication) {
-            using(IKernel kernel = new StandardKernel()) {
-                kernel.Bind<UIApplication>()
-                    .ToConstant(uiApplication)
-                    .InTransientScope();
-                kernel.Bind<Application>()
-                    .ToConstant(uiApplication.Application)
-                    .InTransientScope();
+    protected override void Execute(UIApplication uiApplication) {
+        using IKernel kernel = new StandardKernel();
+        kernel.Bind<UIApplication>()
+            .ToConstant(uiApplication)
+            .InTransientScope();
+        kernel.Bind<Application>()
+            .ToConstant(uiApplication.Application)
+            .InTransientScope();
 
-                kernel.Bind<RevitRepository>()
-                    .ToSelf()
-                    .InSingletonScope();
+        kernel.Bind<RevitRepository>()
+            .ToSelf()
+            .InSingletonScope();
 
-                kernel.Bind<PluginConfig>()
-                    .ToMethod(c => PluginConfig.GetPluginConfig());
+        kernel.Bind<PluginConfig>()
+            .ToMethod(c => PluginConfig.GetPluginConfig());
 
-                kernel.Bind<MainViewModel>().ToSelf();
-                kernel.Bind<MainWindow>().ToSelf()
-                    .WithPropertyValue(nameof(Window.Title), PluginName)
-                    .WithPropertyValue(nameof(Window.DataContext),
-                        c => c.Kernel.Get<MainViewModel>());
+        kernel.Bind<MainViewModel>().ToSelf();
+        kernel.Bind<MainWindow>().ToSelf()
+            .WithPropertyValue(nameof(Window.Title), PluginName)
+            .WithPropertyValue(nameof(Window.DataContext),
+                c => c.Kernel.Get<MainViewModel>());
 
-                MainWindow window = kernel.Get<MainWindow>();
-                if(window.ShowDialog() == true) {
-                    GetPlatformService<INotificationService>()
-                        .CreateNotification(PluginName, "Выполнение скрипта завершено успешно.", "C#")
-                        .ShowAsync();
-                } else {
-                    GetPlatformService<INotificationService>()
-                        .CreateWarningNotification(PluginName, "Выполнение скрипта отменено.")
-                        .ShowAsync();
-                }
-            }
+        MainWindow window = kernel.Get<MainWindow>();
+        if(window.ShowDialog() == true) {
+            GetPlatformService<INotificationService>()
+                .CreateNotification(PluginName, "Выполнение скрипта завершено успешно.", "C#")
+                .ShowAsync();
+        } else {
+            GetPlatformService<INotificationService>()
+                .CreateWarningNotification(PluginName, "Выполнение скрипта отменено.")
+                .ShowAsync();
         }
     }
 }
