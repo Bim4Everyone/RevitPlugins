48bcc209dd13b7f902700ea11a34d6a2f07f0c1e
Nikita Divin <nikitad93@mail.ru>
2025-06-04 16:37:10 +0300

    Произведено обновление кода на C#12

--- src/RevitRemoveRoomTags/Models/PluginConfig.cs
+++ src/RevitRemoveRoomTags/Models/PluginConfig.cs
@@ -4,25 +4,24 @@ using dosymep.Serializers;
 
 using pyRevitLabs.Json;
 
-namespace RevitRemoveRoomTags.Models {
-    internal class PluginConfig : ProjectConfig<PluginSettings> {
-        [JsonIgnore] public override string ProjectConfigPath { get; set; }
+namespace RevitRemoveRoomTags.Models;
+internal class PluginConfig : ProjectConfig<PluginSettings> {
+    [JsonIgnore] public override string ProjectConfigPath { get; set; }
 
-        [JsonIgnore] public override IConfigSerializer Serializer { get; set; }
+    [JsonIgnore] public override IConfigSerializer Serializer { get; set; }
 
-        public static PluginConfig GetPluginConfig() {
-            return new ProjectConfigBuilder()
-                .SetSerializer(new ConfigSerializer())
-                .SetPluginName(nameof(RevitRemoveRoomTags))
-                .SetRevitVersion(ModuleEnvironment.RevitVersion)
-                .SetProjectConfigName(nameof(PluginConfig) + ".json")
-                .Build<PluginConfig>();
-        }
+    public static PluginConfig GetPluginConfig() {
+        return new ProjectConfigBuilder()
+            .SetSerializer(new ConfigSerializer())
+            .SetPluginName(nameof(RevitRemoveRoomTags))
+            .SetRevitVersion(ModuleEnvironment.RevitVersion)
+            .SetProjectConfigName(nameof(PluginConfig) + ".json")
+            .Build<PluginConfig>();
     }
+}
 
-    internal class PluginSettings : ProjectSettings {
-        public override string ProjectName { get; set; }
+internal class PluginSettings : ProjectSettings {
+    public override string ProjectName { get; set; }
 
-        public bool NeedOpenSelectedViews { get; set; }
-    }
+    public bool NeedOpenSelectedViews { get; set; }
 }
