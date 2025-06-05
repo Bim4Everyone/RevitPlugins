b8471bb93ac18cf51b93d9227243ec4480cce3e2
sssenya <tikhomirov_am@a101.ru>
2025-06-05 17:44:15 +0300

    Обновлено MainWindow для фильтрации помещений по назначению и уровню

--- src/RevitFinishing/Views/MainWindow.xaml.cs
+++ src/RevitFinishing/Views/MainWindow.xaml.cs
@@ -49,20 +49,4 @@ public partial class MainWindow {
     private void ButtonCancel_Click(object sender, RoutedEventArgs e) {
         DialogResult = false;
     }
-
-    private void CheckBox_Checked(object sender, RoutedEventArgs e) {
-        ChangeSelected(true);
-    }
-
-    private void CheckBox_Unchecked(object sender, RoutedEventArgs e) {
-        ChangeSelected(false);
-    }
-
-    private void ChangeSelected(bool state) {
-        var listBox = (ListBox) FindName("RoomGroups");
-        var groups = listBox.SelectedItems;
-        foreach(RoomGroupViewModel group in groups) {
-            group.IsChecked = state;
-        }
-    }
 }
