0f75bdd1dd6ceebfaa2615784ae6e57d68eb0a16
Nikita Divin <nikitad93@mail.ru>
2025-06-05 11:38:07 +0300

    Обновление кода на C#12

--- src/RevitOverridingGraphicsInViews/Models/RevitRepository.cs
+++ src/RevitOverridingGraphicsInViews/Models/RevitRepository.cs
@@ -1,27 +1,26 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-namespace RevitOverridingGraphicsInViews.Models {
-    internal class RevitRepository {
-        public RevitRepository(UIApplication uiApplication) {
-            UIApplication = uiApplication;
-        }
-
-        public UIApplication UIApplication { get; }
-        public UIDocument ActiveUIDocument => UIApplication.ActiveUIDocument;
-
-        public Application Application => UIApplication.Application;
-        public Document Document => ActiveUIDocument.Document;
-
-        public List<View> AllViews => new FilteredElementCollector(Document)
-               .OfClass(typeof(View))
-               .WhereElementIsNotElementType()
-               .OfType<View>()
-               .Where(view => !view.IsTemplate)
-               .ToList();
+namespace RevitOverridingGraphicsInViews.Models;
+internal class RevitRepository {
+    public RevitRepository(UIApplication uiApplication) {
+        UIApplication = uiApplication;
     }
+
+    public UIApplication UIApplication { get; }
+    public UIDocument ActiveUIDocument => UIApplication.ActiveUIDocument;
+
+    public Application Application => UIApplication.Application;
+    public Document Document => ActiveUIDocument.Document;
+
+    public List<View> AllViews => new FilteredElementCollector(Document)
+           .OfClass(typeof(View))
+           .WhereElementIsNotElementType()
+           .OfType<View>()
+           .Where(view => !view.IsTemplate)
+           .ToList();
 }
--- src/RevitOverridingGraphicsInViews/Views/MainWindow.xaml.cs
+++ src/RevitOverridingGraphicsInViews/Views/MainWindow.xaml.cs
@@ -1,48 +1,41 @@
-﻿using System.Diagnostics;
-using System;
+using System.Media;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
-using System.Media;
-using Microsoft.Win32;
-using System.Windows.Media;
-using System.IO;
-using System.Reflection;
-using Autodesk.Revit.UI;
 
-namespace RevitOverridingGraphicsInViews.Views {
-    public partial class MainWindow {
+namespace RevitOverridingGraphicsInViews.Views;
+public partial class MainWindow {
 
-        public MainWindow() {
-            InitializeComponent();
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    public override string PluginName => nameof(RevitOverridingGraphicsInViews);
+    public override string ProjectConfigName => nameof(MainWindow);
+
+
+    private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+
+        if(e.ChangedButton == MouseButton.Left) {
+            DragMove();
         }
+    }
 
-        public override string PluginName => nameof(RevitOverridingGraphicsInViews);
-        public override string ProjectConfigName => nameof(MainWindow);
+    private void WindowCloseCommand(object sender, System.Windows.Input.ExecutedRoutedEventArgs e) {
 
+        var player = new SoundPlayer {
+            Stream = Properties.Resources.PaintSound
+        };
+        player.Play();
 
-        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-
-            if(e.ChangedButton == MouseButton.Left) {
-                this.DragMove();
-            }
-        }
-
-        private void WindowCloseCommand(object sender, System.Windows.Input.ExecutedRoutedEventArgs e) {
-
-            SoundPlayer player = new SoundPlayer();
-            player.Stream = Properties.Resources.PaintSound;
-            player.Play();
-
-            this.Close();
-        }
+        Close();
+    }
 
 
-        private void ColorButton_Click(object sender, RoutedEventArgs e) {
+    private void ColorButton_Click(object sender, RoutedEventArgs e) {
 
-            SoundPlayer player = new SoundPlayer();
-            player.Stream = Properties.Resources.SelectColorSound;
-            player.Play();
-        }
+        var player = new SoundPlayer {
+            Stream = Properties.Resources.SelectColorSound
+        };
+        player.Play();
     }
 }
