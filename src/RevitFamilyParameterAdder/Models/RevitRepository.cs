9d1b0620dbbe9e6210a56f6a4abfdc111f60d1b9
Nikita Divin <72355844+DivinN@users.noreply.github.com>
2025-06-06 13:08:16 +0300

    RevitFamilyParameterAdder: Обновление кода на C#12 (#217)

--- src/RevitFamilyParameterAdder/Models/RevitRepository.cs
+++ src/RevitFamilyParameterAdder/Models/RevitRepository.cs
@@ -1,54 +1,45 @@
-﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-namespace RevitFamilyParameterAdder.Models {
-    internal class RevitRepository {
-        public RevitRepository(UIApplication uiApplication) {
-            UIApplication = uiApplication;
-        }
+namespace RevitFamilyParameterAdder.Models;
+internal class RevitRepository {
+    public RevitRepository(UIApplication uiApplication) {
+        UIApplication = uiApplication;
+    }
 
-        public UIApplication UIApplication { get; }
-        public UIDocument ActiveUIDocument => UIApplication.ActiveUIDocument;
+    public UIApplication UIApplication { get; }
+    public UIDocument ActiveUIDocument => UIApplication.ActiveUIDocument;
 
-        public Application Application => UIApplication.Application;
-        public Document Document => ActiveUIDocument.Document;
-
-
-        public List<string> GetParamGroupNames() => Application.OpenSharedParameterFile().Groups
-            .Select(item => item.Name)
-            .OrderBy(item => item)
-            .ToList();
+    public Application Application => UIApplication.Application;
+    public Document Document => ActiveUIDocument.Document;
 
 
-        public List<ExternalDefinition> GetParamsInShPF() {
-            List<ExternalDefinition> paramsInGroup = new List<ExternalDefinition>();
+    public List<string> GetParamGroupNames() => Application.OpenSharedParameterFile().Groups
+        .Select(item => item.Name)
+        .OrderBy(item => item)
+        .ToList();
 
-            DefinitionFile sharedParametersFile = Application.OpenSharedParameterFile();
-            // Проходимся по каждой группе ФОП
-            foreach(DefinitionGroup sharedGroup in sharedParametersFile.Groups) {
-                // Проходимся по каждому параметру в группе параметров
-                foreach(var item in sharedGroup.Definitions) {
-                    paramsInGroup.Add(item as ExternalDefinition);
-                }
-            }
+    public List<ExternalDefinition> GetParamsInShPF() {
+        List<ExternalDefinition> paramsInGroup = [];
 
-            paramsInGroup.Sort((x, y) => string.Compare(x.Name, y.Name));
-            return paramsInGroup;
-        }
-
-        internal bool IsFamilyFile() {
-            if(Document.IsFamilyDocument) {
-                return true;
-            } else {
-                return false;
+        DefinitionFile sharedParametersFile = Application.OpenSharedParameterFile();
+        // Проходимся по каждой группе ФОП
+        foreach(DefinitionGroup sharedGroup in sharedParametersFile.Groups) {
+            // Проходимся по каждому параметру в группе параметров
+            foreach(var item in sharedGroup.Definitions) {
+                paramsInGroup.Add(item as ExternalDefinition);
             }
         }
+
+        paramsInGroup.Sort((x, y) => string.Compare(x.Name, y.Name));
+        return paramsInGroup;
+    }
+
+    internal bool IsFamilyFile() {
+        return Document.IsFamilyDocument;
     }
 }
--- src/RevitFamilyParameterAdder/RevitFamilyParameterAdderCommand.cs
+++ src/RevitFamilyParameterAdder/RevitFamilyParameterAdderCommand.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
 
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-using dosymep;
 using dosymep.Bim4Everyone;
 using dosymep.SimpleServices;
 
@@ -21,57 +16,55 @@ using RevitFamilyParameterAdder.Views;
 
 using Application = Autodesk.Revit.ApplicationServices.Application;
 
-namespace RevitFamilyParameterAdder {
-    [Transaction(TransactionMode.Manual)]
-    public class RevitFamilyParameterAdderCommand : BasePluginCommand {
-        public RevitFamilyParameterAdderCommand() {
-            PluginName = "Добавление параметров в семейство";
-        }
+namespace RevitFamilyParameterAdder;
+[Transaction(TransactionMode.Manual)]
+public class RevitFamilyParameterAdderCommand : BasePluginCommand {
+    public RevitFamilyParameterAdderCommand() {
+        PluginName = "Добавление параметров в семейство";
+    }
 
-        protected override void Execute(UIApplication uiApplication) {
-            using(IKernel kernel = new StandardKernel()) {
-                kernel.Bind<UIApplication>()
-                    .ToConstant(uiApplication)
-                    .InTransientScope();
-                kernel.Bind<Application>()
-                    .ToConstant(uiApplication.Application)
-                    .InTransientScope();
+    protected override void Execute(UIApplication uiApplication) {
+        using IKernel kernel = new StandardKernel();
+        kernel.Bind<UIApplication>()
+            .ToConstant(uiApplication)
+            .InTransientScope();
+        kernel.Bind<Application>()
+            .ToConstant(uiApplication.Application)
+            .InTransientScope();
 
-                kernel.Bind<RevitRepository>()
-                    .ToSelf()
-                    .InSingletonScope();
+        kernel.Bind<RevitRepository>()
+            .ToSelf()
+            .InSingletonScope();
 
-                kernel.Bind<PluginConfig>()
-                    .ToMethod(c => PluginConfig.GetPluginConfig());
+        kernel.Bind<PluginConfig>()
+            .ToMethod(c => PluginConfig.GetPluginConfig());
 
-                kernel.Bind<MainViewModel>().ToSelf();
-                kernel.Bind<MainWindow>().ToSelf()
-                    .WithPropertyValue(nameof(Window.Title), PluginName)
-                    .WithPropertyValue(nameof(Window.DataContext),
-                        c => c.Kernel.Get<MainViewModel>());
+        kernel.Bind<MainViewModel>().ToSelf();
+        kernel.Bind<MainWindow>().ToSelf()
+            .WithPropertyValue(nameof(Window.Title), PluginName)
+            .WithPropertyValue(nameof(Window.DataContext),
+                c => c.Kernel.Get<MainViewModel>());
 
-                Check(kernel);
+        Check(kernel);
 
-                MainWindow window = kernel.Get<MainWindow>();
-                if(window.ShowDialog() == true) {
-                    GetPlatformService<INotificationService>()
-                        .CreateNotification(PluginName, "Выполнение скрипта завершено успешно.", "C#")
+        MainWindow window = kernel.Get<MainWindow>();
+        if(window.ShowDialog() == true) {
+            GetPlatformService<INotificationService>()
+                .CreateNotification(PluginName, "Выполнение скрипта завершено успешно.", "C#")
                         .ShowAsync();
-                } else {
-                    GetPlatformService<INotificationService>()
-                        .CreateWarningNotification(PluginName, "Выполнение скрипта отменено.")
-                        .ShowAsync();
-                }
-            }
+        } else {
+             GetPlatformService<INotificationService>()
+                .CreateWarningNotification(PluginName, "Выполнение скрипта отменено.")
+                .ShowAsync();
         }
+    }
 
-        private void Check(IKernel kernel) {
-            var revitRepositiry = kernel.Get<RevitRepository>();
-            if(!revitRepositiry.IsFamilyFile()) {
-                TaskDialog.Show(PluginName,
-                    $"Данный скрипт работает только в файле семейства.");
-                throw new OperationCanceledException();
-            }
+    private void Check(IKernel kernel) {
+        var revitRepositiry = kernel.Get<RevitRepository>();
+        if(!revitRepositiry.IsFamilyFile()) {
+            TaskDialog.Show(PluginName,
+                $"Данный скрипт работает только в файле семейства.");
+            throw new OperationCanceledException();
         }
     }
 }
