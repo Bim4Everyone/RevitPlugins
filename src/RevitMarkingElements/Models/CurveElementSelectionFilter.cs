fb87cd972431faf26ae3395cee5e6d73efd8dedb
Kirill Gubin <84503067+KGubin@users.noreply.github.com>
2025-06-06 13:34:52 +0300

    RevitMarkingElements: Обновление кода на C#12 (#225)

--- src/RevitMarkingElements/Models/CurveElementSelectionFilter.cs
+++ src/RevitMarkingElements/Models/CurveElementSelectionFilter.cs
@@ -1,13 +1,12 @@
 using Autodesk.Revit.DB;
 
-namespace RevitMarkingElements.Models {
-    public class CurveElementSelectionFilter : Autodesk.Revit.UI.Selection.ISelectionFilter {
-        public bool AllowElement(Element element) {
-            return element is ModelLine || element is ModelNurbSpline;
-        }
+namespace RevitMarkingElements.Models;
+public class CurveElementSelectionFilter : Autodesk.Revit.UI.Selection.ISelectionFilter {
+    public bool AllowElement(Element element) {
+        return element is ModelLine or ModelNurbSpline;
+    }
 
-        public bool AllowReference(Reference reference, XYZ position) {
-            return false;
-        }
+    public bool AllowReference(Reference reference, XYZ position) {
+        return false;
     }
 }
--- src/RevitMarkingElements/Models/RevitRepository.cs
+++ src/RevitMarkingElements/Models/RevitRepository.cs
@@ -11,126 +11,116 @@ using dosymep.Revit;
 using dosymep.Revit.Geometry;
 using dosymep.SimpleServices;
 
-namespace RevitMarkingElements.Models {
-    internal class RevitRepository {
+namespace RevitMarkingElements.Models;
+internal class RevitRepository {
 
-        private readonly ILocalizationService _localizationService;
-        public RevitRepository(UIApplication uiApplication, ILocalizationService localizationService) {
-            UIApplication = uiApplication;
-            _localizationService = localizationService;
-        }
+    private readonly ILocalizationService _localizationService;
+    public RevitRepository(UIApplication uiApplication, ILocalizationService localizationService) {
+        UIApplication = uiApplication;
+        _localizationService = localizationService;
+    }
 
-        public UIApplication UIApplication { get; }
-        public UIDocument ActiveUIDocument => UIApplication.ActiveUIDocument;
-        public Application Application => UIApplication.Application;
-        public Document Document => ActiveUIDocument.Document;
+    public UIApplication UIApplication { get; }
+    public UIDocument ActiveUIDocument => UIApplication.ActiveUIDocument;
+    public Application Application => UIApplication.Application;
+    public Document Document => ActiveUIDocument.Document;
 
-        public List<Element> GetElementsIntersectingLine(List<Element> elements, Curve lineElement) {
-            var intersectingElements = new List<Element>();
+    public List<Element> GetElementsIntersectingLine(List<Element> elements, Curve lineElement) {
+        var intersectingElements = new List<Element>();
 
-            foreach(var element in elements) {
-                var solids = element.GetSolids().Where(s => s.Volume > 0).ToList();
+        foreach(var element in elements) {
+            var solids = element.GetSolids().Where(s => s.Volume > 0).ToList();
 
-                if(solids.Count == 0) {
-                    var boundingBox = element.get_BoundingBox(null);
-                    if(boundingBox != null) {
-                        var boundingBoxSolid = boundingBox.CreateSolid();
-                        if(IsLineIntersectingSolid(lineElement, boundingBoxSolid)) {
-                            intersectingElements.Add(element);
-                            continue;
-                        }
-                    }
-                } else {
-                    if(solids.Any(solid => IsLineIntersectingSolid(lineElement, solid))) {
+            if(solids.Count == 0) {
+                var boundingBox = element.get_BoundingBox(null);
+                if(boundingBox != null) {
+                    var boundingBoxSolid = boundingBox.CreateSolid();
+                    if(IsLineIntersectingSolid(lineElement, boundingBoxSolid)) {
                         intersectingElements.Add(element);
                         continue;
                     }
                 }
+            } else {
+                if(solids.Any(solid => IsLineIntersectingSolid(lineElement, solid))) {
+                    intersectingElements.Add(element);
+                    continue;
+                }
             }
-
-            return intersectingElements;
         }
 
-        private bool IsLineIntersectingSolid(Curve line, Solid solid) {
-            var offsetTransform = Transform.CreateTranslation(new XYZ(0, 0, -solid.ComputeCentroid().Z));
-            var offsetSolid = SolidUtils.CreateTransformed(solid, offsetTransform);
+        return intersectingElements;
+    }
 
-            var options = new SolidCurveIntersectionOptions {
-                ResultType = SolidCurveIntersectionMode.CurveSegmentsInside
-            };
+    private bool IsLineIntersectingSolid(Curve line, Solid solid) {
+        var offsetTransform = Transform.CreateTranslation(new XYZ(0, 0, -solid.ComputeCentroid().Z));
+        var offsetSolid = SolidUtils.CreateTransformed(solid, offsetTransform);
 
-            var intersection = offsetSolid.IntersectWithCurve(line, options);
+        var options = new SolidCurveIntersectionOptions {
+            ResultType = SolidCurveIntersectionMode.CurveSegmentsInside
+        };
 
-            return intersection.SegmentCount > 0 && intersection.GetCurveSegment(0).Length > 0.1;
-        }
+        var intersection = offsetSolid.IntersectWithCurve(line, options);
 
-        public List<CurveElement> SelectLinesOnView() {
-            var selectedLines = new List<CurveElement>();
-            var lineSelectionFilter = new CurveElementSelectionFilter();
-            var finishLineSelection = _localizationService.GetLocalizedString("MainWindow.FinishLineSelection");
-            var isDone = false;
+        return intersection.SegmentCount > 0 && intersection.GetCurveSegment(0).Length > 0.1;
+    }
 
-            // Данная реализация была выбрана для того что бы получить объекты именно в том порядке,
-            // в котором они были выбраны на виде.
-            while(!isDone) {
-                try {
-                    var reference = ActiveUIDocument.Selection.PickObject(
-                        ObjectType.Element,
-                        lineSelectionFilter,
-                        finishLineSelection);
+    public List<CurveElement> SelectLinesOnView() {
+        var selectedLines = new List<CurveElement>();
+        var lineSelectionFilter = new CurveElementSelectionFilter();
+        string finishLineSelection = _localizationService.GetLocalizedString("MainWindow.FinishLineSelection");
+        bool isDone = false;
 
-                    if(reference != null) {
-                        var element = Document.GetElement(reference.ElementId) as CurveElement;
-                        if(element != null && element.GeometryCurve != null) {
-                            selectedLines.Add(element);
-                        }
+        // Данная реализация была выбрана для того что бы получить объекты именно в том порядке,
+        // в котором они были выбраны на виде.
+        while(!isDone) {
+            try {
+                var reference = ActiveUIDocument.Selection.PickObject(
+                    ObjectType.Element,
+                    lineSelectionFilter,
+                    finishLineSelection);
+
+                if(reference != null) {
+                    if(Document.GetElement(reference.ElementId) is CurveElement element && element.GeometryCurve != null) {
+                        selectedLines.Add(element);
                     }
-                } catch(Autodesk.Revit.Exceptions.OperationCanceledException) {
-                    isDone = true;
                 }
+            } catch(Autodesk.Revit.Exceptions.OperationCanceledException) {
+                isDone = true;
             }
-
-            var uniqueLines = new List<CurveElement>();
-            var orderedIds = new HashSet<ElementId>();
-
-            foreach(var line in selectedLines) {
-                if(!orderedIds.Contains(line.Id)) {
-                    orderedIds.Add(line.Id);
-                    uniqueLines.Add(line);
-                }
-            }
-
-            return uniqueLines;
         }
 
-        public List<Element> GetSelectedElements() {
-            var selectedElementIds = ActiveUIDocument.Selection.GetElementIds();
+        var uniqueLines = new List<CurveElement>();
+        var orderedIds = new HashSet<ElementId>();
 
-            if(selectedElementIds == null || !selectedElementIds.Any()) {
-                return new List<Element>();
+        foreach(var line in selectedLines) {
+            if(!orderedIds.Contains(line.Id)) {
+                _ = orderedIds.Add(line.Id);
+                uniqueLines.Add(line);
             }
-
-            var selectedElements = selectedElementIds
-                 .Select(id => Document.GetElement(id))
-                 .Where(element => element != null)
-                 .Where(item => item.IsExistsParam(BuiltInParameter.ALL_MODEL_MARK))
-                 .ToList();
-
-            return selectedElements;
         }
 
-        public XYZ GetElementCoordinates(Element element) {
-            var locationPoint = element.Location as LocationPoint;
-            if(locationPoint != null) {
-                return locationPoint.Point;
-            }
+        return uniqueLines;
+    }
 
-            var locationCurve = element.Location as LocationCurve;
-            if(locationCurve != null) {
-                return locationCurve.Curve.Evaluate(0.5, true);
-            }
+    public List<Element> GetSelectedElements() {
+        var selectedElementIds = ActiveUIDocument.Selection.GetElementIds();
 
-            return null;
+        if(selectedElementIds == null || !selectedElementIds.Any()) {
+            return [];
         }
+
+        var selectedElements = selectedElementIds
+             .Select(Document.GetElement)
+             .Where(element => element != null)
+             .Where(item => item.IsExistsParam(BuiltInParameter.ALL_MODEL_MARK))
+             .ToList();
+
+        return selectedElements;
+    }
+
+    public XYZ GetElementCoordinates(Element element) {
+        return element.Location is LocationPoint locationPoint
+            ? locationPoint.Point
+            : element.Location is LocationCurve locationCurve ? locationCurve.Curve.Evaluate(0.5, true) : null;
     }
 }
