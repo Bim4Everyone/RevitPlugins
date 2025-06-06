f7da46478c13cb3a2ba42c13572c2c0f69ec7f3b
Nikita Divin <72355844+DivinN@users.noreply.github.com>
2025-06-06 13:18:23 +0300

    RevitRemoveRoomTags: Обновление кода на C#12 (#220)

--- src/RevitRemoveRoomTags/Models/UnitUtilsHelper.cs
+++ src/RevitRemoveRoomTags/Models/UnitUtilsHelper.cs
@@ -1,34 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Autodesk.Revit.DB;
 
-namespace RevitRemoveRoomTags.Models {
-    internal class UnitUtilsHelper {
-        public static double ConvertFromInternalValue(double value) {
-            // Перевод во внутренние единицы Revit
+namespace RevitRemoveRoomTags.Models;
+internal class UnitUtilsHelper {
+    public static double ConvertFromInternalValue(double value) {
+        // Перевод во внутренние единицы Revit
 
 #if REVIT_2021_OR_GREATER
-            ForgeTypeId unitType = UnitTypeId.Millimeters;
+        var unitType = UnitTypeId.Millimeters;
 #else
-            DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
+        DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
 #endif
-            return UnitUtils.ConvertFromInternalUnits(value, unitType);
-        }
+        return UnitUtils.ConvertFromInternalUnits(value, unitType);
+    }
 
 
-        public static double ConvertToInternalValue(double value) {
-            // Перевод во внутренние единицы Revit
+    public static double ConvertToInternalValue(double value) {
+        // Перевод во внутренние единицы Revit
 
 #if REVIT_2021_OR_GREATER
-            ForgeTypeId unitType = UnitTypeId.Millimeters;
+        var unitType = UnitTypeId.Millimeters;
 #else
-            DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
+        DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS;
 #endif
-            return UnitUtils.ConvertToInternalUnits(value, unitType);
-        }
+        return UnitUtils.ConvertToInternalUnits(value, unitType);
     }
 }
