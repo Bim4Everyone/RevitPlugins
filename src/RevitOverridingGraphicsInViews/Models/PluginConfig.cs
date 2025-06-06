8c0dabb882b0cb7763d6a9512fb389a0add97a94
Nikita Divin <72355844+DivinN@users.noreply.github.com>
2025-06-06 13:22:18 +0300

    RevitOverridingGraphicsInViews: Обновление кода на C#12 (#221)

--- src/RevitOverridingGraphicsInViews/Models/PluginConfig.cs
+++ src/RevitOverridingGraphicsInViews/Models/PluginConfig.cs
@@ -1,34 +1,33 @@
-﻿using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone;
 using dosymep.Bim4Everyone.ProjectConfigs;
 using dosymep.Serializers;
 
 using pyRevitLabs.Json;
 
-namespace RevitOverridingGraphicsInViews.Models {
-    internal class PluginConfig : ProjectConfig<PluginSettings> {
-        [JsonIgnore] public override string ProjectConfigPath { get; set; }
+namespace RevitOverridingGraphicsInViews.Models;
+internal class PluginConfig : ProjectConfig<PluginSettings> {
+    [JsonIgnore] public override string ProjectConfigPath { get; set; }
 
-        [JsonIgnore] public override IConfigSerializer Serializer { get; set; }
+    [JsonIgnore] public override IConfigSerializer Serializer { get; set; }
 
-        public static PluginConfig GetPluginConfig() {
-            return new ProjectConfigBuilder()
-                .SetSerializer(new ConfigSerializer())
-                .SetPluginName(nameof(RevitOverridingGraphicsInViews))
-                .SetRevitVersion(ModuleEnvironment.RevitVersion)
-                .SetProjectConfigName(nameof(PluginConfig) + ".json")
-                .Build<PluginConfig>();
-        }
+    public static PluginConfig GetPluginConfig() {
+        return new ProjectConfigBuilder()
+            .SetSerializer(new ConfigSerializer())
+            .SetPluginName(nameof(RevitOverridingGraphicsInViews))
+            .SetRevitVersion(ModuleEnvironment.RevitVersion)
+            .SetProjectConfigName(nameof(PluginConfig) + ".json")
+            .Build<PluginConfig>();
     }
+}
 
-    internal class PluginSettings : ProjectSettings {
-        public override string ProjectName { get; set; }
+internal class PluginSettings : ProjectSettings {
+    public override string ProjectName { get; set; }
 
-        public ColorHelper SelectedColor { get; set; }
-        public ColorHelper Color1 { get; set; }
-        public ColorHelper Color2 { get; set; }
-        public ColorHelper Color3 { get; set; }
-        public ColorHelper Color4 { get; set; }
-        public ColorHelper Color5 { get; set; }
+    public ColorHelper SelectedColor { get; set; }
+    public ColorHelper Color1 { get; set; }
+    public ColorHelper Color2 { get; set; }
+    public ColorHelper Color3 { get; set; }
+    public ColorHelper Color4 { get; set; }
+    public ColorHelper Color5 { get; set; }
 
-    }
 }
