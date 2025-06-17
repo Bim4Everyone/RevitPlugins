10bea51647200afcc81886c63d704e7206d700f0
vlastroG <vlastroganov@gmail.com>
2025-06-17 18:17:19 +0300

    Добавить классы для настроек фильтрации

---
+++ src/RevitSleeves/ViewModels/Filtration/ParamValueViewModel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+using dosymep.WPF.ViewModels;
+
+using RevitClashDetective.Models.Value;
+
+namespace RevitSleeves.ViewModels.Filtration;
+internal class ParamValueViewModel : BaseViewModel, IEquatable<ParamValueViewModel> {
+    public ParamValueViewModel(ParamValue paramValue) {
+        ParamValue = paramValue ?? throw new ArgumentNullException(nameof(paramValue));
+    }
+
+
+    public object Value => ParamValue.Value;
+
+    public string DisplayValue => ParamValue.DisplayValue;
+
+    public ParamValue ParamValue { get; }
+
+
+    public override bool Equals(object obj) {
+        return Equals(obj as ParamValueViewModel);
+    }
+
+    public bool Equals(ParamValueViewModel other) {
+        return other != null
+            && ParamValue.Equals(other.ParamValue);
+    }
+
+    public override int GetHashCode() {
+        int hashCode = 931601283;
+        hashCode = hashCode * -1521134295 + EqualityComparer<ParamValue>.Default.GetHashCode(ParamValue);
+        return hashCode;
+    }
+}
