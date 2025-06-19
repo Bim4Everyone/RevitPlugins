458184f03b392bd418dbb3eb2171a6409c5bc61d
vlastroG <vlastroganov@gmail.com>
2025-06-19 17:56:55 +0300

    Добавить значения по умолчанию

--- src/RevitSleeves/Models/Config/PipeSettings.cs
+++ src/RevitSleeves/Models/Config/PipeSettings.cs
@@ -2,7 +2,32 @@ using Autodesk.Revit.DB;
 
 namespace RevitSleeves.Models.Config;
 internal class PipeSettings : MepCategorySettings {
-    public PipeSettings() : base() { }
+    public PipeSettings() : base() {
+        if(Offsets is null || Offsets.Length == 0) {
+            Offsets = [
+                new Offset(){
+                    OffsetType = OffsetType.FromSleeveAxisToMepAxis,
+                    Value = 5
+                },
+                new Offset(){
+                    OffsetType = OffsetType.FromSleeveEndToTopFloorFace,
+                    Value = 50
+                }];
+        }
+        if(DiameterRanges is null || DiameterRanges.Length == 0) {
+            DiameterRanges = [
+                new DiameterRange(){
+                    StartMepSize = 0,
+                    EndMepSize = 25,
+                    SleeveDiameter = 50
+                },
+                new DiameterRange(){
+                    StartMepSize = 26,
+                    EndMepSize = 50,
+                    SleeveDiameter = 100
+                }];
+        }
+    }
 
     public override BuiltInCategory Category => BuiltInCategory.OST_PipeCurves;
 }
