2eaccb34c784e8f8650cbe6cc020907a197f9344
vlastroG <vlastroganov@gmail.com>
2025-07-03 11:41:13 +0300

    Добавить проверку связей

--- src/RevitSleeves/Services/Core/AllLoadedStructureLinksProvider.cs
+++ src/RevitSleeves/Services/Core/AllLoadedStructureLinksProvider.cs
@@ -6,26 +6,43 @@ using Autodesk.Revit.DB;
 
 using dosymep.Bim4Everyone;
 using dosymep.Bim4Everyone.SimpleServices;
+using dosymep.SimpleServices;
 
 using RevitSleeves.Models;
 
 namespace RevitSleeves.Services.Core;
 internal class AllLoadedStructureLinksProvider : IStructureLinksProvider {
     private readonly RevitRepository _revitRepository;
+    private readonly ILocalizationService _localizationService;
     private readonly IBimModelPartsService _bimModelPartsService;
 
-    public AllLoadedStructureLinksProvider(RevitRepository revitRepository, IBimModelPartsService bimModelPartsService) {
+    public AllLoadedStructureLinksProvider(
+        RevitRepository revitRepository,
+        ILocalizationService localizationService,
+        IBimModelPartsService bimModelPartsService) {
+
         _revitRepository = revitRepository ?? throw new ArgumentNullException(nameof(revitRepository));
+        _localizationService = localizationService ?? throw new ArgumentNullException(nameof(localizationService));
         _bimModelPartsService = bimModelPartsService ?? throw new ArgumentNullException(nameof(bimModelPartsService));
     }
 
 
     public ICollection<RevitLinkInstance> GetLinks() {
-        return [.. new FilteredElementCollector(_revitRepository.Document)
+        RevitLinkInstance[] links = [.. new FilteredElementCollector(_revitRepository.Document)
             .OfClass(typeof(RevitLinkInstance))
             .OfType<RevitLinkInstance>()
             .Where(link => _bimModelPartsService.InAnyBimModelParts(link, BimModelPart.ARPart, BimModelPart.KRPart)
                 && RevitLinkType.IsLoaded(_revitRepository.Document, link.GetTypeId()))];
+        string duplicatedLink = links.GroupBy(l => l.Name).FirstOrDefault(g => g.Count() > 1)?.Key;
+        if(!string.IsNullOrEmpty(duplicatedLink)) {
+            throw new InvalidOperationException(
+                string.Format(_localizationService.GetLocalizedString("Errors.DuplicatedLinks"), duplicatedLink);
+        }
+        if(links.Length == 0) {
+            throw new InvalidOperationException(
+                _localizationService.GetLocalizedString("Errors.CannotFindStructureLinks"));
+        }
+        return links;
     }
 
     public string[] GetOpeningFamilyNames() {
