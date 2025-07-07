1b9b60ea05c48341b3c483af1eb2d60ca394002a
Nikita Divin <72355844+DivinN@users.noreply.github.com>
2025-07-07 09:23:22 +0300

    RevitReinforcementCoefficient: Изменен интерфейс на WPF-UI  (#226)

--- src/RevitReinforcementCoefficient/Views/ReportWindow.xaml.cs
+++ src/RevitReinforcementCoefficient/Views/ReportWindow.xaml.cs
@@ -1,8 +1,17 @@
 using System.Windows;
 
+using dosymep.SimpleServices;
+
 namespace RevitReinforcementCoefficient.Views;
 public partial class ReportWindow {
-    public ReportWindow() {
+    public ReportWindow(ILoggerService loggerService,
+        ISerializationService serializationService,
+        ILanguageService languageService, ILocalizationService localizationService,
+        IUIThemeService uiThemeService, IUIThemeUpdaterService themeUpdaterService)
+        : base(loggerService,
+            serializationService,
+            languageService, localizationService,
+            uiThemeService, themeUpdaterService) {
         InitializeComponent();
     }
 
@@ -10,7 +19,7 @@ public partial class ReportWindow {
         DialogResult = true;
     }
 
-    private void ButtonTestForHide_Click(object sender, RoutedEventArgs e) {
+    private void ButtonForHide_Click(object sender, RoutedEventArgs e) {
         Hide();
     }
 }
