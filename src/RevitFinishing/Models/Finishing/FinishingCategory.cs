272a5190d316723b35dfe0a6c2cfc1a2a87b0ed7
sssenya <senia7@bk.ru>
2025-07-14 17:31:00 +0300

    Удалено свойство имени и добавлен модификатор

--- src/RevitFinishing/Models/Finishing/FinishingCategory.cs
+++ src/RevitFinishing/Models/Finishing/FinishingCategory.cs
@@ -4,30 +4,25 @@ using Autodesk.Revit.DB;
 
 namespace RevitFinishing.Models.Finishing;
 internal class FinishingCategory {
-    public string Name { get; set; }
     public string KeyWord { get; set; }
     public List<BuiltInCategory> Category { get; set; }
 
-    public static FinishingCategory Walls { get; } = new FinishingCategory() {
-        Name = "Стены",
+    public static readonly FinishingCategory Walls = new FinishingCategory() {
         KeyWord = "(О) Стена",
         Category = [BuiltInCategory.OST_Walls]
     };
 
-    public static FinishingCategory Baseboards { get; } = new FinishingCategory() {
-        Name = "Плинтусы",
+    public static readonly FinishingCategory Baseboards = new FinishingCategory() {
         KeyWord = "(О) Плинтус",
         Category = [BuiltInCategory.OST_Walls]
     };
 
-    public static FinishingCategory Floors { get; } = new FinishingCategory() {
-        Name = "Перекрытия",
+    public static readonly FinishingCategory Floors = new FinishingCategory() {
         KeyWord = "(АР)",
         Category = [BuiltInCategory.OST_Floors]
     };
 
-    public static FinishingCategory Ceilings { get; } = new FinishingCategory() {
-        Name = "Потолки",
+    public static readonly FinishingCategory Ceilings = new FinishingCategory() {
         KeyWord = "(О) Потолок",
         Category = [BuiltInCategory.OST_Ceilings, BuiltInCategory.OST_Walls]
     };
