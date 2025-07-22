68877a3e12faae3ddb563ef59fadbce80aca20fa
dosymep <dosymep@gmail.com>
2025-07-22 10:17:40 +0300

    Убрал обработчик

--- src/RevitBatchPrint/Views/SheetsView.xaml.cs
+++ src/RevitBatchPrint/Views/SheetsView.xaml.cs
@@ -13,10 +13,6 @@ public partial class SheetsView {
     private void _albumParamName_OnLoaded(object sender, RoutedEventArgs e) {
         _albumParamName.SetCurrentValue(ItemsControl.ItemsSourceProperty, _albumParamName.OriginalItemsSource);
     }
-    
-    private void _albumParamName_OnGotFocus(object sender, RoutedEventArgs e) {
-        //_albumParamName.IsSuggestionListOpen = true;
-    }
 
     private void _albumParamName_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args) {
         if(!string.IsNullOrEmpty(_albumParamName.Text)) {
