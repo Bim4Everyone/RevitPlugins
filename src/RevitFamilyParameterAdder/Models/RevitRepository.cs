6dddca3519f4bc8087fcd0b9e8bdd064e10cbabb
Nikita Divin <nikitad93@mail.ru>
2025-09-01 17:53:48 +0300

    Произведено обновление интерфейса на WPF-UI

--- src/RevitFamilyParameterAdder/Models/RevitRepository.cs
+++ src/RevitFamilyParameterAdder/Models/RevitRepository.cs
@@ -17,29 +17,31 @@ internal class RevitRepository {
     public Application Application => UIApplication.Application;
     public Document Document => ActiveUIDocument.Document;
 
+    public DefinitionFile SharedParametersFile { get; set; }
 
-    public List<string> GetParamGroupNames() => Application.OpenSharedParameterFile().Groups
+
+    public List<string> GetParamGroupNames() => SharedParametersFile.Groups
         .Select(item => item.Name)
         .OrderBy(item => item)
         .ToList();
 
     public List<ExternalDefinition> GetParamsInShPF() {
         List<ExternalDefinition> paramsInGroup = [];
-
-        DefinitionFile sharedParametersFile = Application.OpenSharedParameterFile();
         // Проходимся по каждой группе ФОП
-        foreach(DefinitionGroup sharedGroup in sharedParametersFile.Groups) {
+        foreach(DefinitionGroup sharedGroup in SharedParametersFile.Groups) {
             // Проходимся по каждому параметру в группе параметров
             foreach(var item in sharedGroup.Definitions) {
                 paramsInGroup.Add(item as ExternalDefinition);
             }
         }
-
         paramsInGroup.Sort((x, y) => string.Compare(x.Name, y.Name));
         return paramsInGroup;
     }
 
-    internal bool IsFamilyFile() {
-        return Document.IsFamilyDocument;
+    internal bool IsFamilyFile() => Document.IsFamilyDocument;
+
+    internal bool IsSharedParametersFileConnected() {
+        SharedParametersFile = Application.OpenSharedParameterFile();
+        return SharedParametersFile != null;
     }
 }
--- src/RevitFamilyParameterAdder/Views/MainWindow.xaml.cs
+++ src/RevitFamilyParameterAdder/Views/MainWindow.xaml.cs
@@ -1,8 +1,18 @@
 using System.Windows;
 
+using dosymep.SimpleServices;
+
 namespace RevitFamilyParameterAdder.Views;
 public partial class MainWindow {
-    public MainWindow() {
+    public MainWindow(
+        ILoggerService loggerService,
+        ISerializationService serializationService,
+        ILanguageService languageService, ILocalizationService localizationService,
+        IUIThemeService uiThemeService, IUIThemeUpdaterService themeUpdaterService)
+        : base(loggerService,
+            serializationService,
+            languageService, localizationService,
+            uiThemeService, themeUpdaterService) {
         InitializeComponent();
     }
 
