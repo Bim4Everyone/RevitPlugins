f1505063d6ec5917f6e76300791cfd4fe4c38318
vlastroG <vlastroganov@gmail.com>
2025-09-02 11:03:47 +0300

    Заменить получение иконок

--- src/RevitRefreshLinks/Resources/PathInfoImageConverter.cs
+++ src/RevitRefreshLinks/Resources/PathInfoImageConverter.cs
@@ -1,27 +1,18 @@
 using System;
 using System.Globalization;
 using System.Windows.Data;
-using System.Windows.Media.Imaging;
-
-using dosymep.Bim4Everyone;
 
 using RevitRefreshLinks.ViewModels;
 
+using Wpf.Ui.Controls;
+
 namespace RevitRefreshLinks.Resources;
 internal class PathInfoImageConverter : IValueConverter {
-    // TODO переделать на WPF UI
-    private static readonly string _iconsFolderPath =
-        $"pack://application:,,,/RevitRefreshLinks_{ModuleEnvironment.RevitVersion};component/Resources/Icons/";
-
-    private static readonly BitmapImage _emptyFolder = GetImg("folder.empty.png");
-    private static readonly BitmapImage _folder = GetImg("folder.png");
-    private static readonly BitmapImage _file = GetImg("file.png");
-
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
         if(value is DirectoryViewModel dir) {
-            return dir.Content.Count > 0 ? _folder : _emptyFolder;
+            return SymbolRegular.Folder24;
         } else if(value is FileViewModel) {
-            return _file;
+            return SymbolRegular.Building24;
         }
 
         return default;
@@ -29,9 +20,5 @@ internal class PathInfoImageConverter : IValueConverter {
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
         throw new NotSupportedException();
-    }
-
-    private static BitmapImage GetImg(string iconName) {
-        return new BitmapImage(new Uri(_iconsFolderPath + iconName, UriKind.Absolute));
     }
 }
