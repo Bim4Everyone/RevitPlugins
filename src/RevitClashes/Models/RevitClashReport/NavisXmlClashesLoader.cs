6392343320bceef82a41fac4ebb747abfef4fcde
vlastroG <vlastroganov@gmail.com>
2025-09-10 17:17:55 +0300

    Скорректировать проверку на null

--- src/RevitClashes/Models/RevitClashReport/NavisXmlClashesLoader.cs
+++ src/RevitClashes/Models/RevitClashReport/NavisXmlClashesLoader.cs
@@ -134,7 +134,7 @@ namespace RevitClashDetective.Models.RevitClashReport {
         private ICollection<ClashModel> GetClashModels(XElement clashTest) {
             return clashTest.Descendants("clashresult")
                 .Select(c => GetClashModel(c))
-                .Where(c => c.MainElement.Id.IsNotNull() && c.OtherElement.Id.IsNotNull())
+                .Where(c => (c.MainElement?.Id.IsNotNull() ?? false) && (c.OtherElement?.Id.IsNotNull() ?? false))
                 .ToArray();
         }
 
