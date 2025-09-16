f9ab5d1860ad81a5013c4d37108a7addce26e3f4
Redomine <52177952+Redomine@users.noreply.github.com>
2025-09-16 08:46:42 +0300

    RevitAxonometryViewUpdate: Сортировка по "ФОП_ВИС_Имя системы", сворачивание дубликатов (#286)

--- src/RevitAxonometryViews/ViewModels/HvacSystemViewModel.cs
+++ src/RevitAxonometryViews/ViewModels/HvacSystemViewModel.cs
@@ -5,14 +5,29 @@ using dosymep.WPF.ViewModels;
 namespace RevitAxonometryViews.ViewModels {
     internal class HvacSystemViewModel : BaseViewModel {
         private bool _isSelected;
+        private string _displaySystemName;
+        private string _displaySharedName;
 
         public HvacSystemViewModel(string systemName, string sharedName) {
             SystemName = systemName;
             SharedName = sharedName;
+
+            _displaySystemName = systemName;
+            _displaySharedName = sharedName;
         }
 
         public string SystemName { get; }
         public string SharedName { get; }
+
+        public string DisplaySystemName {
+            get => _displaySystemName;
+            set => RaiseAndSetIfChanged(ref _displaySystemName, value);
+        }
+
+        public string DisplaySharedName {
+            get => _displaySharedName;
+            set => RaiseAndSetIfChanged(ref _displaySharedName, value);
+        }
 
         public bool IsSelected {
             get => _isSelected;
