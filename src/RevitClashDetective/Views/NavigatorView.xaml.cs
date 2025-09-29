d7e905d0048b19fc0ad7323083bf9e94f7ac8d6b
vlastroG <vlastroganov@gmail.com>
2025-09-29 11:25:50 +0300

    Скорректировать индексы столбцов

--- src/RevitClashDetective/Views/NavigatorView.xaml.cs
+++ src/RevitClashDetective/Views/NavigatorView.xaml.cs
@@ -12,8 +12,8 @@ public partial class NavigatorView {
     public NavigatorView() {
         InitializeComponent();
         string[] paramNames = SettingsConfig.GetSettingsConfig(GetPlatformService<IConfigSerializer>()).ParamNames;
-        int startFirst = 8;
-        int startSecond = 14;
+        int startFirst = 9;
+        int startSecond = 16;
         for(int i = 0; i < paramNames.Length; i++, startFirst++, startSecond += 2) {
             var firstElementColumn = new GridColumn() {
                 Header = $"{paramNames[i]} (1)",
