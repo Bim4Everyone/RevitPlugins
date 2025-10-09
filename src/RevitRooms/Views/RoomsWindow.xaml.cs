41e4fcd0dd78c8b923df117e335320ddb9e864ee
sssenya <senia7@bk.ru>
2025-10-09 20:42:44 +0300

    Окно RevitRooms переведено на WPF UI

--- src/RevitRooms/Views/RoomsWindow.xaml.cs
+++ src/RevitRooms/Views/RoomsWindow.xaml.cs
@@ -1,11 +1,21 @@
 using System.Windows;
 
+using dosymep.SimpleServices;
+
 namespace RevitRooms.Views;
 /// <summary>
 /// Interaction logic for RoomsWindow.xaml
 /// </summary>
 public partial class RoomsWindow {
-    public RoomsWindow() {
+    public RoomsWindow(
+        ILoggerService loggerService,
+        ISerializationService serializationService,
+        ILanguageService languageService, ILocalizationService localizationService,
+        IUIThemeService uiThemeService, IUIThemeUpdaterService themeUpdaterService)
+        : base(loggerService,
+               serializationService,
+               languageService, localizationService,
+               uiThemeService, themeUpdaterService) {
         InitializeComponent();
     }
 
