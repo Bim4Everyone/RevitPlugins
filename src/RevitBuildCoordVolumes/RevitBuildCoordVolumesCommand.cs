d51ae10845d9f438a545f926171a98a868e2fbee
KozlovPS <kozlov_ps@Pmail.ru>
2025-11-07 16:36:49 +0300

    RevitBuildCoordVolumes: Создан проект плагина, добавлено описание

---
+++ src/RevitBuildCoordVolumes/RevitBuildCoordVolumesCommand.cs
@@ -0,0 +1,80 @@
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+
+using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.ProjectConfigs;
+using dosymep.Bim4Everyone.SimpleServices;
+using dosymep.SimpleServices;
+using dosymep.WPF.Views;
+using dosymep.WpfCore.Ninject;
+using dosymep.WpfUI.Core.Ninject;
+using dosymep.Xpf.Core.Ninject;
+
+using Ninject;
+
+using RevitBuildCoordVolumes.Models;
+using RevitBuildCoordVolumes.ViewModels;
+using RevitBuildCoordVolumes.Views;
+
+namespace RevitBuildCoordVolumes;
+
+/// <summary>
+/// Класс команды Revit плагина.
+/// </summary>
+/// <remarks>
+/// В данном классе должна быть инициализация контейнера плагина и указание названия команды.
+/// </remarks>
+[Transaction(TransactionMode.Manual)]
+public class RevitBuildCoordVolumesCommand : BasePluginCommand {
+    /// <summary>
+    /// Инициализирует команду плагина.
+    /// </summary>
+    public RevitBuildCoordVolumesCommand() {
+        PluginName = "RevitBuildCoordVolumes";
+    }
+
+    /// <summary>
+    /// Метод выполнения основного кода плагина.
+    /// </summary>
+    /// <param name="uiApplication">Интерфейс взаимодействия с Revit.</param>
+    /// <remarks>
+    /// В случаях, когда не используется конфигурация
+    /// или локализация требуется удалять их использование полностью во всем проекте.
+    /// </remarks>
+    protected override void Execute(UIApplication uiApplication) {
+        // Создание контейнера зависимостей плагина с сервисами из платформы
+        using IKernel kernel = uiApplication.CreatePlatformServices();
+
+        // Настройка доступа к Revit
+        kernel.Bind<RevitRepository>()
+            .ToSelf()
+            .InSingletonScope();
+
+        // Настройка конфигурации плагина
+        kernel.Bind<PluginConfig>()
+            .ToMethod(c => PluginConfig.GetPluginConfig(c.Kernel.Get<IConfigSerializer>()));
+
+        // Используем сервис обновления тем для WinUI
+        kernel.UseWpfUIThemeUpdater();
+
+        // Настройка запуска окна
+        kernel.BindMainWindow<MainViewModel, MainWindow>();
+
+        // Настройка локализации,
+        // получение имени сборки откуда брать текст
+        string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+        // Настройка локализации,
+        // установка дефолтной локализации "ru-RU"
+        kernel.UseWpfLocalization(
+            $"/{assemblyName};component/assets/localization/language.xaml",
+            CultureInfo.GetCultureInfo("ru-RU"));
+
+        // Вызывает стандартное уведомление
+        Notification(kernel.Get<MainWindow>());
+    }
+}
