eaf875af5900309cfcf3f3a32887c405701f252e
vlastroG <vlastroganov@gmail.com>
2025-12-10 11:47:46 +0300

    Сделать скрытие списка результатов

--- src/RevitParamsChecker/Views/Results/CheckResultsListControl.xaml.cs
+++ src/RevitParamsChecker/Views/Results/CheckResultsListControl.xaml.cs
@@ -1,3 +1,5 @@
+using System.Windows;
+
 using dosymep.SimpleServices;
 
 namespace RevitParamsChecker.Views.Results;
@@ -20,5 +22,20 @@ internal partial class CheckResultsListControl {
             uiThemeService,
             themeUpdaterService) {
         InitializeComponent();
+    }
+
+    public static readonly DependencyProperty IsListVisibleProperty = DependencyProperty.Register(
+        nameof(IsListVisible),
+        typeof(bool),
+        typeof(CheckResultsListControl),
+        new PropertyMetadata(true));
+
+    public bool IsListVisible {
+        get => (bool) GetValue(IsListVisibleProperty);
+        set => SetValue(IsListVisibleProperty, value);
+    }
+
+    private void ListToggleViewButton_Clicked(object sender, RoutedEventArgs e) {
+        IsListVisible = !IsListVisible;
     }
 }
