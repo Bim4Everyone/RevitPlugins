0bb324124c11022047db4c854d4916b4530aab37
sssenya <senia7@bk.ru>
2026-02-13 10:38:05 +0300

    обновлен code behind для окна с приоритетами

--- src/RevitDeclarations/Views/TabItems/PrioritiesTabItem.xaml.cs
+++ src/RevitDeclarations/Views/TabItems/PrioritiesTabItem.xaml.cs
@@ -1,10 +1,18 @@
 using System.Linq;
 using System.Windows.Input;
 
+using dosymep.SimpleServices;
+using RevitDeclarations.ViewModels;
+
 
 namespace RevitDeclarations.Views;
-public partial class PrioritiesTabItem {
-    public PrioritiesTabItem() {
+internal partial class PrioritiesTabItem {
+    public PrioritiesTabItem(MainViewModel viewModel, ILoggerService loggerService,
+                            ILanguageService languageService, ILocalizationService localizationService,
+                            IUIThemeService uiThemeService, IUIThemeUpdaterService themeUpdaterService) : base(loggerService,
+            languageService, localizationService,
+            uiThemeService, themeUpdaterService) {
         InitializeComponent();
+        DataContext = viewModel.PrioritiesViewModel;
     }
 }
