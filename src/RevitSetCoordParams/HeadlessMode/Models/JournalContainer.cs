f157e1490aedcb18a1547c212af1e9bb45c788ac
KozlovPS <ps_kozlov@mail.ru>
2026-02-19 17:44:25 +0300

    RevitSetCoordParams: Реализация взаимодействия со сторонним плагином

---
+++ src/RevitSetCoordParams/HeadlessMode/Models/JournalContainer.cs
@@ -0,0 +1,8 @@
+using RevitSetCoordParams.HeadlessMode.Enums;
+
+namespace RevitSetCoordParams.HeadlessMode.Models;
+
+internal class JournalContainer {
+    public SettingType SettingType { get; set; }
+    public string JournalValue { get; set; }
+}
--- src/RevitSetCoordParams/RevitSetCoordParamsCommandHeadless.cs
+++ src/RevitSetCoordParams/RevitSetCoordParamsCommandHeadless.cs
@@ -3,17 +3,21 @@ using System.Globalization;
 using System.Reflection;
 
 using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 using dosymep.Bim4Everyone;
+using dosymep.Bim4Everyone.ProjectConfigs;
 using dosymep.Bim4Everyone.SimpleServices;
 using dosymep.SimpleServices;
 using dosymep.WpfCore.Ninject;
 
 using Ninject;
 
+using RevitSetCoordParams.HeadlessMode.Models;
 using RevitSetCoordParams.Models;
 using RevitSetCoordParams.Models.Interfaces;
+using RevitSetCoordParams.Models.Services;
 using RevitSetCoordParams.Models.Settings;
 
 namespace RevitSetCoordParams;
@@ -34,11 +38,19 @@ public class RevitSetCoordParamsCommandHeadless : BasePluginCommand {
             .ToSelf()
             .InSingletonScope();
 
-        // Настройка локализации,
-        // получение имени сборки откуда брать текст
+        // Настройка конфигурации плагина
+        kernel.Bind<PluginConfig>()
+            .ToMethod(c => PluginConfig.GetPluginConfig(c.Kernel.Get<IConfigSerializer>()));
+
+        // Настройка сервиса проверки параметров
+        kernel.Bind<IParamAvailabilityService>()
+            .To<ParamAvailabilityService>()
+            .InSingletonScope();
+
+        // Получение имени сборки откуда брать текст
         string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
 
-        // Настройка локализации,
+        // Настройка локализации
         // установка дефолтной локализации "ru-RU"
         kernel.UseWpfLocalization(
             $"/{assemblyName};component/assets/localization/language.xaml",
@@ -55,23 +67,53 @@ public class RevitSetCoordParamsCommandHeadless : BasePluginCommand {
                 .CopyProjectParams()
                 .GetIsChecked();
 
-            var projectConfig = pluginConfig.GetSettings(revitRepository.Document);
+            // Получение настроек проекта
+            ConfigSettings defaultSettings;
             ConfigSettings configSettings;
+            var projectConfig = pluginConfig.GetSettings(revitRepository.Document);
             if(projectConfig == null) {
-                configSettings = new ConfigSettings();
-                configSettings.ApplyDefaultValues(revitRepository);
+                defaultSettings = new ConfigSettings();
+                defaultSettings.ApplyDefaultValues(revitRepository);
+                configSettings = defaultSettings;
             } else {
+                defaultSettings = new ConfigSettings();
+                defaultSettings.ApplyDefaultValues(revitRepository);
                 configSettings = projectConfig.ConfigSettings;
             }
 
-            // Создание основного класса настроек и загрузка в него настроек по умолчанию
-            var setCoordParamsSettings = new SetCoordParamsSettings(revitRepository, configSettings);
-            setCoordParamsSettings.LoadConfigSettings();
+            // Класс для чтения JournalData для дальнейшего парсинга
+            var reader = new JournalDataReader(JournalData);
 
-            // Создание основного класса процессора
+            var paramService = kernel.Get<IParamAvailabilityService>();
+            var paramFactory = kernel.Get<IRevitParamFactory>();
+
+            // Класс для парсинга JournalData
+            var resolver = new JournalSettingsResolver(
+                configSettings,
+                defaultSettings,
+                paramService,
+                paramFactory,
+                revitRepository);
+
+            // Переопределение настроек
+            string sourceFile = resolver.ResolveString(reader.SourceFile, x => x.SourceFile);
+            var sourceDocument = revitRepository.FindDocumentsByName(sourceFile);
+
+            var finalSettings = new ConfigSettings {
+                ElementsProvider = resolver.ResolveEnum(reader.ElementsProvider, x => x.ElementsProvider),
+                PositionProvider = resolver.ResolveEnum(reader.PositionProvider, x => x.PositionProvider),
+                SourceFile = sourceFile,
+                TypeModels = resolver.ResolveListString(reader.TypeModels, x => x.TypeModels),
+                ParamMaps = resolver.ResolveParamMaps(reader.ParamMaps, x => x.ParamMaps, sourceDocument),
+                Categories = resolver.ResolveListEnum(reader.Categories, x => x.Categories),
+                MaxDiameterSearchSphereMm = resolver.ResolveDouble(reader.MaxDiameterSearchSphereMm, x => x.MaxDiameterSearchSphereMm),
+                StepDiameterSearchSphereMm = resolver.ResolveDouble(reader.StepDiameterSearchSphereMm, x => x.StepDiameterSearchSphereMm),
+                Search = resolver.ResolveBool(reader.Search, x => x.Search)
+            };
+
+            var setCoordParamsSettings = new SetCoordParamsSettings(revitRepository, finalSettings);
             IIntersectProcessor processor = new IntersectCurveProcessor(localizationService, revitRepository, setCoordParamsSettings);
 
-            // Основной метод
             processor.Run();
 
         } catch(Exception ex) {
