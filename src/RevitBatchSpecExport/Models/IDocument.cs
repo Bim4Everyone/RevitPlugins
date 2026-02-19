5b8d5b27b50990dd302e8a210330b761b939442b
Vladimir Stroganov <vlastroganov@gmail.com>
2026-02-19 16:13:19 +0300

    RevitBatchSpecExport: Создан плагин для пакетного экспорт спецификаций с листов в Excel (#382)

---
+++ src/RevitBatchSpecExport/Models/IDocument.cs
@@ -0,0 +1,15 @@
+namespace RevitBatchSpecExport.Models;
+/// <summary>
+/// Документ Revit для обработки
+/// </summary>
+internal interface IDocument {
+    /// <summary>
+    /// Название документа
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// Путь к Revit документу
+    /// </summary>
+    string Path { get; }
+}
