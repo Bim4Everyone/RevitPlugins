6dfc486fa781b826576aa2cbcf876a77aa28ae6f
sssenya <senia7@bk.ru>
2026-02-25 22:43:09 +0300

    Удалено лишнее поле класса

--- src/RevitDeclarations/Models/Rooms/Separators/FamInstanceRoomSeparator.cs
+++ src/RevitDeclarations/Models/Rooms/Separators/FamInstanceRoomSeparator.cs
@@ -3,13 +3,9 @@ using Autodesk.Revit.DB.Architecture;
 
 namespace RevitDeclarations.Models;
 internal class FamInstanceRoomSeparator : RoomSeparator {
-    private readonly ApartmentsProject _project;
-
     public FamInstanceRoomSeparator(ApartmentsProject project, FamilyInstance door) {
-        _project = project;
-
-        AddRoom(door.get_FromRoom(_project.Phase));
-        AddRoom(door.get_ToRoom(_project.Phase));
+        AddRoom(door.get_FromRoom(project.Phase));
+        AddRoom(door.get_ToRoom(project.Phase));
     }
 
     private void AddRoom(Room room) {
