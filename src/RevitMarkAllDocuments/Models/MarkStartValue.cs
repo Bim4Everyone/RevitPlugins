9e08a027fe035eb6b19aa7142f35e919ec038aea
sssenya <senia7@bk.ru>
2026-04-03 17:03:58 +0300

    Добавлен класс для хранения значения марки

---
+++ src/RevitMarkAllDocuments/Models/MarkStartValue.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitMarkAllDocuments.Models;
+
+internal class MarkStartValue {
+    public string Prefix { get; set; }
+    public string Suffix { get; set; }
+    public string StartValue { get; set; }
+}
