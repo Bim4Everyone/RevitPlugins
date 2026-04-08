1df7883ccae442f38be7022c187b09834233ee11
KozlovPS <kozlov_ps@Pmail.ru>
2026-04-08 15:32:51 +0300

    RevitBuildCoordVolumes: Добавление свойства в класс TypeZone

--- src/RevitBuildCoordVolumes/ViewModels/TypeZoneViewModel.cs
+++ src/RevitBuildCoordVolumes/ViewModels/TypeZoneViewModel.cs
@@ -5,9 +5,14 @@ namespace RevitBuildCoordVolumes.ViewModels;
 internal class TypeZoneViewModel : BaseViewModel {
 
     private string _name;
+    private bool _isChecked;
 
     public string Name {
         get => _name;
         set => RaiseAndSetIfChanged(ref _name, value);
+    }
+    public bool IsChecked {
+        get => _isChecked;
+        set => RaiseAndSetIfChanged(ref _isChecked, value);
     }
 }
