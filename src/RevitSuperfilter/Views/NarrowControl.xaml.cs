efa8bc119a8e0fd82ca51ff9c0d056cfff18950c
dosymep <dosymep@gmail.com>
2026-04-08 13:06:15 +0300

    Добавил контролы

---
+++ src/RevitSuperfilter/Views/NarrowControl.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows.Controls;
+
+namespace RevitSuperfilter.Views;
+
+public partial class NarrowControl : UserControl {
+    public NarrowControl() {
+        InitializeComponent();
+    }
+}
+
---
+++ src/RevitSuperfilter/Views/WideControl.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows.Controls;
+
+namespace RevitSuperfilter.Views;
+
+public partial class WideControl : UserControl {
+    public WideControl() {
+        InitializeComponent();
+    }
+}
+
