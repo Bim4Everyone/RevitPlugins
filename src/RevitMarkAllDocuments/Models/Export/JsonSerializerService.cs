0682bea2f862c87c0ae9349c5eb587b9da9b5005
sssenya <senia7@bk.ru>
2026-04-16 11:21:47 +0300

    Обновлен класс для записи и чтения json

---
+++ src/RevitMarkAllDocuments/Models/Export/JsonSerializerService.cs
@@ -0,0 +1,24 @@
+using System.IO;
+
+using dosymep.Bim4Everyone.SimpleServices;
+using dosymep.SimpleServices;
+
+namespace RevitMarkAllDocuments.Models.Export;
+
+internal class JsonSerializerService {
+    private readonly ISerializationService _serializationService = ServicesProvider
+        .GetPlatformService<ISerializationService>();
+
+    public void ExportMarkData(string filePath, MarkData markData) {
+        string serializedData = _serializationService.Serialize(markData);
+        filePath = Path.ChangeExtension(filePath, "json");
+
+        using var file = File.CreateText(filePath);
+        file.Write(serializedData);
+    }
+
+    public MarkData ImportMarkData(string filePath) {
+        string json = File.ReadAllText(filePath);
+        return _serializationService.Deserialize<MarkData>(json);
+    }
+}
