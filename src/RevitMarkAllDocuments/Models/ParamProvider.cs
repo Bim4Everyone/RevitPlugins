6ddacec2509d10834375d13e26653801bb1bdd79
sssenya <senia7@bk.ru>
2026-04-22 11:24:50 +0300

    Добавлен класс для получения параметров

---
+++ src/RevitMarkAllDocuments/Models/ParamProvider.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+
+namespace RevitMarkAllDocuments.Models;
+
+internal class ParamProvider {
+    private readonly List<FilterableParam> _params;
+    private readonly bool _isMarkForTypes;
+
+    public ParamProvider(RevitRepository revitRepository, Category category, bool isMarkForTypes) {
+        _params = [.. revitRepository.GetFilterableParams(category)];
+        _isMarkForTypes = isMarkForTypes;
+    }
+
+    /// <summary>
+    /// Метод получения параметров для фильтрации и сортировки.
+    /// Для типоразмеров это только параметры типоразмера.
+    /// Для экземпляров это параметры экземпляра и парамтеры типоразмера.
+    /// </summary>
+    public List<FilterableParam> GetParamsForFilterAndSort() {
+        if(_isMarkForTypes) {
+            return [.. _params.Where(x => x.IsTypeParam)];
+        }
+
+        return _params;
+    }
+
+    /// <summary>
+    /// Метод получения параметров, в которые может быть записано значение марки.
+    /// Для типоразмеров это только параметры типоразмера. 
+    /// Для экземпляров это только параметры экземпляра.
+    /// </summary>
+    public List<FilterableParam> GetParamsForMarks() {
+        return [.. _params.Where(x => x.IsTypeParam == _isMarkForTypes)];
+    }
+}
