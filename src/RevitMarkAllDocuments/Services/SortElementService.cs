902682fb351df17417114c757d3682abd6e9dffd
sssenya <senia7@bk.ru>
2026-04-26 14:07:50 +0300

    Рефакторинг метода сортировки

--- src/RevitMarkAllDocuments/Services/SortElementService.cs
+++ src/RevitMarkAllDocuments/Services/SortElementService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,32 +14,25 @@ internal class SortElementService {
     public IOrderedEnumerable<MarkedElement> SortElements(bool isForType,
                                                           IList<MarkedElement> elements, 
                                                           IList<FilterableParam> sortParams) {
-        IOrderedEnumerable<MarkedElement> sortedElements;
+        var sortedElements = elements.OrderBy(GetSortKey(isForType, sortParams[0]));
 
-        var selectedParamST = sortParams[0].RevitParam.StorageType;
-
-        if(selectedParamST == StorageType.Integer || selectedParamST == StorageType.ElementId) {
-            sortedElements = elements.OrderBy(x => x.GetElementWithParam(isForType, sortParams[0]).GetParamValue<int>(sortParams[0].RevitParam));
-        } else if(selectedParamST == StorageType.Double) {
-            sortedElements = elements.OrderBy(x => x.GetElementWithParam(isForType, sortParams[0]).GetParamValue<double>(sortParams[0].RevitParam));
-        } else {
-            sortedElements = elements.OrderBy(x => x.GetElementWithParam(isForType, sortParams[0]).GetParamValue<string>(sortParams[0].RevitParam));
-        }
-
-        if(sortParams.Count > 1) {
-            foreach(var param in sortParams.Skip(1)) {
-                var paramST = param.RevitParam.StorageType;
-
-                if(paramST == StorageType.Integer || paramST == StorageType.ElementId) {
-                    sortedElements = sortedElements.ThenBy(x => x.GetElementWithParam(isForType, param).GetParamValue<int>(param.RevitParam));
-                } else if(paramST == StorageType.Double) {
-                    sortedElements = sortedElements.ThenBy(x => x.GetElementWithParam(isForType, param).GetParamValue<double>(param.RevitParam));
-                } else {
-                    sortedElements = sortedElements.ThenBy(x => x.GetElementWithParam(isForType, param).GetParamValue<string>(param.RevitParam));
-                }
-            }
+        foreach(var param in sortParams.Skip(1)) {
+            sortedElements = sortedElements.ThenBy(GetSortKey(isForType, param));
         }
 
         return sortedElements;
+    }
+
+    private Func<MarkedElement, IComparable> GetSortKey(bool isForType, FilterableParam param) {
+        return param.RevitParam.StorageType switch {
+            StorageType.Integer => x => 
+                x.GetElementWithParam(isForType, param).GetParamValue<int>(param.RevitParam),
+            StorageType.ElementId => x => 
+                x.GetElementWithParam(isForType, param).GetParamValue<long>(param.RevitParam),
+            StorageType.Double => x => 
+                x.GetElementWithParam(isForType, param).GetParamValue<double>(param.RevitParam),
+            _ => x => 
+                x.GetElementWithParam(isForType, param).GetParamValue<string>(param.RevitParam),
+        };
     }
 }
