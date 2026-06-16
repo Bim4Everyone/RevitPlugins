86f2e21e5c8c549cd5ee572867c64036a9a6f55f
vlastroG <vlastroganov@gmail.com>
2026-06-16 15:00:52 +0300

    Скорректировать проверку перекрытий

--- src/RevitPylonLoadAreas/Models/Selection/FloorSelectionFilter.cs
+++ src/RevitPylonLoadAreas/Models/Selection/FloorSelectionFilter.cs
@@ -10,7 +10,12 @@ namespace RevitPylonLoadAreas.Models.Selection;
 internal sealed class FloorSelectionFilter : ISelectionFilter {
     public bool AllowElement(Element elem) {
         // допустимы только перекрытия с 1 телом
-        return elem is Floor && elem.GetSolids()?.Where(s => s.GetVolumeOrDefault(0) > 0)?.Count() == 1;
+        return elem is Floor
+               && elem.GetSolids()
+                   .SelectMany(SolidUtils.SplitVolumes)
+                   ?.Where(s => s.GetVolumeOrDefault(0) > 0)
+                   ?.Count()
+               == 1;
     }
 
     public bool AllowReference(Reference reference, XYZ position) {
