3c720c9cc17ab4d805ccbbee9bc12864fc6fd910
Дивин Никита Владимирович <Divin_n@a101.ru>
2026-07-06 16:32:56 +0300

    В CustomParamItem добавлено свойство зависимости для стиля TextBox

--- src/RevitPackageDocumentation/Views/Controls/CustomParamItemControl.xaml.cs
+++ src/RevitPackageDocumentation/Views/Controls/CustomParamItemControl.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace RevitPackageDocumentation.Views.Controls;
@@ -5,7 +6,15 @@ namespace RevitPackageDocumentation.Views.Controls;
 /// Логика взаимодействия для CustomParamItemControl.xaml
 /// </summary>
 public partial class CustomParamItemControl : UserControl {
+    public static readonly DependencyProperty TextBoxStyleProperty =
+        DependencyProperty.Register(nameof(TextBoxStyle), typeof(Style), typeof(CustomParamItemControl));
+
     public CustomParamItemControl() {
         InitializeComponent();
+    }
+
+    public Style TextBoxStyle {
+        get => (Style) GetValue(TextBoxStyleProperty);
+        set => SetValue(TextBoxStyleProperty, value);
     }
 }
