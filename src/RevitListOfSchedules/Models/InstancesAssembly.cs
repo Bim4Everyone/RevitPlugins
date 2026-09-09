2128318091acae8a1c79fe6fb0bf234bb8c14648
Pavel Kozlov <64225538+KozlovPS@users.noreply.github.com>
2026-09-09 16:52:10 +0300

    RevitListOfSchedules: Исправление метода получения имени спецификации (#499)

--- src/RevitListOfSchedules/Models/InstancesAssembly.cs
+++ src/RevitListOfSchedules/Models/InstancesAssembly.cs
@@ -4,22 +4,19 @@ using System.Linq;
 
 using Autodesk.Revit.DB;
 
-using dosymep.Bim4Everyone;
 using dosymep.Revit;
 using dosymep.SimpleServices;
 
 namespace RevitListOfSchedules.Models;
 internal class InstancesAssembly {
-    private readonly ILocalizationService _localizationService;
     private readonly IList<string> _approvedLines;
     private readonly RevitRepository _revitRepository;
 
     public InstancesAssembly(
         ILocalizationService localizationService,
         RevitRepository revitRepository) {
-        _localizationService = localizationService;
         _revitRepository = revitRepository;
-        string localizedApprovedLines = _localizationService.GetLocalizedString("InstancesAssembly.ApprovedLines");
+        string localizedApprovedLines = localizationService.GetLocalizedString("InstancesAssembly.ApprovedLines");
         _approvedLines = localizedApprovedLines
             .Split([','], StringSplitOptions.RemoveEmptyEntries)
             .Select(s => s.Trim())
@@ -27,41 +24,58 @@ internal class InstancesAssembly {
     }
 
     public void PlaceFamilyInstances(
-        string sheetNumber,
-        string sheetRevNumber,
-        IList<ViewSchedule> listOfSchedules,
-        FamilySymbol familySymbol,
-        ViewDrafting viewDrafting,
-        string albumName) {
-        foreach(var schedule in listOfSchedules) {
-            var tableData = schedule.GetTableData();
-            var headData = tableData.GetSectionData(SectionType.Header);
+    string sheetNumber,
+    string sheetRevNumber,
+    IList<ViewSchedule> listOfSchedules,
+    FamilySymbol familySymbol,
+    ViewDrafting viewDrafting,
+    string albumName) {
+        
+    foreach(var schedule in listOfSchedules) {
+        var tableData = schedule.GetTableData();
+        var headData = tableData.GetSectionData(SectionType.Header);
 
-            if(headData != null && !headData.HideSection) {
-                string resultScheduleName = null;
-                bool found = false;
+        // Спецификации без шапки не учитываем
+        if(headData is null || headData.HideSection) {
+            continue;
+        }
 
-                for(int i = headData.FirstRowNumber; i < headData.NumberOfRows && !found; i++) {
-                    for(int j = headData.FirstColumnNumber; j < headData.NumberOfColumns && !found; j++) {
-                        string cellText = headData.GetCellText(i, j);
-                        if(_approvedLines.Any(cellText.ToLower().Contains)) {
-                            resultScheduleName = cellText;
-                            found = true;
-                        }
-                    }
+        // Шапка должна содержать хотя бы одну ячейку
+        if(headData.NumberOfRows < 1 || headData.NumberOfColumns < 1) {
+            continue;
+        }
+
+        string resultScheduleName = null;
+
+        for (int rowIndex = headData.FirstRowNumber; rowIndex <= headData.LastRowNumber; rowIndex++) {
+            for (int columnIndex = headData.FirstColumnNumber; columnIndex <= headData.LastColumnNumber; columnIndex++) {
+                
+                string cellText = schedule.GetCellText(SectionType.Header, rowIndex, columnIndex);
+
+                if(string.IsNullOrEmpty(cellText) || !_approvedLines.Any(x => cellText.IndexOf(x, StringComparison.OrdinalIgnoreCase) >= 0)) {
+                    continue;
                 }
-                if(found) {
-                    PlaceFamilyInstance(sheetNumber, sheetRevNumber, resultScheduleName, familySymbol, viewDrafting, albumName);
-                } else if(!found
-                    && headData.NumberOfRows == 1
-                    && headData.NumberOfColumns == 1
-                    && string.IsNullOrEmpty(headData.GetCellText(0, 0))) {
 
-                    PlaceFamilyInstance(sheetNumber, sheetRevNumber, schedule.Name, familySymbol, viewDrafting, albumName);
-                }
+                resultScheduleName = cellText;
+                break;
+            }
+
+            if(resultScheduleName is not null) {
+                break;
             }
         }
+
+        // Если в шапке ничего подходящего нет, проверяем название самой спецификации
+        if(resultScheduleName is null && _approvedLines.Any(x => schedule.Name.IndexOf(x, StringComparison.OrdinalIgnoreCase) >= 0)) {
+            resultScheduleName = schedule.Name;
+        }
+
+        // Если нашли подходящее имя — размещаем
+        if(resultScheduleName is not null) {
+            PlaceFamilyInstance(sheetNumber, sheetRevNumber, resultScheduleName, familySymbol, viewDrafting, albumName);
+        }
     }
+}
 
     public void PlaceFamilyInstance(
         string sheetNumber,
