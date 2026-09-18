fb6cbe8feffe92b6d4f966af581b9905165658fc
KozlovPS <ps_kozlov@mail.ru>
2026-09-18 17:23:49 +0300

    RevitDeclarations: Создание класса с помещением и его границами

---
+++ src/RevitDeclarations/Models/Rooms/RoomBoundaryInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+
+namespace RevitDeclarations.Models.Rooms;
+
+internal sealed class RoomBoundaryInfo(Room room, List<BoundarySegment> segments) {
+    public Room Room { get; } = room;
+    public List<BoundarySegment> Segments { get; } = segments;
+}
